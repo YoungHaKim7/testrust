ddf1b9c8fa14e1ac8d5cca0fc6803cabad4fefee
YoungHa Kim <ytok1108@aol.com>
2021-12-06 01:10:18 +0900

    loop _ Contrl Flow

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 fn main() {
     // Control Flow
-    let condition = true;
-    let number = if condition { 5 } else { 6 };
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter;
+        }
+    };
+    println!("The result is {}", result);
 }
