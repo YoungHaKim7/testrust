b552fe2114766750ad0fd34ab63ef0932e244cb6
YoungHa Kim <ytok1108@aol.com>
2021-12-06 00:58:05 +0900

    function 2

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 fn main() {
-    my_function(11, 22);
+    let sum = my_function(11, 22);
+    println!("The sum is : {}", sum);
 }
 
-fn my_function(x: i32, y: i32) {
+fn my_function(x: i32, y: i32) -> i32 {
     println!("The value of x is : {}", x);
     println!("The value of x is : {}", y);
+    let sum = x + y;
+    sum
 }
