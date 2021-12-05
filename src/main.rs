f956bfc170882be58eb9c215c2f1d093d176d05c
YoungHa Kim <ytok1108@aol.com>
2021-12-06 00:50:32 +0900

    2 argument

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
-    my_function();
+    my_function(11, 22);
 }
 
-fn my_function() {
-    println!("Another function.");
+fn my_function(x: i32, y: i32) {
+    println!("The value of x is : {}", x);
+    println!("The value of x is : {}", y);
 }
