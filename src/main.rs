1b088d93629a17120b0f27de33ca73ed674656a5
YoungHa Kim <ytok1108@aol.com>
2021-12-06 01:03:55 +0900

    control Flow 2 if 5 else 6

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,5 @@
 fn main() {
     // Control Flow
-    let number = 5;
-
-    if number < 10 {
-        println!("first condition was true");
-    } else if number < 22 {
-        println!("second condition was true");
-    } else {
-        println!("condition was false");
-    }
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
 }
