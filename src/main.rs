b62c2f732f9962d19a91d8581768cfebfddcc30a
YoungHa Kim <ytok1108@aol.com>
2021-12-06 00:37:51 +0900

    Decimal, Hex, Octal, Binary, Byte, Bool ...

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,35 @@
 fn main() {
-    let x = 5;
-    println!("The value of x is {}", x);
-    let x = "six";
-    println!("The value of x is {}", x);
+    let a: i32 = 98_222; // Decimal
+    let b: i32 = 0xff; // Hex
+    let c: i32 = 0o77; // Octal
+    let d: i32 = 0b1111_0000; // Binary
+    let e: u8 = b'A'; // Byte (u8 only)
+
+    let f: u8 = 255; // OverFlow  257은 1로 인식
+    let g: f64 = 2.0;
+
+    //addition
+    let sum: i32 = 5 + 10;
+
+    //subtraction
+    let difference: f64 = 95.5 - 4.3;
+
+    //multiplication
+    let product: i32 = 4 * 30;
+
+    // division
+    let quotient: f64 = 56.7 / 32.2;
+
+    //remainder
+    let remainder: i32 = 43 % 5;
+
+    // Booleans
+    let t: bool = true;
+
+    let f: bool = false;
+
+    // Character
+    let x: char = 'z';
+    let z: char = 'Ψ';
+    let heart eyed cat: char = '🎃';
 }
