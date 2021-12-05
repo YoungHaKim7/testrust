ec4b07669837bd9050605299262550cb030651f5
YoungHa Kim <ytok1108@aol.com>
2021-12-06 01:01:03 +0900

    Contrl Flow__If_else if_else

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 fn main() {
-    let sum = my_function(11, 22);
-    println!("The sum is : {}", sum);
-}
+    // Control Flow
+    let number = 5;
 
-fn my_function(x: i32, y: i32) -> i32 {
-    println!("The value of x is : {}", x);
-    println!("The value of x is : {}", y);
-    let sum = x + y;
-    sum
+    if number < 10 {
+        println!("first condition was true");
+    } else if number < 22 {
+        println!("second condition was true");
+    } else {
+        println!("condition was false");
+    }
 }
