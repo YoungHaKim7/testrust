ad7e8124b91c83da9846ec56ca2e5721d112299d
YoungHa Kim <ytok1108@aol.com>
2021-12-06 01:21:16 +0900

    Comment 2 Type // and /*  */

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 fn main() {
-    // Control Flow
-    let a = [10, 20, 30, 40, 50];
+    // Line comment
 
-    for element in a.iter() {
-        println!("the value is: {}", element);
-    }
-    for number in 1..4 {
-        println!("{}!", number);
-    }
+    /*
+        Block comment
+    */
 }
