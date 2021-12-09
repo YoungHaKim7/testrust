86efac72d3232f16153abe2efc90801c84d85ea8
YoungHa Kim <ytok1108@aol.com>
2021-12-10 00:20:52 +0900

    debug print..binary

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
 fn main() {
-    let my_variable = &9;
-    println!("{:p}", my_variable);
+    //u8
+
+    // 0, 1
+    // 0, 2
+    // 0, 4
+    // 0, 8
+    // 0, 16
+
+    let my_variable = 14;
+    println!("{:b}", my_variable);
 }
