4e36e0232557927fca40a286cd432f656c4de81a
YoungHa Kim <ytok1108@aol.com>
2021-12-10 00:01:11 +0900

    reference

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,6 @@
-// mutability
-// shadowing 같은 이름을 다시 쓰는 것
-
 fn main() {
-    let my_variable = 9;
-    println!("{}", my_variable);
-    {
-        let my_variable = "Some string";
-        println!("{}", my_variable);
-    }
-    println!("{}", my_variable);
+    let my_number = 15; // This is an i32
+    let single_reference = &my_number; //  reference to my_number
+    let double_reference = &single_reference; // This is a &&i32
+    let five_references = &&&&&my_number; // This is a &&&&&i32
 }
