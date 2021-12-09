7d780760cf97ffc06798d714ff948c745493c2f2
YoungHa Kim <ytok1108@aol.com>
2021-12-10 00:04:46 +0900

    raw text print

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
 fn main() {
-    let my_number = 15; // This is an i32
-    let single_reference = &my_number; //  reference to my_number
-    let double_reference = &single_reference; // This is a &&i32
-    let five_references = &&&&&my_number; // This is a &&&&&i32
+    print!(r#"c:\thisdrive\new_drive"#); //raw text
 }
