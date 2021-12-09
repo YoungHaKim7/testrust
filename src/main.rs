9f3973e3027e83100c4e6f2f03d5899d1e1e7add
YoungHa Kim <ytok1108@aol.com>
2021-12-09 23:27:05 +0900

    shadowing two 2

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,12 @@
 // mutability
 // shadowing 같은 이름을 다시 쓰는 것
 
-// immutable by default
-// mut = mutable
-
-fn double(input: i32) -> i32 {
-    input * 2
-}
-
-fn triple(input: i32) -> i32 {
-    input * 3
-}
-
 fn main() {
-    let x = 9;
-    let x = double(x);
-    let x = triple(x);
-
-    println!("{}", x);
+    let my_variable = 9;
+    println!("{}", my_variable);
+    {
+        let my_variable = "Some string";
+        println!("{}", my_variable);
+    }
+    println!("{}", my_variable);
 }
