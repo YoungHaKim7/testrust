3ae6a6365db6ecdc7a5281761053ed58457083cd
YoungHa Kim <ytok1108@aol.com>
2021-12-10 00:56:49 +0900

    String

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 fn main() {
-    let title = "TODAY'S NEWS";
-    println!("{:-^30}", title); // no variable name, pad with -, put in centre, 30 characters long
-    let bar = "|";
-    println!("{: <15}{: >15}", bar, bar); // no variable name, pad with space, 15 characters each, one to the left, one to the right
-    let a = "SEOUL";
-    let b = "TOKYO";
-    println!("{city1:-<15}{city2:->15}", city1 = a, city2 = b); // variable names city1 and city2, pad with -, one to the left, one to the right
+    // String
+    // &str ref str "string slice"
+    let my_name = "David"; // &str
+    let my_name1 = "David".to_string(); // String
+    let other_name = String::from("David2");
+    // growable + shrinkable
+    let mut my_other_name = "David3".to_string();
+    my_other_name.push('!');
+    println!("{}", my_other_name);
 }
