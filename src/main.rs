bdbb469ffbd2a0156883e06f2744f468c4970b49
YoungHa Kim <ytok1108@aol.com>
2021-12-10 01:27:38 +0900

    Len() & capacity

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,22 @@ fn main() {
 
     // method = .Function
     let mut my_name = "".to_string();
-    println!("Capacity is : {}", my_name.capacity());
+    println!(
+        "Length is {} and capacity is : {}",
+        my_name.len(),
+        my_name.capacity()
+    );
     my_name.push_str("David!");
-    println!("Capacity is : {}", my_name.capacity());
+    println!(
+        "Length is {} and capacity is : {}",
+        my_name.len(),
+        my_name.capacity()
+    );
     my_name.push('!');
     my_name.push_str(" and I live in Seoul");
-    println!("Capacity is : {}", my_name.capacity());
+    println!(
+        "Length is {} and capacity is : {}",
+        my_name.len(),
+        my_name.capacity()
+    );
 }
