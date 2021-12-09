1019eb5e0dad425c30a8c8b961b96c88a92e3808
YoungHa Kim <ytok1108@aol.com>
2021-12-10 01:07:12 +0900

    String str &str

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
     // String
     // &str ref str "string slice"
+
+    /*
+    String = Sized types   데이타는 heap에 있다. String은 어떤 구조의 타입이다. 크기를 알수 있기 때문에 컴파일 에러 안남.
+    str = dynamic types     str은 데이타의 크기를 알 수 없다. 그래서
+    &str = dynamic types     Reference를 해 준다.
+    */
     let my_name = "David"; // &str
     let my_name1 = "David".to_string(); // String
     let other_name = String::from("David2");
