331aac985714041037d9e8850adb1fcf65599d67
YoungHa Kim <ytok1108@aol.com>
2021-12-09 22:55:57 +0900

    functions, code blocks

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
-// mod print;
-// mod vars;
-// mod types;
-mod strings;
+fn give_number(one: i32, two: i32) -> i32 {
+    one * two
+}
 
 fn main() {
-    // print::run();
-    // vars::run();
-    // types::run();
-    strings::run();
+    let my_number = give_number(9, 8);
+    println!("{}", my_number);
 }
