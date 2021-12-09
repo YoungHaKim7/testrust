0306aecc9732238b51c69245688ee99949b7cd96
YoungHa Kim <ytok1108@aol.com>
2021-12-10 01:12:57 +0900

    String Methods

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
 fn main() {
     // String
-    // &str ref str "string slice"
+    // .capacity
+    // .push
+    // .push_str
+    // .pop
+    // .with_capacity
 
-    /*
-    String = Sized types   데이타는 heap에 있다. String은 어떤 구조의 타입이다. 크기를 알수 있기 때문에 컴파일 에러 안남.
-    str = dynamic types     str은 데이타의 크기를 알 수 없다. 그래서
-    &str = dynamic types     Reference를 해 준다.
-    */
-    let my_name = "David"; // &str
-    let my_name1 = "David".to_string(); // String
-    let other_name = String::from("David2");
-    // growable + shrinkable
-    let mut my_other_name = "David3".to_string();
-    my_other_name.push('!');
-    println!("{}", my_other_name);
+    // method = .Function
+    let mut my_name = "David".to_string();
+    my_name.push('!');
+    println!("My name is {}", my_name);
 }
