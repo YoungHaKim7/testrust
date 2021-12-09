3a28661f722f07db205974f77c2b1b531e2885b6
YoungHa Kim <ytok1108@aol.com>
2021-12-10 00:44:55 +0900

    print Module std::fmt

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
 fn main() {
-    //u8
-
-    // 0, 1
-    // 0, 2
-    // 0, 4
-    // 0, 8
-    // 0, 16
-
-    let my_variable = 14;
-    println!("{:b}", my_variable);
+    let title = "TODAY'S NEWS";
+    println!("{:-^30}", title); // no variable name, pad with -, put in centre, 30 characters long
+    let bar = "|";
+    println!("{: <15}{: >15}", bar, bar); // no variable name, pad with space, 15 characters each, one to the left, one to the right
+    let a = "SEOUL";
+    let b = "TOKYO";
+    println!("{city1:-<15}{city2:->15}", city1 = a, city2 = b); // variable names city1 and city2, pad with -, one to the left, one to the right
 }
