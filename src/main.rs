b0b9ef0c3144c4015bfa10449d4037bc55e8c670
YoungHa Kim <ytok1108@aol.com>
2021-12-09 23:06:58 +0900

    function + return value return x !!!

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-fn print_number(one: i32, two: i32) {
-    let mutiplied = one * two;
-    println!("{}", mutiplied);
+fn give_number(one: i32, two: i32) -> i32 {
+    let mutiplied_by_ten = {
+        let first_number = 10;
+        first_number * one * two
+    };
+    mutiplied_by_ten
 }
 
 fn main() {
-    print_number(7, 8);
+    let my_number = give_number(9, 1);
+    println!("{}", my_number);
 }
