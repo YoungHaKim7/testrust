82969a034abb7c66a2a741903d24b02d357ded6e
YoungHa Kim <ytok1108@aol.com>
2021-12-09 23:00:35 +0900

    functions another way

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-fn give_number(one: i32, two: i32) -> i32 {
-    one * two
+fn print_number(one: i32, two: i32) {
+    let mutiplied = one * two;
+    println!("{}", mutiplied);
 }
 
 fn main() {
-    let my_number = give_number(9, 8);
-    println!("{}", my_number);
+    print_number(7, 8);
 }
