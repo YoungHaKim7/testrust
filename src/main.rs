5266ca1c7d55fb8d5394b0ce0023f78b05616a6c
YoungHa Kim <ytok1108@aol.com>
2021-12-10 01:14:53 +0900

    push_str .. String Methods

--- src/main.rs
+++ src/main.rs
@@ -9,5 +9,6 @@ fn main() {
     // method = .Function
     let mut my_name = "David".to_string();
     my_name.push('!');
+    my_name.push_str(" and I live in Seoul");
     println!("My name is {}", my_name);
 }
