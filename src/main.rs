f481c2cee32aabcf470b8999502739679ba54903
YoungHa Kim <ytok1108@aol.com>
2021-12-10 23:12:59 +0900

    This code doesn't work

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 // OWNERSHIP - 소유권
 
+fn return_it() -> &'static String {
+    let country = String::from("대한민국");
+    &country // return &String
+}
+
 // & = reference
 
 fn main() {
-    let country = String::from("대한민국");
-    let ref_one = &country;
-    let ref_two = &country;
-
-    println!("Country is : {}", ref_one);
+    let my_country = return_it();
 }
