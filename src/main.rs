d0abf63ac3a6f0de636eeb38712b6df79eea22d6
YoungHa Kim <ytok1108@aol.com>
2021-12-10 22:18:07 +0900

    f64 i32 as i32 / Floats

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let my_number = 9_u8;
-    let other_number = 10_000_000_000u64;
-    println!("{}  {}", my_number, other_number);
+    let my_number = 9.; // f64
+    let other_number = 9; // i32
+    println!("{}", my_number as i32 + other_number);
 }
