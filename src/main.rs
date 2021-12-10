cf3ba6e171a70553b9c3bd5e396eab72ae7aace7
YoungHa Kim <ytok1108@aol.com>
2021-12-10 20:50:49 +0900

    #[warn(non_upper_case_globals)]

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,7 @@
+#[allow(non_upper_case_globals)]
+const high_score: i32 = 20;
+// static
+
 fn main() {
-    // method = .Function
-    // reallocation
-
-    // String
-    // .capacity
-    // .push
-    // .push_str
-    // .pop
-    // .with_capacity
-    // allocation
-
-    let mut my_name = String::with_capacity(26);
-    println!(
-        "Length is {} and capacity is : {}",
-        my_name.len(),
-        my_name.capacity()
-    );
-    my_name.push_str("David!");
-    println!(
-        "Length is {} and capacity is : {}",
-        my_name.len(),
-        my_name.capacity()
-    );
-    my_name.push('!');
-    my_name.push_str(" and I live in Seoul");
-    println!(
-        "Length is {} and capacity is : {}",
-        my_name.len(),
-        my_name.capacity()
-    my_name.push('a');
-    println!(
-        "Length is {} and capacity is : {}",
-        my_name.len(),
-        my_name.capacity()
-    );
+    let x = 8; //'let' binding: i32
 }
