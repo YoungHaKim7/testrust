037902fa0b6e3d5ce93d9c79a1a9ac54469eab04
YoungHa Kim <ytok1108@aol.com>
2021-12-10 22:54:03 +0900

    empty tuple Debug print

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@
 
 fn empty_tuple() {}
 
+// Display {}
+// Debug print 디버그는 type이름과 properties 프로그램을 보기 위한 print
 fn main() {
     let tuple = empty_tuple();
-    tuple
+    println!("{:?}", tuple);
 }
