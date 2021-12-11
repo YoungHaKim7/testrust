ee9f5884d8ebfff6085914dc4c85d2703a4efac8
YoungHa Kim <ytok1108@aol.com>
2021-12-12 00:49:59 +0900

    Eng. Array [1..5]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 // Collection types: arrays, vectors (Vecs)
 // ARRAYS     [ ] all the same type
 fn main() {
-    let my_array = ["My", "array"];
-    println!("{}", my_array[1]);
+    let array_of_ten = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    let three_to_five = &array_of_ten[2..5];
+    let start_at_two = &array_of_ten[1..5];
+    let end_at_five = &array_of_ten[..5];
+    let everything = &array_of_ten[..];
+
+    println!(
+        " Three to five: {:?},\n start at two: {:?},\n end at five: {:?},\n everything: {:?}",
+        three_to_five, start_at_two, end_at_five, everything
+    );
 }
