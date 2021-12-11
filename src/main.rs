d0b928475e356c14cd2ddf6359daffe0f501cd9c
YoungHa Kim <ytok1108@aol.com>
2021-12-11 22:42:53 +0900

    C style loops

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
-// uninitialized variable
-// control flow
-
-// possibly uninitialized = maybe doesn't have a value yet
+fn loop_then_return(mut counter: i32) -> i32 {
+    loop {
+        counter += 1;
+        if counter % 50 == 0 {
+            // 102 / 50  2 remainder 2
+            break;
+        }
+    }
+    counter
+}
 
 fn main() {
-    let my_number = {
+    let my_number;
+
+    {
         // 복잡한 코딩 후
-        let x = 9;
-        x + 9
-    };
+        let x = loop_then_return(43);
+        my_number = x
+    }
 
     println!("{}", my_number);
 }
