f0639d5159c4fd10fab42cfaf0240e04c999e2ef
YoungHa Kim <ytok1108@aol.com>
2021-12-11 20:01:12 +0900

    OK reference dereference shadowing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    // shadowing
-    let country = "대한민국";
-    let country_ref = &country;
-    let country = 8;
-    println!("{}, {}", country_ref, country);
+    let mut number = 10;
+    let number_change = &mut number;
+    *number_change += 10;
+    let number_ref = &number;
+    println!("{}", number_ref);
 }
