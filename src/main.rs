9b5f9b5090759f663f840151f4d2a49264e7e9f0
YoungHa Kim <ytok1108@aol.com>
2021-12-11 22:33:58 +0900

    possibly uninitialized = maybe doesn't have a value yet

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,10 @@
-// It's trivial to copy the bytes
+// uninitialized variable
+// control flow
 
-// Ownership and copy types
-fn prints_number(number: i32) {
-    println!("{}", number);
-}
+// possibly uninitialized = maybe doesn't have a value yet
 
-fn print_string(input: String) {
-    println!("{}", input);
-}
-
-// copy - copy types
-// clone - non-copy types
 fn main() {
-    let my_number = 8;
-    prints_number(my_number);
-    prints_number(my_number);
+    let my_number;
 
-    let my_country = "Austria".to_string();
-    print_string(my_country.clone());
-    print_string(my_country);
-    print_string(my_country);
+    println!("{}", my_number);
 }
