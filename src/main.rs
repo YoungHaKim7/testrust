92c2c38ee68ff478a9758dd5ee32581f14110243
YoungHa Kim <ytok1108@aol.com>
2021-12-11 22:25:21 +0900

    Rust_ copy and clone

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,18 @@ fn prints_number(number: i32) {
     println!("{}", number);
 }
 
+fn print_string(input: String) {
+    println!("{}", input);
+}
+
+// copy - copy types
+// clone - non-copy types
 fn main() {
     let my_number = 8;
     prints_number(my_number);
     prints_number(my_number);
+
+    let my_country = "Austria".to_string();
+    print_string(my_country.clone());
+    print_string(my_country);
 }
