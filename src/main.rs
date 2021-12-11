152d033f7ac6a425167a1fb244dfe0f58ac73638
YoungHa Kim <ytok1108@aol.com>
2021-12-11 20:18:13 +0900

    rust_ excellent code__again print

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 // OWNERSHIP
 // move semantics
 
-fn print_country(country_name: String) -> String {
+fn print_country(country_name: &String) {
     println!("My country is {}", country_name);
-    country_name
 }
 
 fn main() {
-    let mut country = "대 한 민 국! ".to_string();
-    country = print_country(country);
-    country = print_country(country);
-    country = print_country(country);
+    let country = "대 한 민 국! ".to_string();
+    print_country(&country);
+    print_country(&country);
+    print_country(&country);
 }
