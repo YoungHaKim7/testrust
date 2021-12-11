c95adb41027b8e4323cd27277d1feab96e1e07b3
YoungHa Kim <ytok1108@aol.com>
2021-12-11 22:37:51 +0900

    initialize the variables of the rust style.

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@
 // possibly uninitialized = maybe doesn't have a value yet
 
 fn main() {
-    let my_number;
-
-    {
-        my_number = 9;
-    }
+    let my_number = {
+        // 복잡한 코딩 후
+        let x = 9;
+        x + 9
+    };
 
     println!("{}", my_number);
 }
