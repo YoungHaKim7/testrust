2581bf8b21b3e31af2c93ee3697fdac7b20ddccf
YoungHa Kim <ytok1108@aol.com>
2021-12-12 20:02:12 +0900

    use _  Tuple _

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@
 
 fn main() {
     let str_vec = vec!["one", "two", "three"];
-    let (a, b, c) = (str_vec[0], str_vec[1], str_vec[2]);
-    println!("{}", b);
+    let (a, b, _) = (str_vec[0], str_vec[1], str_vec[2]);
+    println!("{}, {}", a, b);
 }
