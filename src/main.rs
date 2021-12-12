3121f46c0d65b2659c73fe7acdfd72443498d433
YoungHa Kim <ytok1108@aol.com>
2021-12-12 18:05:34 +0900

    Eng22. Vecs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 // Vecs
 
 fn main() {
-    let name1 = String::from("Windy");
-    let name2 = String::from("Gomesy");
-    let name3 = String::from("MB");
+    let my_vec: Vec<String> = Vec::new();
 
-    let mut my_vec = Vec::new();
+    let my_vec = vec![7, 8, 9, 10];
+    // let name1 = String::from("Windy");
+    // let name2 = String::from("Gomesy");
+    // let name3 = String::from("MB");
 
-    my_vec.push(name1);
-    my_vec.push(name2);
-    my_vec.push(name3);
+    // let mut my_vec = Vec::new();
 
-    println!("{:?}", my_vec);
+    // my_vec.push(name1);
+    // my_vec.push(name2);
+    // my_vec.push(name3);
+
+    // println!("{:?}", my_vec);
 }
