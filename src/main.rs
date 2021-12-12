efe5fa6f5726101d2264962bcc3ef064998cbabd
YoungHa Kim <ytok1108@aol.com>
2021-12-12 20:42:25 +0900

    Different ways to use matches.

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@
 
 fn main() {
     let my_number: u8 = 5;
-    match my_number {
-        8 => println!("It's an eight"),
-        10 => println!("It's a ten"),
-        _ => println!("It's something else"),
-    }
+    let something = match my_number {
+        8 => 10,
+        10 => 200,
+        _ => 55,
+    };
+
+    println!("{}", something);
 }
