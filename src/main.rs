a51bf3a2f913c369394074d185efded230c0f3a2
YoungHa Kim <ytok1108@aol.com>
2021-12-12 20:23:52 +0900

    if , else if, else

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 // CONTROL FLOW
 
 fn main() {
-    let my_number = 9;
-    if my_number == 9 {
+    let my_number = 17;
+    if my_number == 7 {
         println!("It's seven");
+    } else if my_number == 6 {
+        println!("It's six")
+    } else {
+        println!("It's something else");
     }
 }
