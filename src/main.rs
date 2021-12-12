4bc2ab74474109cf57f3fd6a9eea32cb19b2c3f9
YoungHa Kim <ytok1108@aol.com>
2021-12-12 16:58:59 +0900

    Some None Option enum

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,6 @@ fn main() {
     let array = [
         "1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월",
     ]; // indexing
-    println!("{:?}", array.get(3)); // first
+    println!("{:?}", array.get(12)); // first
+                                     // Some None Option enum
 }
