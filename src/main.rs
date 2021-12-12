ab4fc7f231a4847b9f5a4fa169b3a3706708dc1c
YoungHa Kim <ytok1108@aol.com>
2021-12-12 18:16:56 +0900

    Vec _Capacity()

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,6 @@
 // Vecs
 
 fn main() {
-    let vec_of_ten = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    // Everthing is the same as above except we added vec!.
-    let three_to_five = &vec_of_ten[2..5];
-    let start_at_two = &vec_of_ten[1..];
-    let end_at_five = &vec_of_ten[..5];
-    let everything = &vec_of_ten[..];
-
-    println!(
-        " Three to five: {:?},\n start at two: {:?},\n end at five: {:?},\n everything: {:?}",
-        three_to_five, start_at_two, end_at_five, everything
-    );
+    let mut num_vec: Vec<char> = Vec::new();
+    println!("{}", num_vec.capacity()); // 0 elements: prints 0
 }
