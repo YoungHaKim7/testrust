f8a5ad89183f0cbf6c5cba29385f1074dc0ef7c6
YoungHa Kim <ytok1108@aol.com>
2021-12-12 16:57:21 +0900

    How to ask what's in the array?__array.get()

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,5 @@ fn main() {
     let array = [
         "1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월",
     ]; // indexing
-    println!("{:?}", array[1]); // first
+    println!("{:?}", array.get(3)); // first
 }
