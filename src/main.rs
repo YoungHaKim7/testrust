71aebf4a2246db8e05fd71c1b92e0bf7ddda6a54
YoungHa Kim <ytok1108@aol.com>
2021-12-12 18:22:23 +0900

    Eng21 Vec___vec.push__for i in 0..10000

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,15 @@
 fn main() {
     let mut num_vec: Vec<char> = Vec::new();
     println!("{}", num_vec.capacity()); // 0 elements: prints 0
+    num_vec.push('a');
+    println!("{}", num_vec.capacity());
+    num_vec.push('a');
+    num_vec.push('a');
+    num_vec.push('a');
+    num_vec.push('a');
+    println!("{}", num_vec.capacity());
+    for i in 0..10000 {
+        num_vec.push('a');
+    }
+    println!("{}", num_vec.capacity());
 }
