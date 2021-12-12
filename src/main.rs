00edeb2307ec2e72992518c8bbf835f37f0cd641
YoungHa Kim <ytok1108@aol.com>
2021-12-12 18:28:36 +0900

    Eng22Vecs___into()

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
-// Vecs
+// Vecs ..... into()
 
 fn main() {
-    let mut num_vec: Vec<char> = Vec::with_capacity(5);
-    println!("{}", num_vec.capacity()); // 0 elements: prints 0
-    num_vec.push('a');
-    println!("{}", num_vec.capacity());
-    num_vec.push('a');
-    num_vec.push('a');
-    num_vec.push('a');
-    num_vec.push('a');
-    println!("{}", num_vec.capacity());
+    let my_vec: Vec<u8> = [1, 2, 3].into();
+    let my_vec2: Vec<_> = [9, 0, 10].into(); // Vec<_> means "choose the Vec type for me"
+                                             // Rust will choose Vec<i32>
 }
