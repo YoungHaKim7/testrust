6216d39681bfc8edbff2d2de1c3930e2dd14c4bf
YoungHa Kim <ytok1108@aol.com>
2021-12-12 18:11:03 +0900

    Eng.22: Vecs

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,15 @@
 // Vecs
 
 fn main() {
-    let my_vec: Vec<String> = Vec::new();
+    let vec_of_ten = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    // Everthing is the same as above except we added vec!.
+    let three_to_five = &vec_of_ten[2..5];
+    let start_at_two = &vec_of_ten[1..];
+    let end_at_five = &vec_of_ten[..5];
+    let everything = &vec_of_ten[..];
 
-    let my_vec = vec![7, 8, 9, 10];
-    // let name1 = String::from("Windy");
-    // let name2 = String::from("Gomesy");
-    // let name3 = String::from("MB");
-
-    // let mut my_vec = Vec::new();
-
-    // my_vec.push(name1);
-    // my_vec.push(name2);
-    // my_vec.push(name3);
-
-    // println!("{:?}", my_vec);
+    println!(
+        " Three to five: {:?},\n start at two: {:?},\n end at five: {:?},\n everything: {:?}",
+        three_to_five, start_at_two, end_at_five, everything
+    );
 }
