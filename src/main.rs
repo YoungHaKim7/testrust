56f93e0ff3ae2a3ef4e15fe12a823c780b1c02e0
YoungHa Kim <ytok1108@aol.com>
2021-12-12 20:35:01 +0900

    If __Its a positive odd number"
    "
    
    ""
    "
    
    __
    "

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 // CONTROL FLOW
 
 fn main() {
-    let my_number = 17;
-    if my_number == 7 {
-        println!("It's seven");
+    let my_number = 5;
+    if my_number % 2 == 1 && my_number > 0 {
+        // % 2 means the number that ramains after diving by two
+        println!("It's a positive odd number");
     } else if my_number == 6 {
         println!("It's six")
     } else {
-        println!("It's something else");
+        println!("It's a different number")
     }
 }
