1b6f891e6240b969d7c5f79509d701af55e6656a
YoungHa Kim <ytok1108@aol.com>
2021-12-13 22:41:47 +0900

    speed (array > Vec)__Vec<u8>__Vec<String>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-// Slices
-// Vecs
-
-// dynamically sized type
+// Array(가장 빠름), Vec(약간 느리지만 사용성 높고 편리하게 쓸수 있음)
+// &str, String
+// Vec<u8>
+// Vec<String>
 
 fn main() {
     let seasons = ["봄", "여름", "가을", "겨울", "봄", "여름", "가을", "겨울"];
