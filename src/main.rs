d9a81525dd3528c9ccbfdd4ebd8bc87e82af752c
YoungHa Kim <ytok1108@aol.com>
2021-12-13 22:36:41 +0900

    Print from the fourth one.

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,5 @@
 
 fn main() {
     let seasons = ["봄", "여름", "가을", "겨울", "봄", "여름", "가을", "겨울"];
-    println!("{:?}", &seasons[..]); // up to and including
+    println!("{:?}", &seasons[3..]); // up to and including
 }
