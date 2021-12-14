975394d0c41e230806a6852894ab2812089a112f
YoungHa Kim <ytok1108@aol.com>
2021-12-14 22:25:49 +0900

    Vec<String> __Initializing variables.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-// Array(가장 빠름), Vec(약간 느리지만 사용성 높고 편리하게 쓸수 있음)
-// &str, String
-// Vec<u8>
 // Vec<String>
 
 fn main() {
-    let seasons = ["봄", "여름", "가을", "겨울", "봄", "여름", "가을", "겨울"];
-    println!("{:?}", &seasons[..=4]); // up to and including
+    let my_string = String::new();
+    let my_vec = Vec::new();
 }
