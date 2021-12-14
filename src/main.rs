df1dbe956cb6f21810e7b26982efe852ecb2183a
YoungHa Kim <ytok1108@aol.com>
2021-12-14 22:35:20 +0900

    How to use a vector.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 // Vec<String>
+// Vec<u8>
+// T = some type
+// generics
 
 fn main() {
-    let my_string = String::new();
-    let my_vec = Vec::new();
+    let name1 = String::from("Windy");
+    let name2 = String::from("Gomesy");
+
+    let mut my_vec = Vec::new();
+
+    my_vec.push(name1);
+    my_vec.push(name2);
+
+    println!("My cats are {:?}", my_vec);
 }
