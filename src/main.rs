6e89255f7b2620749466a653c59c2b53c367e8ff
YoungHa Kim <ytok1108@aol.com>
2021-12-14 22:53:53 +0900

    my_vec.capacity

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,11 @@ fn main() {
     let name2 = String::from("Gomesy");
 
     let mut my_vec = Vec::new();
-
+    println!("Space for my_vec: {}", my_vec.capacity());
     my_vec.push(name1);
+    println!("Space for my_vec: {}", my_vec.capacity());
     my_vec.push(name2);
+    println!("Space for my_vec: {}", my_vec.capacity());
 
     println!("My cats are {:?}", my_vec);
 }
