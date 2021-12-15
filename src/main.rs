e7d588c10f28a950e8f61298338cb9b9c61872c2
YoungHa Kim <ytok1108@aol.com>
2021-12-15 20:19:55 +0900

    Put an array in a vector.

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,5 @@
 // From, Into
 
 fn main() {
-    let my_name = String::from("Dave MacLeod");
-    let my_city: String = "Seoul".into(); // &str
-
-    println!("{}", my_city);
+    let my_vec = Vec::from([8, 9, 10]); // [i32; 3]
 }
