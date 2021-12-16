07846d421ba6bdefd1946b4e5e5b8721522b3d98
YoungHa Kim <ytok1108@aol.com>
2021-12-16 21:46:08 +0900

    Destructuring

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 // Vec<(&str, i32)>
+// Destructuring
+// Structure
 
 fn main() {
-    let my_vec = vec![("Hey", 9), ("Hello there", 8972983)];
+    let str_tuple = ("one", "two", "three");
+
+    let (a, b, c) = str_tuple;
+
+    println!("Item a is : {}", a);
 }
