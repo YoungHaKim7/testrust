1041e04bacf60118149844b00f085d8624624dfd
YoungHa Kim <ytok1108@aol.com>
2021-12-16 21:37:17 +0900

    tuple__& print

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
-// tuples
-// Vec<String>
-
 fn main() {
-    let x = ();
+    // array[0]   vec[0]
+    // struct
+    let random_tuple = ("Here is a name", 8, vec!['a'], 'b', [8, 9, 10], 7.7);
+    println!(
+        "Inside the tuple is : 
+First item: {}
+Second item: {}
+Third item: {:?}:
+Foutrh item: {}
+Fifth item: {:?}
+Sixth item: {}",
+        random_tuple.0,
+        random_tuple.1,
+        random_tuple.2,
+        random_tuple.3,
+        random_tuple.4,
+        random_tuple.5,
+    )
 }
