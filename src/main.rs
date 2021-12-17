6b5284e5ad21eec7b50d6f2a05317e958e925d35
YoungHa Kim <ytok1108@aol.com>
2021-12-17 22:43:00 +0900

    print ___ tuple struct

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,15 @@ struct FileDirectory; // Unit struct
 
 struct Colour(u8, u8, u8); // tuple struct
 
+/*struct SizeAndColour {
+    size: u32,
+    colour: Colour,
+}
+ // named struct
+ */
+
 fn main() {
     let my_directory = FileDirectory;
     let some_colours = Colour(50, 60, 0);
+    println!("The first colour is : {}", some_colours.0);
 }
