5b9fbd45b233292ad8315dc2bd2312bbfaff9a4a
YoungHa Kim <ytok1108@aol.com>
2021-12-17 22:20:24 +0900

    Control flow part2___@ match__

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
-fn main() {
-    let my_number = 10;
-
-    if my_number == 10 {
-        let my_number = 8;
-    } else {
-        let my_variable = "something else";
+fn match_number(input: i32) {
+    match input {
+        number @ 4 => println!(
+            "{} is an unlucky number in China: multiplied by 2 is {}",
+            number,
+            number * 2
+        ),
+        number @ 13 => println!("{} is a lucky number in Italy. In bocca al lupo", number),
+        _ => println!("Looks like a normal number"),
     }
+}
+
+fn main() {
+    match_number(50);
+    match_number(13);
+    match_number(4);
 }
