2665fcb010d30efab3dac23a41283b7ae08b6e84
YoungHa Kim <ytok1108@aol.com>
2021-12-17 21:32:05 +0900

    Control flow part2__shadowing

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,9 @@
-fn match_colours(rbg: (i32, i32, i32)) {
-    match rbg {
-        (r, _, _) if r < 10 => println!("Not much red"),
-        (_, b, _) if b < 10 => println!("Not much blue"),
-        (_, _, g) if g < 10 => println!("Not much green"),
-        _ => println!("Each colour has at least ten"),
-    }
-}
-
 fn main() {
-    let first = (200, 0, 0);
-    let second = (50, 50, 50);
-    let third = (200, 50, 0);
+    let my_number = 10;
 
-    match_colours(first);
-    match_colours(second);
-    match_colours(third);
+    if my_number == 10 {
+        let my_number = 8;
+    } else {
+        let my_variable = "something else";
+    }
 }
