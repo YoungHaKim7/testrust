debeecb545d44ffd31b6d5b60ea29015b5afb023
YoungHa Kim <ytok1108@aol.com>
2021-12-17 19:13:31 +0900

    Match is most often used in rust.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 // Control Flow and match
 
 fn main() {
+    // match
     let my_number = 5;
-    let my_second_number = 10;
-    if my_number == 5 && my_second_number == 10 {
-        println!("They both match.");
-    } else if my_number == 6 {
-        // && and  /  || or
-        println!("It's six");
-    } else {
-        println!("It's a different number");
+
+    match my_number {
+        // switch
+        0 => println!("It's a zero"), // => fat arrow
+        1 => println!("It's a one"),
+        _ => println!("It's a different number"), //  _ "I don't care"  "anything else"
     }
 }
