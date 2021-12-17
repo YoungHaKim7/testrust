1d86d81629148e3511be6661e32f71c64da6cc0a
YoungHa Kim <ytok1108@aol.com>
2021-12-17 22:39:05 +0900

    Structs__Unit struct & tuple struct

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,10 @@
-fn match_number(input: i32) {
-    match input {
-        number @ 4 => println!(
-            "{} is an unlucky number in China: multiplied by 2 is {}",
-            number,
-            number * 2
-        ),
-        number @ 13 => println!("{} is a lucky number in Italy. In bocca al lupo", number),
-        _ => println!("Looks like a normal number"),
-    }
-}
+// STRUCTS =   your own type, your own data structure
+
+struct FileDirectory; // Unit struct
+
+struct Colour(u8, u8, u8); // tuple struct
 
 fn main() {
-    match_number(50);
-    match_number(13);
-    match_number(4);
+    let my_directory = FileDirectory;
+    let some_colours = Colour(50, 60, 0);
 }
