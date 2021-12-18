1798e1f4aa59fa45a39d8542b1104e9236f6e4c1
YoungHa Kim <ytok1108@aol.com>
2021-12-18 21:11:00 +0900

    Match statement3

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
         (c, m) if married == false => println!("Not married with {}", children),
         // false 표시는 !married
         // true 표시는 marred로 표현할 수 있다.!!
-        (c, m) if c == 0 && married => println!("Married but with no children"),
+        (c, m) if c == 0 && m => println!("Married but with no children"),
         _ => println!("Some other type of marriage and children combination"),
     }
 }
