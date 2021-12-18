913a868590f991a6f5ce81e833efa44a61f9a0fb
YoungHa Kim <ytok1108@aol.com>
2021-12-18 21:09:40 +0900

    Match statements__true_married__/false_marred

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 fn main() {
-    let sky = "cloudy"; //&str
-    let temperature = "warm";
+    let children = 5;
+    let married = true;
 
-    match (sky, temperature) {
-        ("cloudy", "cold") => println!("It's not very nice today"),
-        ("clear", "warm") => println!("It's a nice day"),
-        ("cloudy", _) => println!("Cloudy and something else"),
-        _ => println!("Not sure what the weather is. "),
+    match (children, married) {
+        (c, m) if married == false => println!("Not married with {}", children),
+        // false 표시는 !married
+        // true 표시는 marred로 표현할 수 있다.!!
+        (c, m) if c == 0 && married => println!("Married but with no children"),
+        _ => println!("Some other type of marriage and children combination"),
     }
 }
