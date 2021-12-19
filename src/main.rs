3a278bfe934c96b41b0234aa40202fb35964111a
YoungHa Kim <ytok1108@aol.com>
2021-12-19 17:28:12 +0900

    The code does not work because of the mixture of numbers and characters

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,7 @@
-// rgb
-
-fn match_colours(rbg: (u32, u32, u32)) {
-    match rbg {
-        (r, _, _) if r < 10 => println!("Not much red"),
-        (_, b, _) if b < 10 => println!("Not much blue"),
-        (_, _, g) if g < 10 => println!("Not much green"),
-        _ => println!("Every colour has at least 10"),
-    }
-}
-
 fn main() {
-    let first = (200, 0, 0);
-    let second = (50, 50, 50);
-    let third = (200, 50, 0);
-
-    match_colours(first);
-    match_colours(second);
-    match_colours(third);
+    let my_number = 10;
+    let some_variable = match my_number {
+        10 => 8,
+        _ => "Not ten",
+    };
 }
