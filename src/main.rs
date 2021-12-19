99b9d576c376e791d44f71289f513f9d941a0020
YoungHa Kim <ytok1108@aol.com>
2021-12-19 17:31:58 +0900

    If it's the same type, the code works.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
     //
     let my_number = 8;
-    let some_variable = if my_number == 10 { 8 } else { "Something else" };
+    let some_variable = if my_number == 10 { 8 } else { 10 };
 }
