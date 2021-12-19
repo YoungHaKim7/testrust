ff8e06e82b39ddb5498e7334c8fd97ca96edf571
YoungHa Kim <ytok1108@aol.com>
2021-12-19 17:42:57 +0900

    number @__@__Kor31_More match statements

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 fn match_number(input: i32) {
     match input {
-        number => println!("It's the number {}", number), // number라는 아무런거나 맞는거 변수 넣는다.
         number @ 0..=10 => println!("It's between 0 and 10. It's the number {}", number),
         _ => println!("It's greater than ten"),
     }
 }
 
-fn main() {}
+fn main() {
+    match_number(10);
+    match_number(2);
+    match_number(100);
+}
