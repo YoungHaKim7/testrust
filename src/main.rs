1471732accb8a59446d6d974a4a80c6d21bc1143
YoungHa Kim <ytok1108@aol.com>
2021-12-19 17:40:44 +0900

    - matches any value

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-fn main() {
-    //
-    let my_number = 8;
-    let some_variable = if my_number == 10 { 8 } else { 10 };
+fn match_number(input: i32) {
+    match input {
+        number => println!("It's the number {}", number), // number라는 아무런거나 맞는거 변수 넣는다.
+        number @ 0..=10 => println!("It's between 0 and 10. It's the number {}", number),
+        _ => println!("It's greater than ten"),
+    }
 }
+
+fn main() {}
