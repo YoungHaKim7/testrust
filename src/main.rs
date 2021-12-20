3036b6c91d07109aa6abe0579c22bd6f77e435a3
YoungHa Kim <ytok1108@aol.com>
2021-12-20 22:04:27 +0900

    Named structure, which is used most frequently in structures.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,3 @@
-// struct = and 이거 들어있고 들어 있고 할때!!
-// Rust에는 struct가 3가지가 있다.
-//      1. unit struct - 아무것도 없고 이름만 씀 -"byte는 0이다."
-//      2. tuple struct
-//      3. named struct - 가장 많이 쓰고 제일 편리함.!!
-// enum = or 선택해서 쓸 때 씀.... struct와 약간 비슷
-//unit struct
-struct FileDirectory;
-
-// tuple struct
-#[derive(Debug)] //attribute
-struct Colour(u8, u8, u8);
-
 // named struct
 struct Country {
     population: u32,
@@ -19,5 +6,9 @@ struct Country {
 }
 
 fn main() {
-    let 
+    let canada = Country {
+        population: 35_000_000,
+        capital: "Ottawa".to_string(),
+        leader_name: "Justin Trudeau".to_string(),
+    };
 }
