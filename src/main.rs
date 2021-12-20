03516edf2b147864f822f1e4a674a9377150bb30
YoungHa Kim <ytok1108@aol.com>
2021-12-20 20:58:44 +0900

    Unit Struct(The byte of the structure is 0 byte.)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 // struct = and 이거 들어있고 들어 있고 할때!!
 // Rust에는 struct가 3가지가 있다.
-//      1. unit struct - 아무것도 없고 이름만 씀
+//      1. unit struct - 아무것도 없고 이름만 씀 -"byte는 0이다."
 //      2.  struct
 //      3.  struct
 // enum = or 선택해서 쓸 때 씀.... struct와 약간 비슷
@@ -13,5 +13,5 @@ fn takes_file_directory(input: FileDirectory) {
 
 fn main() {
     let x = FileDirectory;
-    takes_file_directory(x);
+    println!("The size is {}", std::mem::size_of_val(&x));
 }
