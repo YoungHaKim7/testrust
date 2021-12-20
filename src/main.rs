fe532734e6fee6e3499278b1ecb47f1f17c72360
YoungHa Kim <ytok1108@aol.com>
2021-12-20 21:43:20 +0900

    struct 3 ___

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,22 @@
 // Rust에는 struct가 3가지가 있다.
 //      1. unit struct - 아무것도 없고 이름만 씀 -"byte는 0이다."
 //      2. tuple struct
-//      3.  struct
+//      3. named struct - 가장 많이 쓰고 제일 편리함.!!
 // enum = or 선택해서 쓸 때 씀.... struct와 약간 비슷
+//unit struct
+struct FileDirectory;
 
-// trait
 // tuple struct
 #[derive(Debug)] //attribute
 struct Colour(u8, u8, u8);
 
+// named struct
+struct Country {
+    population: u32,
+    capital: String,
+    leader_name: String,
+}
+
 fn main() {
-    let my_colour = Colour(20, 50, 100);
-    println!("The second colour is {:?}", my_colour);
+    let 
 }
