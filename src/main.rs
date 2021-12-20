0bb1dd039801654deb928f92801e6f27780cadb6
YoungHa Kim <ytok1108@aol.com>
2021-12-20 21:36:05 +0900

    tuple struct___result : The The second colour is 50

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,14 @@
 // struct = and 이거 들어있고 들어 있고 할때!!
 // Rust에는 struct가 3가지가 있다.
 //      1. unit struct - 아무것도 없고 이름만 씀 -"byte는 0이다."
-//      2.  struct
+//      2. tuple struct
 //      3.  struct
 // enum = or 선택해서 쓸 때 씀.... struct와 약간 비슷
 
-struct FileDirectory;
-
-fn takes_file_directory(input: FileDirectory) {
-    println!("I got a file directory");
-}
+// tuple struct
+struct Colour(u8, u8, u8);
 
 fn main() {
-    let x = FileDirectory;
-    println!("The size is {}", std::mem::size_of_val(&x));
+    let my_colour = Colour(20, 50, 100);
+    println!("The second colour is {}", my_colour.1);
 }
