292e289d1f7ba02c646ded7257203845ed37d387
YoungHa Kim <ytok1108@aol.com>
2021-12-20 20:55:59 +0900

    unit struct__There are three types of structures.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
-fn match_number(input: i32) {
-    match input {
-        number @ 0..=10 => println!("It's between 0 and 10. It's the number {}", number),
-        _ => println!("It's greater than ten"),
-    }
+// struct = and 이거 들어있고 들어 있고 할때!!
+// Rust에는 struct가 3가지가 있다.
+//      1. unit struct - 아무것도 없고 이름만 씀
+//      2.  struct
+//      3.  struct
+// enum = or 선택해서 쓸 때 씀.... struct와 약간 비슷
+
+struct FileDirectory;
+
+fn takes_file_directory(input: FileDirectory) {
+    println!("I got a file directory");
 }
 
 fn main() {
-    match_number(10);
-    match_number(2);
-    match_number(100);
+    let x = FileDirectory;
+    takes_file_directory(x);
 }
