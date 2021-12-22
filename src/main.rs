46eb2d31f5dcaa6ed9e4bee70aceb215063e6797
김영하 <ytok1108@aol.com>
2021-12-22 20:47:26 +0900

    Struct size

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::mem::size_of_val;
+
 #[derive(Debug)]
 struct Country {
     population: u32,
@@ -14,4 +16,6 @@ fn main() {
         capital,
         leader_name,
     };
+
+    println!("Country is {} bytes in size", size_of_val(&my_country));
 }
