456bd9e641482c1422df7bff1f2611b36cccfb51
김영하 <ytok1108@aol.com>
2021-12-22 21:28:49 +0900

    Struct size___Country is 22056 bytes in size

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,11 @@
 use std::mem::size_of_val;
 
-struct Numbers {
-    one: u8,
-    two: u8,
-    three: u8,
-    four: u32,
-}
-
 #[derive(Debug)]
 struct Country {
     population: u32,
     capital: String,
     leader_name: String,
+    all_populations: [u32; 5500],
 }
 
 fn main() {
@@ -22,16 +16,8 @@ fn main() {
         population,
         capital,
         leader_name,
+        all_populations: [500; 5500],
     };
 
     println!("Country is {} bytes in size", size_of_val(&my_country));
-
-    let numbers = Numbers {
-        one: 8,
-        two: 19,
-        three: 20,
-        four: 30,
-    };
-
-    println!("size is {}", size_of_val(&numbers));
 }
