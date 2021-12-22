72719cc0035641539acace7899e81f530161ba44
김영하 <ytok1108@aol.com>
2021-12-22 21:42:42 +0900

    Enums

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,9 @@
-use std::mem::size_of_val;
+// struct = and
+// enum = or     enum=enumeration e=from   number
 
-#[derive(Debug)]
-struct Country {
-    population: u32,
-    capital: String,
-    leader_name: String,
-    all_populations: [u32; 5500],
+enum ThingsInTheSky {
+    Sun,   //0
+    Stars, // 1
 }
 
-fn main() {
-    let population = 35_000_000;
-    let capital = "Ottawa".to_string();
-    let leader_name = "Justin Trudeau".to_string();
-    let my_country = Country {
-        population,
-        capital,
-        leader_name,
-        all_populations: [500; 5500],
-    };
-
-    println!("Country is {} bytes in size", size_of_val(&my_country));
-}
+fn main() {}
