5cf3df5b05d901aea629ef8432bfc5543eefc7b9
김영하 <ytok1108@aol.com>
2021-12-22 21:13:08 +0900

    struct size ____size is 3

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use std::mem::size_of_val;
 
+struct Numbers {
+    one: u8,
+    two: u8,
+    three: u8,
+}
+
 #[derive(Debug)]
 struct Country {
     population: u32,
@@ -18,4 +24,12 @@ fn main() {
     };
 
     println!("Country is {} bytes in size", size_of_val(&my_country));
+
+    let numbers = Numbers {
+        one: 8,
+        two: 19,
+        three: 20,
+    };
+
+    println!("size is {}", size_of_val(&numbers));
 }
