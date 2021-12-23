477013cacf408a8463178c473c8c40f643ffd7d9
김영하 <ytok1108@aol.com>
2021-12-23 21:22:21 +0900

    Enums 2 as u32

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,14 @@
-enum Mood {
-    Happy,
-    Sleepy,
-    NotBad,
-    Angry,
-}
-
-fn match_mood(mood: &Mood) -> i32 {
-    use Mood::*;
-
-    match mood {
-        Happy => 10,
-        Sleepy => 6,
-        NotBad => 7,
-        Angry => 2,
-    }
+enum Season {
+    Spring,
+    Summer,
+    Autumn,
+    Winter,
 }
 
 fn main() {
-    let my_mood = Mood::NotBad;
-    let happiness_level = match_mood(&my_mood);
-    println!("Out of 1 to 10, my happiness is {} ", happiness_level);
+    use Season::*;
+    let four_seasons = vec![Spring, Summer, Autumn, Winter]; //Vec<Season>
+    for season in four_seasons {
+        println!("The number is: {}", season as u32);
+    }
 }
