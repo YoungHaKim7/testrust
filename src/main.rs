1b0d9d02a970ad9d7e6e6ee119fda25a9582ae7d
김영하 <ytok1108@aol.com>
2021-12-24 20:01:45 +0900

    Enums3___vec

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,23 @@
-enum Star {
-    BrownDwarf = 10,
-    RedDwarf = 50,
-    YellowStar = 100,
-    RedGiant = 1000,
-    DeadStar,
+enum Number {
+    U32(u32),
+    I32(i32),
+}
+
+fn get_number(input: i32) -> Number {
+    let number = match input.is_positive() {
+        true => Number::U32(input as u32),
+        false => Number::I32(input),
+    };
+    number
 }
 
 fn main() {
-    use Star::*;
-    let starvec = vec![BrownDwarf, RedDwarf, YellowStar, RedDwarf, DeadStar];
+    let my_vec = vec![get_number(-800), get_number(8)]; // Vec<Number>
 
-    for star in starvec {
-        match star as u32 {
-            size if size <= 80 => println!("Not the biggest star: {}", size),
-            size if size >= 80 => println!("Pretty big star: {}", size),
-            _ => println!("Some other star"),
+    for item in my_vec {
+        match item {
+            Number::U32(number) => println!("It's a u32 with the value {}", number),
+            Number::I32(number) => println!("It's an i32 with the value {}", number),
         }
     }
-    println!("What about DeadStar? It is : {}", DeadStar as u32);
 }
