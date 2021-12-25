6acef4c411534aa17c054cf911309f278fbfe5df
김영하 <ytok1108@aol.com>
2021-12-25 16:07:37 +0900

    Loops

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,13 @@
-enum Number {
-    U32(u32),
-    I32(i32),
-}
-
-fn get_number(input: i32) -> Number {
-    match input.is_positive() {
-        true => Number::U32(input as u32),
-        false => Number::I32(input),
-    }
-}
+// loops
 
 fn main() {
-    let my_vec = vec![get_number(-800), get_number(8)]; // Vec<Number>
+    let mut counter = 0;
 
-    for item in my_vec {
-        match item {
-            Number::U32(number) => println!("It's a u32 with the value {}", number),
-            Number::I32(number) => println!("It's an i32 with the value {}", number),
+    loop {
+        counter += 1;
+        println!("The counter is: {}", counter);
+        if counter == 5 {
+            break;
         }
     }
 }
