eeedf4d08394a7f31e581993a24a5c7498bc257f
김영하 <ytok1108@aol.com>
2021-12-25 16:29:22 +0900

    loop 2X__comment

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,19 @@ fn main() {
     let mut counter2 = 0;
 
     'first_loop: loop {
+        // Give the first loop a name
         counter += 1;
         println!("The counter is: {}", counter);
         if counter == 9 {
+            // Starts a second loop inside this loop
             println!("Now entering the second loop");
 
             'secon_loop: loop {
+                // now we are inside 'second_loop
                 println!("The second counter is: {}", counter2);
                 counter2 += 1;
                 if counter2 == 3 {
-                    break 'first_loop;
+                    break 'first_loop; // Break out of 'first_loop so we can exit the program
                 }
             }
         }
