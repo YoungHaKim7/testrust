87347dd249d8a92a9bfd33155c951e9dacb8c0c9
김영하 <ytok1108@aol.com>
2021-12-26 16:48:50 +0900

    Return the desired value in the repeat statement.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 // loops
 
 fn main() {
-    for _number in 0..=3 {
-        // inclusive Range
-        println!("I don't need a number");
-    }
+    // break
+    let mut counter = 5;
+
+    let my_number = loop {
+        counter += 1;
+        if counter % 53 == 3 {
+            break counter;
+        }
+    };
+
+    println!("my_number is now : {}", my_number);
 }
