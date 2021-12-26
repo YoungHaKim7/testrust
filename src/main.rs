dbd1e72c40b9840a7a72559973e3ef6ef174c718
김영하 <ytok1108@aol.com>
2021-12-26 16:37:18 +0900

    for ___inclusive Range

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 // loops
 
 fn main() {
-    for number in 0..3 {
-        // exclusive Range
+    for number in 0..=3 {
+        // inclusive Range
         println!("The number is {}", number);
     }
 }
