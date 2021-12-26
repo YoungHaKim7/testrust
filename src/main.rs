86c24e3dbe726971e29f6116fc0d7ad1c9554b77
김영하 <ytok1108@aol.com>
2021-12-26 16:28:05 +0900

    Kor38__More loops__while

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,14 @@
 // loops
 
 fn main() {
-    let mut counter = 0;
-    let mut counter2 = 0;
+    let mut counter = 0; // set a counter to 0
 
-    'first_loop: loop {
-        // Give the first loop a name
-        counter += 1;
-        println!("The counter is: {}", counter);
-        if counter == 9 {
-            // Starts a second loop inside this loop
-            println!("Now entering the second loop");
-
-            'second_loop: loop {
-                // now we are inside 'second_loop
-                println!("The second counter is: {}", counter2);
-                counter2 += 1;
-                if counter2 == 3 {
-                    break 'first_loop; // Break out of 'first_loop so we can exit the program
-                }
-            }
+    while counter != 5 {
+        counter += 1; //increase the counter by 1
+        println!("The counter is now: {}", counter);
+        if counter == 5 {
+            // stop when counter == 5
+            break;
         }
     }
 }
