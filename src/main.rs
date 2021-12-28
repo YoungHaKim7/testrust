909de57519008705e802ad0e7a4e1af761daaf27
김영하 <ytok1108@aol.com>
2021-12-28 20:58:07 +0900

    change_to_Dog&Cat___More impl blocks

--- src/main.rs
+++ src/main.rs
@@ -27,10 +27,21 @@ impl Animal {
     fn print(&self) {
         println!("I am a : {:?}", self);
     }
+
+    fn change_to_dog(&mut self) {
+        self.animal_type = AnimalType::Dog;
+        println!("Changed to dog! Now I am: {:?}", self);
+    }
+
+    fn change_to_cat(&mut self) {
+        self.animal_type = AnimalType::Cat;
+        println!("Changed to cat! Now I am: {:?}", self);
+    }
 }
 
 fn main() {
-    let my_animal = Animal::new_dog(10);
+    let mut my_animal = Animal::new_dog(10);
     my_animal.print(); // syntactic sugar
-    Animal::print(&my_animal);
+    my_animal.change_to_cat();
+    my_animal.change_to_dog();
 }
