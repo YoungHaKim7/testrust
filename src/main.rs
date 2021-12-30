4420ff0fa921ff27a7034db17329cab8b6a4eb94
김영하 <ytok1108@aol.com>
2021-12-30 21:10:48 +0900

    Kor41_enum impl blocks__01

--- src/main.rs
+++ src/main.rs
@@ -11,32 +11,9 @@ enum AnimalType {
 }
 
 impl Animal {
-    fn new_old_cat() -> Self {
-        Self {
-            age: 15,
-            animal_type: AnimalType::Cat,
-        }
+    fn new(age: u8, animal_type: AnimalType) -> Self {
+        Self { age, animal_type }
     }
-}
-
-impl Animal {
-    fn new_cat(age: u8) -> Self {
-        Self {
-            age,
-            animal_type: AnimalType::Cat,
-        }
-    }
-    fn new_dog(age: u8) -> Self {
-        Self {
-            age,
-            animal_type: AnimalType::Dog,
-        }
-    }
-
-    fn print(&self) {
-        println!("I am a : {:?}", self);
-    }
-
     fn change_to_dog(&mut self) {
         self.animal_type = AnimalType::Dog;
         println!("Changed to dog! Now I am: {:?}", self);
@@ -49,10 +26,6 @@ impl Animal {
 }
 
 fn main() {
-    let mut my_animal = Animal::new_dog(10);
-    my_animal.print(); // syntactic sugar
-    my_animal.change_to_cat();
-    my_animal.change_to_dog();
-
-    let my_old_cat = Animal::new_old_cat();
+    // use AnimalType::*;
+    let my_cat = Animal::new(10, AnimalType::Cat);
 }
