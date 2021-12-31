fa5516bbdec4239c5084770eda77d8f890a4f59a
김영하 <ytok1108@aol.com>
2021-12-31 20:18:50 +0900

    Dereferencing and the dot operator___02

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,16 @@ struct Item {
     number: u8,
 }
 
+// . dot operator
+impl Item {
+    fn compare_number(&self, other_number: u8) {
+        println!("Are they equal? {}", self.number == other_number)
+    }
+}
+
 fn main() {
     let item = Item { number: 10 };
-    let reference = &item.number; // &u8
-    reference.dlkdslkjsjkldsjlkdsj();
+    let reference_item = &item;
+
+    item.compare_number(10);
 }
