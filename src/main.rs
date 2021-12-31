3ba4ef9b94a036efe1e10c33c7e7a2d130a3e22b
김영하 <ytok1108@aol.com>
2021-12-31 20:08:10 +0900

    can't compare {integer} with &{integer}

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,8 @@
-// destructuring
-
-struct Person {
-    name: String,
-    real_name: String,
-    height: u8,
-    happiness: bool,
-}
-
-#[derive(Debug)]
-struct Person2 {
-    name: String,
-    height: u8,
-}
-
-impl Person2 {
-    fn from_person(i: Person) -> Self {
-        let Person { name, height, .. } = i;
-
-        Self { name, height }
-    }
-}
+// references and the dot operator
 
 fn main() {
-    let papa_doc = Person {
-        name: "Papa Doc".to_string(),
-        real_name: "Clarence".to_string(),
-        height: 170,
-        happiness: false,
-    };
+    let my_number = 10; // i32
+    let references = &my_number; // &i32
 
-    let person2 = Person2::from_person(papa_doc);
-    println!("Person2 type is : {:?}", person2);
+    println!("Are they the same? {}", my_number == references);
 }
