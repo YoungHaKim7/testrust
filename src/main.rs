cdc42bfb75a6f18773a4897131a7fbe43db4c161
김영하 <ytok1108@aol.com>
2022-01-01 16:35:16 +0900

    Kor044__Generics__

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-fn print_and_give_item() -> i32 {
-    let number = 9;
-    println!("The number is : {}", number);
-    9
+fn give_thing<GenericType>(input: GenericType) -> GenericType {
+    //T
+    input
 }
 
-//generics(i32, String)   반대 개념은 concrete
-// 외국말 중 노래 중에 특성이 없고 재미없는 노래를 It's a little generic 이라고 함.
-
 fn main() {
-    let x = print_and_give_item();
+    let x = give_thing(String::from("Take this thing"));
+    let y = give_thing(9);
+    println!("{}", x);
+    println!("{}", y);
 }
