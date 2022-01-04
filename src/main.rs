521dd80e00e3a5f4bfda10cad0a74b858e4de58c
YoungHa Kim <ytok1108@aol.com>
2022-01-04 20:55:08 +0900

    thread __main__ panicked at _called _Option::unwrap()_ on a _None_ value_

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,11 @@ fn take_fifth(value: Vec<i32>) -> Option<i32> {
         Some(value[4]) // i32
     }
 }
-// wrap in an Option
+// .expect
 
 fn main() {
-    let new_vec = vec![1, 2, 3, 4, 5, 6];
+    let new_vec = vec![1, 2, 3];
     let index = take_fifth(new_vec); // Option<i32>
 
-    // Some(number)
-    if index.is_some() {
-        // bool
-        // Option<i32>
-        println!("I got a number: {}", index.unwrap());
-    }
+    index.unwrap();
 }
