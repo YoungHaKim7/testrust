f7a7f83413bcf45e72bd63604c4d175c797e802f
YoungHa Kim <ytok1108@aol.com>
2022-01-04 20:44:39 +0900

    More Option

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+// Option<T>
+
 fn take_fifth(value: Vec<i32>) -> Option<i32> {
     if value.len() < 5 {
         None
@@ -8,7 +10,11 @@ fn take_fifth(value: Vec<i32>) -> Option<i32> {
 // wrap in an Option
 
 fn main() {
-    let new_vec = vec![1, 2, 4, 7, 8, 10, 10];
-    let index = take_fifth(new_vec);
-    println!("{:?}", index);
+    let new_vec = vec![1, 2];
+    let index = take_fifth(new_vec); // Option<i32>
+
+    match index {
+        Some(number) => println!("I got a number: {}", number),
+        None => println!("There was nothing inside"),
+    }
 }
