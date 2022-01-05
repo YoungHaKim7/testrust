7bf11bde327d0c677e495375e2e8806d18528154
YoungHa Kim <ytok1108@aol.com>
2022-01-05 20:06:56 +0900

    Result__It's an e It's an error, guys

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
-fn check_error() -> Result<(), ()> {
-    Ok(())
+fn check_error(input: i32) -> Result<(), ()> {
+    if input % 2 == 0 {
+        Ok(())
+    } else {
+        Err(())
+    }
 }
+
+// .is_ok()
+// .is_err()
+
+// .is_some()
+// .is_none()
 
 // enum Option<T> {
 //     None,
@@ -13,5 +23,9 @@ fn check_error() -> Result<(), ()> {
 // }
 
 fn main() {
-    let variable = check_error();
+    if check_error(5).is_ok() {
+        println!("It's okay, guys!")
+    } else {
+        println!("It's an error, guys!")
+    }
 }
