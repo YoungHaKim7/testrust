4164c61793e8921859a9310d1029c2b392dbd918
YoungHa Kim <ytok1108@aol.com>
2022-01-05 19:59:12 +0900

    Kor048__Result__

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
-// Option<T>
-
-fn take_fifth(value: Vec<i32>) -> Option<i32> {
-    if value.len() < 5 {
-        None
-    } else {
-        Some(value[4]) // i32
-    }
+fn check_error() -> Result<(), ()> {
+    Ok(())
 }
-// .expect
+
+// enum Option<T> {
+//     None,
+//     Some(T),
+// }
+
+// enum Result<T, E> {
+//     Ok(T),
+//     Err(E),
+// }
 
 fn main() {
-    let new_vec = vec![1, 2, 3];
-    let index = take_fifth(new_vec); // Option<i32>
-
-    index.expect("Needed at least five items - make sure Vec has at least five");
+    let variable = check_error();
 }
