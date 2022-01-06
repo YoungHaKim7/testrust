ca82498e97bd85c0d2ff46ef152338a0e974af58
YoungHa Kim <ytok1108@aol.com>
2022-01-06 21:13:00 +0900

    More Result___parse()__&ParseIntError

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
-fn check_if_five(number: i32) -> Result<i32, String> {
-    match number {
-        5 => Ok(number),
-        _ => Err("Sorry, the number wasn't five.".to_string()),
-    }
+fn parse_number(number: &str) -> Result<i32, std::num::ParseIntError> {
+    number.parse()
 }
 
 fn main() {
-    let mut result_vec = Vec::new(); // Vec<Result<i32, String>>
+    let mut result_vec = vec![];
+    result_vec.push(parse_number("8"));
+    result_vec.push(parse_number("tnohunthoe"));
+    result_vec.push(parse_number("8"));
 
-    for number in 2..=7 {
-        result_vec.push(check_if_five(number));
+    for number in result_vec {
+        println!("{:?}", number);
     }
-
-    println!("{:#?}", result_vec);
 }
