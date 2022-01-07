7f46d8325b0197c200caae8a0e8c8111e70f4314
YoungHa Kim <ytok1108@aol.com>
2022-01-07 20:36:34 +0900

    if let__Some__

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 fn main() {
     let my_vec = vec![2, 3, 4];
 
+    // if let
     for index in 0..10 {
-        match my_vec.get(index) {
-            Some(number) => println!("The number is : {}", number),
-            None => {}
+        if let Some(number) = my_vec.get(index) {
+            println!("The number is : {}", number);
         }
     }
 }
+
+//         match my_vec.get(index) {
+//             Some(number) => println!("The number is : {}", number),
+//             None => {}
+//         }
+//     }
+// }
