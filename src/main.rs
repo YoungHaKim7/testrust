94b0189bdba9a51ca850a6efb064a69e59db1afb
YoungHa Kim <ytok1108@aol.com>
2022-01-09 17:12:02 +0900

    haspmap.insert__overwrite

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,12 @@ use std::collections::HashMap;
 
 fn main() {
     // Key -> Value
-    let canadian_cities = vec!["Calgary", "Vancouver", "Gimli"];
-    let german_cities = vec!["Karlsruhe", "Bad Doberan", "Bielefeld"];
+    let mut book_hashmap = HashMap::new();
 
-    let mut city_hashmap = HashMap::new();
+    book_hashmap.insert(1, "L'Allemagne Moderne");
+    book_hashmap.insert(1, "Le Petit Prince");
+    book_hashmap.insert(1, "섀도우 오브 유어 스마일");
+    book_hashmap.insert(1, "Eye of the World");
 
-    for city in canadian_cities {
-        city_hashmap.insert(city, "Canada");
-    }
-    for city in german_cities {
-        city_hashmap.insert(city, "Germany");
-    }
-
-    println!("{:?}", city_hashmap["Bielefeld"]); // 안전하지 X 운 좋게 스펠링이 맞아야 출력
-    println!("{:?}", city_hashmap.get("Bielefeld"));
-    println!("{:?}", city_hashmap.get("Bielefeldd")); // .unwrap()했다면 폭팔했지만 get으로 안전하게 출력함
+    println!("{:?}", book_hashmap.get(&1));
 }
