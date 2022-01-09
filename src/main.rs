d5e1f83f2dfb76a185e38ada0947fbb1ba855a70
YoungHa Kim <ytok1108@aol.com>
2022-01-09 17:15:19 +0900

    hashmap_get__is_none_if_else

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,10 @@ fn main() {
     let mut book_hashmap = HashMap::new();
 
     book_hashmap.insert(1, "L'Allemagne Moderne");
-    book_hashmap.insert(1, "Le Petit Prince");
-    book_hashmap.insert(1, "섀도우 오브 유어 스마일");
-    book_hashmap.insert(1, "Eye of the World");
 
-    println!("{:?}", book_hashmap.get(&1));
+    if book_hashmap.get(&1).is_none() {
+        book_hashmap.insert(1, "Le Petit Prince");
+    } else {
+        println!("Already got a book");
+    }
 }
