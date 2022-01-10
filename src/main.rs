aca3f2a1c4533248d4c23116b4ab0daab90efadb
YoungHa Kim <ytok1108@aol.com>
2022-01-10 20:51:49 +0900

    Kor053__HashMap entry

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 use std::collections::HashMap;
 
 fn main() {
-    // Key -> Value
+    let book_collection = vec![
+        "L'Allemagne Moderne",
+        "Le Petit Prince",
+        "섀도우 오브 유어 스마일",
+        "Eye of the World",
+        "Eye of the World",
+    ]; //Eye of the World appears twice
+
     let mut book_hashmap = HashMap::new();
+    for book in book_collection {
+        book_hashmap.entry(book).or_insert(true);
+    }
 
-    book_hashmap.insert(1, "L'Allemagne Moderne");
-
-    if let Some(book_name) = book_hashmap.get(&1) {
-        println!("Already got a book: {}", book_name);
-    } else {
-        book_hashmap.insert(1, "Le Petit Prince");
+    for (book, true_or_false) in book_hashmap {
+        println!("Do we have {}? {}", book, true_or_false);
     }
 }
