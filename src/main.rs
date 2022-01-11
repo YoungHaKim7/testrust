c2485477edce2aeceff3ea7f36bbebbf4c5b3e7a
YoungHa Kim <ytok1108@aol.com>
2022-01-11 20:45:35 +0900

    HashSet

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,25 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
+// HashSet  key값만 있고 Value값은 없다.!
+// BTreeSet
 
 fn main() {
-    let data = vec![
-        ("male", 9),
-        ("female", 5),
-        ("male", 0),
-        ("female", 6),
-        ("female", 5),
-        ("male", 10),
+    let many_numbers = vec![
+        94, 42, 59, 64, 32, 22, 38, 5, 59, 49, 15, 89, 74, 29, 14, 68, 82, 80, 56, 41, 36, 81, 66,
+        51, 58, 34, 59, 44, 19, 93, 28, 33, 18, 46, 61, 76, 14, 87, 84, 73, 71, 29, 94, 10, 35, 20,
+        35, 80, 8, 43, 79, 25, 60, 26, 11, 37, 94, 32, 90, 51, 11, 28, 76, 16, 63, 95, 13, 60, 59,
+        96, 95, 55, 92, 28, 3, 17, 91, 36, 20, 24, 0, 8, 82, 58, 93, 68, 54, 80, 56, 22, 67, 82,
+        58, 64, 80, 16, 61, 57, 14, 11,
     ];
+    let mut number_hashset = HashSet::new();
 
-    let mut survey_hash = HashMap::new();
-
-    for (gender, number) in data {
-        // (&str, i32)
-        survey_hash.entry(gender).or_insert(Vec::new()).push(number);
+    for number in many_numbers {
+        number_hashset.insert(number);
     }
 
-    for (male_or_female, numbers) in survey_hash {
-        println!("{:?}, {:?}", male_or_female, numbers);
-    }
+    let hashset_length = number_hashset.len();
+    println!(
+        "There are {} unique numbers, so we are missing {}.",
+        hashset_length,
+        100 - hashset_length
+    );
 }
