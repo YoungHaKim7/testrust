8f48a4d6582e64576501ac0610774b468a03bd33
YoungHa Kim <ytok1108@aol.com>
2022-01-12 20:16:48 +0900

    KOR055_BinaryHeap__Priority queue result Popped off 30. Remaining numbers are: [25,15,20,0,10,5] Popped off 25. Remaining numbers are: [20,15,5,0,10] Popped off 20. Remaining numbers are: [15,10,5,0] Popped off 15. Remaining numbers are: [10,0,5] Popped off 10. Remaining numbers are: [5,0] Popped off 5. Remaining numbers are:[0] Popped off 0. Remaining numbers are:[]

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,29 @@
-use std::collections::HashSet;
-// HashSet  key값만 있고 Value값은 없다.!
-// BTreeSet  HashSet과 동일하지만 순서대로 출력하고 싶으면 쓰면 됨. 대신 HashSet보다는 약간 느리다.!
+// BinaryHeap - 가장 큰 숫자가 먼저 출력되는게 BinaryHeap의 특징이다.
+// 돈을 가장 많이 낸 사람이 먼저 출력 받을 수 있게 활용이 가능함. 최고!! vip관리할때 쓰는 BinaryHeap
+// priority queue
+use std::collections::BinaryHeap;
+
+fn show_remainder(input: &BinaryHeap<i32>) -> Vec<i32> {
+    let mut remainder_vec = vec![];
+    for number in input {
+        remainder_vec.push(*number)
+    }
+    remainder_vec
+}
 
 fn main() {
-    let many_numbers = vec![
-        94, 42, 59, 64, 32, 22, 38, 5, 59, 49, 15, 89, 74, 29, 14, 68, 82, 80, 56, 41, 36, 81, 66,
-        51, 58, 34, 59, 44, 19, 93, 28, 33, 18, 46, 61, 76, 14, 87, 84, 73, 71, 29, 94, 10, 35, 20,
-        35, 80, 8, 43, 79, 25, 60, 26, 11, 37, 94, 32, 90, 51, 11, 28, 76, 16, 63, 95, 13, 60, 59,
-        96, 95, 55, 92, 28, 3, 17, 91, 36, 20, 24, 0, 8, 82, 58, 93, 68, 54, 80, 56, 22, 67, 82,
-        58, 64, 80, 16, 61, 57, 14, 11, 11, 11, 11,
-    ];
-    let mut number_hashset = HashSet::new();
+    let many_numbers = vec![0, 5, 10, 15, 20, 25, 30];
+    let mut my_heap = BinaryHeap::new();
 
     for number in many_numbers {
-        number_hashset.insert(number);
+        my_heap.push(number);
     }
 
-    let hashset_length = number_hashset.len();
-    println!(
-        "There are {} unique numbers, so we are missing {}.",
-        hashset_length,
-        100 - hashset_length
-    );
-
-    let mut missing_vec = vec![];
-    for number in 0..100 {
-        //0..=99 동일한 표현
-        if number_hashset.get(&number).is_none() {
-            missing_vec.push(number);
-        }
-    }
-
-    print!("It does not contain:");
-    for number in missing_vec {
-        print!("{} ", number);
+    while let Some(number) = my_heap.pop() {
+        println!(
+            "Popped off {}. Remaining numbers are : {:?}",
+            number,
+            show_remainder(&my_heap)
+        );
     }
 }
