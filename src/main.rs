e01cf43859675456a0e68b67dc047f6a0fef8432
YoungHa Kim <ytok1108@aol.com>
2022-01-14 19:26:10 +0900

    ? Operator's explanation.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 // The question mark operator
-// ?
+// ? 써 주면 될수도 있고 안될수도 있고 라는 뜻이 됨
+// 에러가 생길것 같다 생각이 들면 ? 써서 방지해준다. 이유는 편하기 때문!!
+// 난중에 .await?  서버에서 다른 코드를 기다렸다가 같이 실행하는 코딩명령어
+// ? 코드를 써 주겠되면 무조건 Result<i32, ParseIntError> 를 꼭 써줘야한다!!
 use std::num::ParseIntError;
+
+let parsed_number = input.parse::<u16>?.to_string().parse::<u32>()?.to_string().parse::<i32>()?; // Add a ? each time to check and pass it on
 
 fn parse_str(input: &str) -> Result<(), ParseIntError> {
     let parsed_number = input.parse::<i32>()?; // return Error
