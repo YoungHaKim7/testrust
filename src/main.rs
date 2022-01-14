0a982bc0e35b9ae246dd27a2fabce004defc90e4
YoungHa Kim <ytok1108@aol.com>
2022-01-14 19:15:16 +0900

    The same Code

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,14 @@
 // ?
 use std::num::ParseIntError;
 
-// gives the error to the caller
-fn parse_str(input: &str) -> Result<i32, ParseIntError> {
+fn parse_str(input: &str) -> Result<(), ParseIntError> {
     let parsed_number = input.parse::<i32>()?; // return Error
-    Ok(parsed_number)
+    println!("It worked! {}", parsed_number);
+    Ok(())
 }
 
 fn main() {
     for item in vec!["Senven", "8", "9.0", "nice", "6060"] {
-        let parsed = parse_str(item);
-        println!("{:?}", parsed);
+        parse_str(item);
     }
 }
