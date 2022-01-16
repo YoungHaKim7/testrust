013117da25a6ac1cc9c4f3ad8c782612834f2491
YoungHa Kim <ytok1108@aol.com>
2022-01-16 17:09:48 +0900

    Trit intro__The most commonly used A is clone, copy, debug.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
-// stcuct User // things
-// enum Months // choices
-// trait // verbs / adjectives
-
-// traits   [power superpower]
 use std::fmt::Debug;
 
-#[derive(Debug)]
-struct MyStruct {
-    number: usize,
+#[derive(Clone, Copy, Debug)]
+struct ThingsToAdd {
+    first_thing: u32,
+    second_thing: f32,
 }
 
-fn print_as_debug<T>(input: T)
-where
-    T: Debug,
-{
-    println!("{:?}", input);
-}
+fn main() {
+    let my_thing = ThingsToAdd {
+        first_thing: 32,
+        second_thing: 8.8,
+    };
+    let second_thing = ThingsToAdd {
+        first_thing: 32,
+        second_thing: 8.8,
+    };
 
-fn main() {}
+    let sum = my_thing + second_thing;
+}
