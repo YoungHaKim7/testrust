6ee53cda6e312a651cc7970ee41cd743e8638824
YoungHa Kim <ytok1108@aol.com>
2022-01-19 19:38:10 +0900

    Kor062_Another small trait

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,57 @@
-use std::fmt;
-
-#[derive(Debug)]
-struct Cat {
-    name: String,
-    age: u8,
+struct Monster {
+    health: i32,
 }
 
-impl fmt::Display for Cat {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let name = &self.name;
-        let age = self.age;
-        write!(f, "My cat's name is {name} and it is {age} years old")
+struct Wizard {}
+struct Ranger {}
+
+trait FightClose {
+    fn attack_with_sword(&self, opponent: &mut Monster) {
+        opponent.health -= 10;
+        println!(
+            "You strike with your sword! Your opponent's health is now {}",
+            opponent.health
+        );
+    }
+    fn attack_with_hand(&self, opponent: &mut Monster) {
+        opponent.health -= 2;
+        println!(
+            "You strike with your sword! Your opponent's health is now {}",
+            opponent.health
+        );
     }
 }
+impl FightClose for Wizard {}
+impl FightClose for Ranger {}
+
+trait FightFromDistance {
+    fn attack_with_bow(&self, opponent: &mut Monster, distance: u32) {
+        if distance < 10 {
+            opponent.health -= 10;
+            println!(
+                "You attack with your bow! Your opponent's health is now {}",
+                opponent.health
+            )
+        }
+    }
+    fn attack_with_rock(&self, opponent: &mut Monster, distance: u32) {
+        if distance < 3 {
+            opponent.health -= 4;
+            println!(
+                "You attack with a rock! Your opponent's health is now {}",
+                opponent.health
+            )
+        }
+    }
+}
+impl FightFromDistance for Ranger {}
 
 fn main() {
-    let mr_mantle = Cat {
-        name: "Reggie Mantle".to_string(),
-        age: 4,
-    };
+    let radagast = Wizard {};
+    let aragorn = Ranger {};
 
-    println!("{mr_mantle}");
+    let mut uruk_hai = Monster { health: 40 };
+
+    radagast.attack_with_sword(&mut uruk_hai);
+    aragorn.attack_with_bow(&mut uruk_hai, 7);
 }
