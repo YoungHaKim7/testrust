0a9e9ca9b0135efdba6e57ba1773addb527d27ac
YoungHa Kim <ytok1108@aol.com>
2022-01-21 20:41:39 +0900

    Implementing From___2

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,43 @@
-use std::fmt::Display;
+#[derive(Debug)] // So we can print City
+struct City {
+    name: String,
+    population: u32,
+}
 
-fn print_vec<T: Display>(input: &Vec<T>) {
-    for item in input {
-        println!("{item} ");
+#[derive(Debug)] // Country also needs to be printed
+struct Country {
+    cities: Vec<City>, //Our cities go in here
+}
+
+impl City {
+    fn new(name: &str, population: u32) -> Self {
+        Self {
+            name: name.to_string(),
+            population,
+        }
     }
-    println!();
+}
+
+// Country::from(vec![City, City])
+impl From<Vec<City>> for Country {
+    fn from(cities: Vec<City>) -> Self {
+        Self { cities }
+    }
+}
+
+impl Country {
+    fn print_cities(&self) {
+        for city in &self.cities {
+            println!("{:?} has a population of {:?}", city.name, city.population);
+        }
+    }
 }
 
 fn main() {
-    let array_vec = Vec::from([8, 9, 10]);
-    print_vec(&array_vec);
+    let helsinki = City::new("Helsinki", 631_695);
+    let turku = City::new("Turku", 186_756);
 
-    let str_vec = Vec::from("What kind of vec is this?");
-    print_vec(&str_vec);
-
-    let string_vec = Vec::from("What kind of Vec is a String vec?");
-    print_vec(&string_vec);
+    let finland_cities = vec![helsinki, turku];
+    let finland = Country::from(finland_cities);
+    finland.print_cities();
 }
