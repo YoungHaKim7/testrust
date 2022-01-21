e707806795e0edea93c5fbd3e02381a7ebbe885a
YoungHa Kim <ytok1108@aol.com>
2022-01-21 20:49:01 +0900

    Kor064__Implementing From__

--- src/main.rs
+++ src/main.rs
@@ -43,4 +43,6 @@ fn main() {
 
     //.into()   From이랑 똑같지만 복잡하게 쭉 쓰고 마지막에 .into하고 만들고 싶은 타입을 입력해줌
     // let x = .....iter().for_each().into()
+    // 단독으로 into를 쓸수 없다. 이유는 타입이 정해져 있지 않기 때문이다.__rust_force_expr!
+    // from 을 쓰고 into로 연결해 준다.
 }
