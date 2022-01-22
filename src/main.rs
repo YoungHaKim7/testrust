dc5c1377b66e7e3e51425b8b51c70ffc69289cb7
YoungHa Kim <ytok1108@aol.com>
2022-01-22 21:15:19 +0900

    Blanket trait 003

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
 // blanket trait implementtations
 // implementing a trait for every type that want to have it
+use std::fmt::Debug;
 
-trait Prints {
+trait Prints: Debug {
     fn prints_something(&self) {
-        println!("I like to print things");
+        println!("I am: {:?}", self);
     }
 }
 
+#[derive(Debug)]
 struct Person;
 struct Building;
 
-impl<T> Prints for T {}
+impl<T: Debug> Prints for T {}
 
 fn main() {
     let my_person = Person;
     let my_building = Building;
     my_person.prints_something();
-    let x = String::from("Hello");
-    x.prints_something();
 }
