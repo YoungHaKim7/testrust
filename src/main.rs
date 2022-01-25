d0c47d1d895c128f8e6acbcea088a438ea640d0e
YoungHa Kim <ytok1108@aol.com>
2022-01-25 20:50:24 +0900

    Rust STYLE Same Code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 // Chaining methods and functional style
 fn main() {
-    let new_vec = (1..=10).collect::<Vec<i32>>();
+    let new_vec = (1..=10).collect::<Vec<_>>();
 
     println!("{new_vec:?}")
 }
