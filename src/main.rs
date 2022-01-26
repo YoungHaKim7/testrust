6a7fb04f33cfdf83eee2c73db3b84bc387869883
YoungHa Kim <ytok1108@aol.com>
2022-01-26 21:16:08 +0900

    Kor069__Iterator intro_

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,20 @@
-// Chaining methods and functional style
-
-// .next()
-// .next() Some(T) None
-// Some(1) = 1
-// Some(2) = 2
-//
+// Iterator = a collection of things that you can call .next() on
+// Iterator많이 쓰는 기능!!!
+// .iter() - iterator of references &T
+// .iter_mut() - iterator of mutable references &mut T
+// .into_iter() - consuming iterator 이터레이터가 사라짐? - iterator를 계속 안 쓰게 된다면, 이게 젤 편함references를 안봐도 되기 때문!!
+// map이라는 기능은 vec안에 있는 것을 하나씩 가지고 자기 맘대로 할 수 있다.
+// map |x|  이름을 정해주는 거임.  |num| 이렇게 해도 됨
 
 fn main() {
-    let my_vec = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let vector1 = vec![1, 2, 3];
+    let vector1_a = vector1.iter().map(|x| x + 1).collect::<Vec<i32>>();
+    let vector1_b: Vec<i32> = vector1.into_iter().map(|x| x * 10).collect();
 
-    let new_vec = my_vec
-        .into_iter()
-        .skip(1)
-        .skip(1)
-        .skip(1)
-        .skip(1)
-        .take(4)
-        .collect::<Vec<i32>>();
+    let mut vector2 = vec![10, 20, 30];
+    vector2.iter_mut().for_each(|num| *num += 100);
 
-    println!("{new_vec:?}");
+    println!("{vector1_a:?}");
+    println!("{vector2:?}");
+    println!("{:?}", vector1_b);
 }
