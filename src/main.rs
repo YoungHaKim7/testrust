299ce0c67a9dba672120d0f77d87c72c00c729e7
YoungHa Kim <ytok1108@aol.com>
2022-01-27 21:26:16 +0900

    Kor070_Iterators and assert_eq

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     let my_vec = vec!['a','b','거', '魔'];
     let mut my_vec_iter = my_vec.iter();
-    println!("{:?}", my_vec_iter.next());
 
+    assert_eq!(my_vec_iter.next(), Some(&'a')); // 2개 같아야 패닉이 안됨. test할 때 쓰는 기능
 }
