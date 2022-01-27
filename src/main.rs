13c1a883f6d9f1bd197dbde810395b6786651d23
YoungHa Kim <ytok1108@aol.com>
2022-01-27 21:28:55 +0900

    Iterators and assert_eq

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,8 @@ fn main() {
     let mut my_vec_iter = my_vec.iter();
 
     assert_eq!(my_vec_iter.next(), Some(&'a')); // 2개 같아야 패닉이 안됨. test할 때 쓰는 기능
+    assert_eq!(my_vec_iter.next(), Some(&'b')); 
+    assert_eq!(my_vec_iter.next(), Some(&'거'));
+    assert_eq!(my_vec_iter.next(), Some(&'魔'));
+    assert_eq!(my_vec_iter.next(), None); 
 }
