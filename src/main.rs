b3aada9171faff5cc8eea737bb01de03464bae03
YoungHa Kim <ytok1108@aol.com>
2022-01-30 17:03:40 +0900

    Kor073__unwrap_or_else_closure || __

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 // closure 
 // anonymous function
-
 // zero cost abstractions = Rust 가장 중요한 개념 Rust에서 추구하는 목표임.
+// .iter().map().filter().inspect().collect() 이렇게 되도 속도가 빠르다. 
 
 fn main () {
-    let my_number = 9;
-    let anonymous_function = || println!("I am a function"); // anonymous function
-    let closure = || println!("{my_number}");// closure
+    let my_vec = vec![8,9,10];
+    //unwrap_or_else 는 closure || pipes 를 쓸 수 있다.!! 최고!!
+    let fourth = my_vec.get(3).unwrap_or_else(|| {
+        if my_vec.get(0).is_some() {
+            &my_vec[0]
+        } else {
+            &0
+        }
+    });
 
-    .iter().map().filter().inspect().collect()
+    println!("{fourth}");
 
 }
