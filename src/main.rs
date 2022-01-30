780b916a355e599da099cdc93a012c985bea8241
YoungHa Kim <ytok1108@aol.com>
2022-01-30 17:13:32 +0900

    unwrap_or(9) result: 9

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,8 @@
-// closure 
-// anonymous function
-// zero cost abstractions = Rust 가장 중요한 개념 Rust에서 추구하는 목표임.
-// .iter().map().filter().inspect().collect() 이렇게 되도 속도가 빠르다. 
 
 fn main () {
     let my_vec = vec![];
-    //unwrap_or_else 는 closure || pipes 를 쓸 수 있다.!! 최고!!
-    let fourth = my_vec.get(3).unwrap_or_else(|| {
-        if my_vec.get(0).is_some() {
-            &my_vec[0]
-        } else {
-            &0
-        }
-    });
-    // [3]_4번째를 출력하고 4번째가 없으면 [0]1번째를 출력해서 1번째가 없으면 그래도 &[0] 첫번째를 출력하라는 Code
-    // 4번째를 만들면 4번째인 9가 출력되는 Code
-    // 변수가 empty vec이면 안전하게 0출력됨.
+
+    let fourth = my_vec.get(3).unwrap_or(&9);
 
     println!("{fourth}");
 
