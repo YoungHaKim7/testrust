79f12003637714f490dda21cbd8cf158d8e3d6b7
YoungHa Kim <ytok1108@aol.com>
2022-01-31 20:35:40 +0900

    for_each // () end

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     num_vec
         .iter() // 2, 4, 6
         .enumerate() // (0,2), (1,4), (2,6)
-        .for_each(|(index, number)| {
+        .for_each(|(index, number)| { // ()
             println!("The number at index {index} is {number}");
         });
 }
