7f2057b9b39eb11bf6f71e6243acd6fedb5c96e2
YoungHa Kim <ytok1108@aol.com>
2022-02-01 17:17:52 +0900

    Kor075__unused __Map__that must be used iterators are lazy and do nothing unless consumed

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 fn main() {
     let num_vec = vec![2,4,6];
 
-    let new_thing = num_vec
+    num_vec
         .iter() // 2, 4, 6
         .enumerate() // (0,2), (1,4), (2,6)
         .map(|(index, number)| {
             println!("The number at index {index} is {number}");
         });
-
-    new_thing.lkjsdlfkjweoiujncx();
 }
