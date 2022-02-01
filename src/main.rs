af256fe04748886f8cee24762fa18f0927e7dcec
YoungHa Kim <ytok1108@aol.com>
2022-02-01 17:47:28 +0900

    result: (1, one) (2, two) (0, zero)

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::collections::HashMap;
 
 fn main() {
     let some_numbers = vec![0,1,2,3,4,5,]; // a Vec<i32>
-    let some_words = vec!["zero", "one","two","three","four"]; //Vec<&str> 4까지만 연결되고 나머지는 버려짐 2개가 다르기 때문
+    let some_words = vec!["zero", "one","two"]; //Vec<&str> 2까지만 연결되고 나머지는 버려짐 2개가 다르기 때문
 
     let number_word_hashmap: HashMap<i32, &str> = some_numbers
         .into_iter()
