b15d20bfb3c98ea1b7a96e4a1801a50d7952005b
YoungHa Kim <ytok1108@aol.com>
2022-02-01 17:21:03 +0900

    collect::<Vec<i32>>() result The number at index 0 is 2 The number at index 1 is 4 The number at index 2 is 6 [0, 0, 0]

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 fn main() {
     let num_vec = vec![2,4,6];
 
-    num_vec
+    let new_thing = num_vec
         .iter() // 2, 4, 6
         .enumerate() // (0,2), (1,4), (2,6)
         .map(|(index, number)| {
             println!("The number at index {index} is {number}");
-        });
+            0
+        })
+        .collect::<Vec<_>>();
+
+        println!("{new_thing:?}");
 }
