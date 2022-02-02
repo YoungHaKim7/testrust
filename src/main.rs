c1a662e1a2736f376066e47091c41b7676b48e7e
YoungHa Kim <ytok1108@aol.com>
2022-02-02 20:07:09 +0900

    Kor076_char methods

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,9 @@
-// .zip 
-// 주의할점 iterator여 한다.!! .into_iter()한번 쓰면 사라지는 기능
-use std::collections::HashMap;
+//.chars() - iterator of char
+//.count() - counts nubmer of items in iterator
 
-fn main() {
-    let some_numbers = vec![0,1,2,3,4,5,]; // a Vec<i32>
-    let some_words = vec!["zero", "one","two"]; //Vec<&str> 2까지만 연결되고 나머지는 버려짐 2개가 다르기 때문
+fn main () {
+    let big_string = "Hello there, I am a &str";
 
-    let number_word_hashmap: HashMap<i32, &str> = some_numbers
-        .into_iter()
-        .zip(some_words.into_iter())
-        .collect();
+    big_string.chars().for_each(|c| println!("{c}"));
 
-        // closure와 Iterator 연습하는거라 쓰는 기능
-        // iter와 for_each를 하면 모든 아이템을 볼 수 있다.
-    number_word_hashmap.iter().for_each(|stuff| {
-        println!("{stuff:?}")
-    });
 }
