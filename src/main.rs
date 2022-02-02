842493e68ab7cab8aeadf88e137d5a8126449888
YoungHa Kim <ytok1108@aol.com>
2022-02-02 20:16:50 +0900

    Kor076__char_methods

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 //.chars() - iterator of char
 //.count() - counts nubmer of items in iterator
+// char_indices
+// = chars().enumerate()
 
-fn main () {
+// index indices(라틴어 index의 복수형)
+
+fn main() {
     let big_string = "Hello there, I am a &str";
 
-    println!("big_string has {} characters", big_string.chars().count());
-
+    big_string.char_indices().for_each(|(index, charrrrrrr)| {
+        println!("At index {index} is the char {charrrrrrr}");
+    });
 }
