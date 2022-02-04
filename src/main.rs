ee826461e957eaedb6753fc602679fe02084ea12
YoungHa Kim <ytok1108@aol.com>
2022-02-04 20:40:48 +0900

    Kor 078__ok_or Result: [OK("Unknown"), Ok("Doug Suttle"),Err("No CEO found"),Err("No CEO found")]

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,44 @@
 // ok - Result to Option
+// ok_or - Option to Result
+// ok_or_else - Option to Result with closure  else가 나오면 closure가 있는거다.!! 암기!!
+
+struct Company {
+    name: String,
+    ceo: Option<String>,
+}
+
+impl Company {
+    fn new(name: &str, ceo: &str) -> Self {
+        // ""
+        let ceo = match ceo {
+            "" => None,
+            ceo => Some(ceo.to_string()),
+        };
+
+        Self {
+            name: name.to_string(),
+            ceo,
+        }
+    }
+
+    fn get_ceo(&self) -> Option<String> {
+        self.ceo.clone()
+    }
+}
 
 fn main() {
-    let user_input = vec![
-        "8.9",
-        "Nine point nine five",
-        "8.0",
-        "7.6",
-        "eleventy-twelve",
+    let company_vec = vec![
+        Company::new("Umbrella Corporation", "Unknown"),
+        Company::new("Ovintiv", "Doug Suttles"),
+        Company::new("The Red-Headed League", ""),
+        Company::new("Stark Enterprises", ""),
     ];
 
-    let actual_numbers = user_input
-        .into_iter()
-        .filter_map(|input| input.parse().ok()) // Ok(num) / Err(err)
-        .collect::<Vec<f32>>();
+    let mut results_vec = vec![];
 
-    println!("{actual_numbers:?}");
+    company_vec.iter().for_each(|company| {
+        results_vec.push(company.get_ceo().ok_or("No CEO found"));
+    });
+
+    println!("{results_vec:?}");
 }
