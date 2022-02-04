15a1fa75f515a98b0a3229a08c40dc2942f7fc97
YoungHa Kim <ytok1108@aol.com>
2022-02-04 20:48:10 +0900

    Kor078_ok_or_else ok(unkown), ok(doug suttle), Err(No ceo found for the red0headed leagu), Err(no ceo found for stark enterprises)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // ok - Result to Option
 // ok_or - Option to Result
 // ok_or_else - Option to Result with closure  else가 나오면 closure가 있는거다.!! 암기!!
+// format! 이 기능은 String만들기 편한 매크로!
 
 struct Company {
     name: String,
@@ -37,7 +38,10 @@ fn main() {
     let mut results_vec = vec![];
 
     company_vec.iter().for_each(|company| {
-        results_vec.push(company.get_ceo().ok_or("No CEO found"));
+        results_vec.push(company.get_ceo().ok_or_else(|| {
+            let err_message = format!("No CEO found for {}", company.name);
+            err_message
+        }));
     });
 
     println!("{results_vec:?}");
