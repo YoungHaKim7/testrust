8b3004d345d4f6501f71581cf2b276c1994fbb56
YoungHa Kim <ytok1108@aol.com>
2022-02-05 16:20:07 +0900

    Kor079_map

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,14 @@
-// ok - Result to Option
-// ok_or - Option to Result
-// ok_or_else - Option to Result with closure  else가 나오면 closure가 있는거다.!! 암기!!
-// format! 이 기능은 String만들기 편한 매크로!
+// map
+// and_then 맵이랑 비슷한데 조금 다음
+// 실전에서 많이 쓰는 기능 map & and_then
 
-struct Company {
-    name: String,
-    ceo: Option<String>,
-}
-
-impl Company {
-    fn new(name: &str, ceo: &str) -> Self {
-        // ""
-        let ceo = match ceo {
-            "" => None,
-            ceo => Some(ceo.to_string()),
-        };
-
-        Self {
-            name: name.to_string(),
-            ceo,
-        }
-    }
-
-    fn get_ceo(&self) -> Option<String> {
-        self.ceo.clone()
-    }
-}
+// .map(|some_thing|some_thing+1) None 이면 None이 나옴.
 
 fn main() {
-    let company_vec = vec![
-        Company::new("Umbrella Corporation", "Unknown"),
-        Company::new("Ovintiv", "Doug Suttles"),
-        Company::new("The Red-Headed League", ""),
-        Company::new("Stark Enterprises", ""),
-    ];
+    let some_output = Some(vec![8, 9, 10]);
 
-    let mut results_vec = vec![];
+    let first =
+        some_output.map(|some_vec| some_vec.iter().map(|num| num + 1).collect::<Vec<i32>>());
 
-    company_vec.iter().for_each(|company| {
-        results_vec.push(company.get_ceo().ok_or_else(|| {
-            let err_message = format!("No CEO found for {}", company.name);
-            println!("Oh no! Error: {err_message}");
-            err_message
-        }));
-    });
-
-    println!("{results_vec:?}");
+    println!("{first:?}");
 }
