6bfff91b63bbe2f1dd222e4174bb7f8dc3d04b6e
YoungHa Kim <ytok1108@aol.com>
2022-02-05 16:26:27 +0900

    Kor079_map_and_then_for Option result: Some(9,10,11)

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,16 @@
 // .map(|some_thing|some_thing+1) None 이면 None이 나옴.
 
 fn main() {
-    let some_output: Option<Vec<i32>> = None; // Some(vec![8, 9, 10]);
+    let some_output = Some(vec![8, 9, 10]);
 
-    let first =
-        some_output.map(|some_vec| some_vec.iter().map(|num| num + 1).collect::<Vec<i32>>());
+    let first = some_output
+        .clone()
+        .map(|some_vec| some_vec.iter().map(|num| num + 1).collect::<Vec<i32>>());
 
+    let second = some_output.map(|some_vec| match some_vec.len() {
+        0 => None,
+        1 => Some(vec![some_vec[0]]),
+        _ => Some(some_vec),
+    });
     println!("{first:?}");
 }
