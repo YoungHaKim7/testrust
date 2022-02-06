bb593464989b93f934d8e63e8ec264952f098c76
YoungHa Kim <ytok1108@aol.com>
2022-02-06 17:15:45 +0900

    Kor080__and

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,12 @@
-// map
-// and_then 맵이랑 비슷한데 조금 다음
-// 실전에서 많이 쓰는 기능 map & and_then
-
-// .map(|some_thing|some_thing+1) None 이면 None이 나옴.
+// and
+// or
 
 fn main() {
-    let some_output = Some(vec![8, 9, 10]);
+    let one = true;
+    let two = false;
+    let three = true;
+    let four = true;
 
-    let first = some_output
-        .clone()
-        .map(|some_vec| some_vec.iter().map(|num| num + 1).collect::<Vec<i32>>());
-
-    let second = some_output
-        .and_then(|some_vec| match some_vec.len() {
-            // .get() Option
-            0 => None,
-            1 => Some(vec![some_vec[0]]),
-            _ => Some(some_vec),
-        })
-        .flatten(); //flatten
-
-    println!("{first:?}");
-    println!("{second:?}");
+    println!("{}", one && three);
+    println!("{}", one && two && three && four);
 }
