8488df23fee1e96dea370f7c3a68c44170dd2b54
YoungHa Kim <ytok1108@aol.com>
2022-02-07 19:58:55 +0900

    Kor081_any_result: Is true inside?

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@
 fn in_char_vec(char_vec: &Vec<char>, check: char) {
     println!(
         "Is {} inside?",
-        char_vec.iter().any(|character| *character == check)
+        char_vec.iter().any(|&character| character == check)
     )
 }
 
 fn main() {
     let char_vec = ('a'..'監').collect::<Vec<char>>();
-    println!("{}", char_vec.iter().count());
+    in_char_vec(&char_vec, 'i');
 }
