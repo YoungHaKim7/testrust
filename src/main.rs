464cbc75fcbb7927cdc1ba07e5c828d020f50466
YoungHa Kim <ytok1108@aol.com>
2022-02-07 19:57:09 +0900

    081_any

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,13 @@
 // any 여러개 중에 한개만 맞아도 true
 // all true / 다 맞아야 true
 
+fn in_char_vec(char_vec: &Vec<char>, check: char) {
+    println!(
+        "Is {} inside?",
+        char_vec.iter().any(|&character| character == check)
+    )
+}
+
 fn main() {
     let char_vec = ('a'..'監').collect::<Vec<char>>();
     println!("{}", char_vec.iter().count());
