8bfe7caa4ebcb0a40e278e087f16bf2ed82afe31
YoungHa Kim <ytok1108@aol.com>
2022-02-08 21:07:58 +0900

    Koa082_reverse itarators003  result: Final counter is : 1001

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,15 @@ fn main() {
     let mut big_vec = vec![6; 1000];
     big_vec.push(5);
 
-    println!("{:?}", big_vec.iter().rev().any(|&number| number == 5)); //true
-                                                                       // short circuit
+    let mut counter = 0;
+    let mut big_iter = big_vec.into_iter();
+
+    loop {
+        counter += 1;
+        if big_iter.next() == Some(5) {
+            break;
+        }
+    }
+
+    println!("Final counter is : {counter}");
 }
