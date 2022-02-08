49aeb6e7a09e185f53a5da0fab6f02516e1cc829
YoungHa Kim <ytok1108@aol.com>
2022-02-08 21:05:19 +0900

    Kor082_reverse iterators 002

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@
 fn main() {
     let mut big_vec = vec![6; 1000];
     big_vec.push(5);
-    // rev() reverse
-    let mut iterator = big_vec.iter().rev();
-    assert_eq!(Some(&5), iterator.next());
-    assert_eq!(Some(&6), iterator.next());
+
+    println!("{:?}", big_vec.iter().rev().any(|&number| number == 5));
 }
