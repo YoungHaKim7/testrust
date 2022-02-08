3d35c83139881b3d8fdd5d9159c1b6244487a5e7
YoungHa Kim <ytok1108@aol.com>
2022-02-08 21:00:21 +0900

    Kor082_reverse iterators__result: Some(5) Some(6)

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,10 @@
-// ('a'..='i').collect
-// any 여러개 중에 한개만 맞아도 true
-// all true / 다 맞아야 true
-
-fn in_char_vec(char_vec: &Vec<char>, check: char) {
-    println!(
-        "Is {check} inside? {}",
-        char_vec.iter().any(|&character| character == check)
-    )
-}
+// any
 
 fn main() {
-    let char_vec = ('a'..'監').collect::<Vec<char>>();
-    in_char_vec(&char_vec, 'i');
-    in_char_vec(&char_vec, '붹');
-    in_char_vec(&char_vec, '無');
-
-    // short-circuiting 애니ani는 딱 한번만 맞아도 true가 나온다.
-    let smaller_vec = ('A'..'z').collect::<Vec<char>>();
-    println!(
-        "All alphabetic? {}",
-        smaller_vec
-            .iter()
-            .all(|&character| character.is_alphabetic())
-    );
-    println!(
-        "All less than the character 행? {}",
-        smaller_vec.iter().all(|&c| c < '행')
-    );
-    println!("{smaller_vec:?}");
+    let mut big_vec = vec![6; 1000];
+    big_vec.push(5);
+    // rev() reverse
+    let mut iterator = big_vec.iter().rev();
+    println!("{:?}", iterator.next());
+    println!("{:?}", iterator.next());
 }
