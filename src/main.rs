d87dc0ce6e5d10e5b16a8be40c8e82286b75bad4
YoungHa Kim <ytok1108@aol.com>
2022-02-09 19:40:13 +0900

    Kor083_find_position_result: Some(30) None Some(2) None

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@
 // cycle
 
 fn main() {
-    let num_vec = vec![10, 20, 32, 40, 50, 60, 70, 80, 90, 100];
+    let num_vec = vec![10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
     println!("{:?}", num_vec.iter().find(|&n| n % 3 == 0));
+    println!("{:?}", num_vec.iter().find(|&number| number % 2 == 30));
+    println!("{:?}", num_vec.iter().position(|&num| num % 3 == 0));
+    println!("{:?}", num_vec.iter().position(|&num| num * 2 == 30));
 }
