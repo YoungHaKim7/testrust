cbc5afb55f6507fd1693fb290eff87a13f8db9e3
YoungHa Kim <ytok1108@aol.com>
2022-02-10 22:24:39 +0900

    Kor084_skip_take_collect a~j_russian Lan

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 // skip, take, fold
 
+// cargo run David MacLeod // auguments
+
 fn main() {
    let ten_chars = ('a'..).take(10).collect::<Vec<_>>();
    println!("{ten_chars:?}");
+
+   let skip_then_ten_chars = ('a'..).skip(1000).take(10).collect::<Vec<_>>();
+   println!("{skip_then_ten_chars:?}");
 }
