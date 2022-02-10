882dc5d2d097619923663d18e88cea31f8537bd7
YoungHa Kim <ytok1108@aol.com>
2022-02-10 22:18:37 +0900

    Kor084_ 10char_ result: struct[a~j]

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
-// cycle  계속 똑같은 것만 나오는거임 . iterator 끝나지 않는 trait
-// iterator를 만든다음에 완전 다른 타입이 나온다.clcle iterator로 만든다.
+// skip, take, fold
 
 fn main() {
-    // .take(6)
-    let even_odd = vec!["even", "odd"].into_iter().cycle();
-
-    let six_items = even_odd.into_iter().cycle().take(6).collect::<Vec<_>>();
-    println!("{six_items:?}");
+   let ten_chars = ('a'..).take(10).collect::<Vec<_>>();
+   println!("{ten_chars:?}");
 }
