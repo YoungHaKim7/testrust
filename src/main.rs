b0e86040ce34786edafca5668d1b925496e6297d
YoungHa Kim <ytok1108@aol.com>
2022-02-10 22:36:48 +0900

    kor084_fold methods_ result: 45

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 // skip, take, fold
-
-// cargo run David MacLeod // auguments
-
 fn main() {
-   let ten_chars = ('a'..).take(10).collect::<Vec<_>>();
-   println!("{ten_chars:?}");
+    let some_numbers = vec![9,6,9,10,11];
 
-   let skip_then_ten_chars = ('a'..).skip(1000).take(10).collect::<Vec<_>>();
-   println!("{skip_then_ten_chars:?}");
+    // 0
+    // 0,9
+    // 9 + 6
+    // 15 +
+
+    println!("{}", some_numbers
+        .iter()
+        .fold(0,|total_so_far, next_number|total_so_far + next_number));
 }
