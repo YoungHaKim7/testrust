fe0dff561a0d4a0ef46499dc736e3567ac72cc1b
YoungHa Kim <ytok1108@aol.com>
2022-02-11 22:55:09 +0900

    Kor085_ fold method again

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 // skip, take, fold
+
 fn main() {
-    let some_numbers = vec![9,6,9,10,11];
+    let some_numbers = vec![9, 6, 9, 10, 11];
 
-    // 0
-    // 0,9
-    // 9 + 6
-    // 15 +
+    println!(
+        "{}",
+        some_numbers
+            .iter()
+            .fold(0, |total_so_far, next_number| total_so_far + next_number)
+    );
 
-    println!("{}", some_numbers
-        .iter()
-        .fold(100,|total_so_far, next_number|total_so_far - next_number));
+    println!("{}", some_numbers.iter().sum::<i32>());
 }
