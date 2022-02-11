426e03a15a396b39059db482544ecd10685768d4
YoungHa Kim <ytok1108@aol.com>
2022-02-11 23:07:24 +0900

    Kor085_fold method again result: I-d-o-n-h-a-v-e

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
 // skip, take, fold
-
 fn main() {
-    let some_numbers = vec![9, 6, 9, 10, 11];
+    let a_string = "I don't have any dashes in me.";
 
-    println!(
-        "{}",
-        some_numbers
-            .iter()
-            .fold(0, |total_so_far, next_number| total_so_far + next_number)
-    );
-
-    let total: i32 = some_numbers.iter().sum();
-    println!("{}", some_numbers.iter().sum::<i32>());
+    let dashed = a_string
+        .chars() // iterator
+        .fold("-".to_string(), |mut string_so_far, next_char| {
+            string_so_far.push(next_char); // ()
+            string_so_far.push('-'); // ()
+            string_so_far
+        });
+    println!("{dashed}");
 }
