6f648cc76b53ceb52665fa77992b5df9d8a8898a
YoungHa Kim <ytok1108@aol.com>
2022-02-12 19:07:19 +0900

    Kor086_Max

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-// fold
+use std::cmp::max;
 
 fn main() {
     let my_vec = vec![-878, 879879, 98798, 0, 76756];
 
     let biggest = my_vec // biggest
         .into_iter()
-        .fold(i32::MIN, |num1, num2| if num1 > num2 { num1 } else { num2 });
+        .fold(i32::MIN, |num1, num2| max(num1, num2));
     println!("Biggest is : {biggest}");
 }
