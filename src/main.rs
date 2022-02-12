c7eb1dd46b77787a8c7c71dbe89a107fb654527b
YoungHa Kim <ytok1108@aol.com>
2022-02-12 20:51:16 +0900

    windows

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
-use std::cmp::{max, min};
+// .take_while // take
+// .by_ref
+// .skip_while
+// .map_while
+// .cloned
+// lifetime 레퍼런스가 살아있는지 죽었는지 모를때 .cloned를 쓴다.
+// --------------
+// .chunks // 1,2,3,4,5,6,7,8,9
+// .windows // 1,2,3 -이걸 쓰면 굉장한 크기의 숫자를 만들수 있다.
 
 fn main() {
-    let my_vec = vec![-878, 879879, -98798, 0, 76756];
+    let num_vec = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
-    let biggest = my_vec // Smallest
-        .into_iter()
-        .fold(i32::MAX, |num1, num2| min(num1, num2));
-    println!("Smallest is : {biggest}");
+    for chunk in num_vec.chunks(3) {
+        println!("chunk is {chunk:?}");
+    }
+
+    for window in num_vec.windows(3) {
+        println!("Windows is {window:?}");
+    }
 }
