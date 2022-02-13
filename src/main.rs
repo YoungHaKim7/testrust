ca802790d6b5ce0db55bbdaf31c7ddddc90f169a
YoungHa Kim <ytok1108@aol.com>
2022-02-13 17:07:47 +0900

    A code that I use a lot at work.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 // match_indices indices = indexes
 // peekable
 
+fn user_input() -> String {
+    // do something
+    "user_name".to_string()
+}
+
 fn main() {
     // enuberate
     let rules = "Rule number 1 : No fighting.
         Rule number 2 : Goto bed at 8 pm.
         Rule number 3 : Wake up at 6am ";
 
-    let rule_locations = rules.match_indices("Rule").collect::<Vec<(_, _)>>();
+    let rule_locations = rules.match_indices(user_input).collect::<Vec<(_, _)>>();
     println!("Rule locations: {rule_locations:?}");
 }
