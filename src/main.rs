d9a2b7e6239d9be87cef96edbc458862d1becf11
YoungHa Kim <ytok1108@aol.com>
2022-02-13 16:59:41 +0900

    Kor087__indices

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,11 @@
-// .take_while // take
-// .by_ref
-// .skip_while
-// .map_while
-// .cloned
-// lifetime 레퍼런스가 살아있는지 죽었는지 모를때 .cloned를 쓴다.
-// --------------
-// .chunks // 1,2,3,4,5,6,7,8,9
-// .windows // 1,2,3 -이걸 쓰면 굉장한 크기의 숫자를 만들수 있다.
+// match_indices indices = indexes
+// peekable
 
 fn main() {
-    let num_vec = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+    // enuberate
+    let rules = "Rule number 1 : No fighting.
+        Rule number 2 : Goto bed at 8 pm.
+        Rule number 3 : Wake up at 6am ";
 
-    for chunk in num_vec.chunks(3) {
-        println!("chunk is {chunk:?}");
-    }
-
-    for window in num_vec.windows(3) {
-        println!("Windows is {window:?}");
-    }
+    let rule_locations = rules.match_indices("Rule").collect::<Vec<(i32, &str)>>();
 }
