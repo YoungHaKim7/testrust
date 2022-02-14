9f51cc09765b456a80cbc83e532a46284197824a
YoungHa Kim <ytok1108@aol.com>
2022-02-14 20:48:02 +0900

    macro 003 result: [src/main.rs:4] 9 = 9 [src/main.rs:5] my_number += 10 = () [src/main.rs:7] vec9, 10] = [     8,     9,     10, ] [src/main.rs:9] new_vec.iter().map(x x * 2).collect::<Vec<i32>>() = [     16,     18,     20, ]

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 // dbg! // debug = quick print
-// standard error
-// standard output
 
 fn main() {
-    let my_number = 9;
-    dbg!(my_number);
-    println!("{my_number}");
-    dbg!(10);
-    println!("Something else");
+    let mut my_number = dbg!(9);
+    dbg!(my_number += 10);
+
+    let new_vec = dbg!(vec![8, 9, 10]);
+
+    let double_vec = dbg!(new_vec.iter().map(|x| x * 2).collect::<Vec<i32>>());
 }
