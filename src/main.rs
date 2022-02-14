fc2a29e471c79a9b76bdcb9fbbe08224cf7bf735
YoungHa Kim <ytok1108@aol.com>
2022-02-14 20:35:19 +0900

    Kor088__the dbg! macro

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,7 @@
-// peekable
-// .peek()   .next()와 비슷한데 다음 아이템이 뭔지 미리 본다. 아이템을 미리 뺀다?
+// dbg! // debug = quick print
+// standard error
+// standard output
 
 fn main() {
-    // enuberate
-    let just_numbers = vec![1, 5, 100];
-
-    let mut number_iter = just_numbers.iter().peekable();
-
-    for _ in 0..3 {
-        println!("I love the number {}", number_iter.peek().unwrap()); // 1번만 계속 나옴 next() 나올때까지 주구장창 1번 반복
-        println!("I really love the number {}", number_iter.peek().unwrap());
-        number_iter.next();
-    }
+    dbg!()
 }
