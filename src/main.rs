6e806ec30af520e721844e16b3ffaacfdece97b6
YoungHa Kim <ytok1108@aol.com>
2022-02-14 20:52:18 +0900

    Debug method that you use when you work.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 // dbg! // debug = quick print
-
+// AWS
 fn main() {
     let mut my_number = dbg!(9);
     dbg!(my_number += 10);
 
+    dbg!();
+
     let new_vec = dbg!(vec![8, 9, 10]);
+
+    dbg!();
 
     let double_vec = dbg!(new_vec.iter().map(|x| x * 2).collect::<Vec<i32>>());
 }
