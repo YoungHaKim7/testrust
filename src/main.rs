8f57e26fdb6f1ca4f77a174ca31e564066d2c24d
YoungHa Kim <ytok1108@aol.com>
2022-02-15 21:27:24 +0900

    LifeTime____String vs. &str

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 // Sting &str 다음 시간에 할 내용은 String과 &str의 차이
+// &str 어떤 Reference다. 데이타가 안에 있음.
+// Reference를 땡겨와야 하는데 원본 자료가 drop이 되서 사라진 경우
+// 코드가 아주 위험한 상황이다.!
 
-struct Book<'a> {
-    title: &'a str,
+struct Book {
+    title: &str,
 }
 
 fn main() {
