093f1e346d177b8678396153abb289022f8fe86e
YoungHa Kim <ytok1108@aol.com>
2022-02-15 21:18:10 +0900

    Kor 089__same code ??? code error: missing lifetime specifier

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 // Sting &str 다음 시간에 할 내용은 String과 &str의 차이
 
 struct Book {
-    title: String,
+    title: &str,
 }
 
 fn main() {
-    let my_book = Book {
-        title: "my_title".to_string(),
-    };
+    let my_book = Book { title: "my_title" };
 }
