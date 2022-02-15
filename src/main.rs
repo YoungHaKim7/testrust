a5f7ce554ba7ef79c788851effdb0cde972cc21c
YoungHa Kim <ytok1108@aol.com>
2022-02-15 20:54:30 +0900

    KOR089__dbg

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
-// dbg! // debug = quick print
-// AWS
+// .inspect
+
 fn main() {
-    let mut my_number = dbg!(9);
-    dbg!(my_number += 10);
+    let new_vec = [8, 9, 10];
 
-    dbg!();
+    let double_vec = new_vec
+        .iter()
+        .map(|x| x * 2)
+        .filter(|num| *num > 17)
+        .collect::<Vec<_>>();
 
-    let new_vec = dbg!(vec![8, 9, 10]);
-
-    dbg!();
-
-    let double_vec = dbg!(new_vec.iter().map(|x| x * 2).collect::<Vec<i32>>());
+    dbg!(double_vec);
 }
