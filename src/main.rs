1619227e31fec357fdcebd214c420ac98b6f4a43
YoungHa Kim <ytok1108@aol.com>
2022-02-16 21:54:57 +0900

    available code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn returns_reference() -> &'static str {
-    let my_string = "David".to_string(); // &'static - for the life of the program
-    &my_string
+    // let my_string = "David".to_string(); // &'static - for the life of the program
+    "David"
 }
 
 fn main() {}
