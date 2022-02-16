c3b9484894c95e8beaf99ef9957f5a33e8bc2847
YoungHa Kim <ytok1108@aol.com>
2022-02-16 21:33:52 +0900

    Kor090_String vs. &str ._____'static

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
-// Sting &str 다음 시간에 할 내용은 String과 &str의 차이
-// &str 어떤 Reference다. 데이타가 안에 있음.
-// Reference를 땡겨와야 하는데 원본 자료가 drop이 되서 사라진 경우
-// 코드가 아주 위험한 상황이다.!
-// segmentation fault
-// use after free
-// 이것을 해결한게 Rust의 LifeTime이다. !! 대박!!!
+// String
+// &str
 
-struct Book {
-    title: &str,
-}
+// string literal 프로그램이 존재하는 동안 있는다 아주 간단한 스트링
+// borrowed str
 
 fn main() {
-    let my_book = Book { title: "my_title" };
+    let my_name = "David"; // 'static string이라 계속 살아 있는다.
+
+    {
+        let my_string = "David".to_string(); // &'static - for the life of the program
+        let my_string_ref = &my_string; // &str - reference to something else
+    }
 }
