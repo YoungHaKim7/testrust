3e85b55419965b788a6e2f73db53007b20cfbfdc
YoungHa Kim <ytok1108@aol.com>
2022-02-16 21:47:46 +0900

    missing lifetime specifier this function's return type contains a borro.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,6 @@
-// String
-// &str
-
-// string literal 프로그램이 존재하는 동안 있는다 아주 간단한 스트링
-// borrowed str
-
-fn main() {
-    let my_name = "David"; // 'static string이라 계속 살아 있는다.
-
-    {
-        let my_string = "David".to_string(); // &'static - for the life of the program
-        let my_string_ref = &my_string; // &str - reference to something else
-    }
+fn returns_reference() -> &str {
+    let my_string = "David".to_string(); // &'static - for the life of the program
+    let my_string_ref = &my_string; // &str - reference to something else
 }
+
+fn main() {}
