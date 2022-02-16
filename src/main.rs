65927b6405eafa9cb4f9fd1d7057dc59fc808ae3
YoungHa Kim <ytok1108@aol.com>
2022-02-16 22:11:36 +0900

    Lifetime Definition

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-// String 조금 느리고 &str조금더 빠르다. 이왕이면 &str위주로 쓰자!!
-
 struct Book<'booklifetime> {
     // Generics T, U와 비슷하다.
     name: &'booklifetime str,
 }
+use std::fmt::Display;
+
+fn print_thing<T: Display>(input: T) {}
 
 fn main() {
     let my_book = Book { name: "my book" };
