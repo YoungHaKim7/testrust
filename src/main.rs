7a51b4e403d23fe0d63191563bbce08069588986
YoungHa Kim <ytok1108@aol.com>
2022-02-17 21:15:42 +0900

    'Static' is impossible because a book exists in the main page.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 // 'static 만 특별한 Lifetime이다.
 // 이 예문이 가능한건 'static 뿐이다.
-struct Book<'book> {
-    name: &'book str,
+struct Book<'static> {
+    name: &'static str,
 }
-
-use std::fmt::Display;
-fn print_thing<T: Display>(input: T) {}
 
 fn main() {
     let my_book = Book { name: "my book" };
