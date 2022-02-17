14c0695eb721332f76c185b8ce025aaa91cfacab
YoungHa Kim <ytok1108@aol.com>
2022-02-17 21:37:45 +0900

    lifetimes 003

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,16 @@
-// 'static 만 특별한 Lifetime이다.
-// 이 예문이 가능한건 'static 뿐이다.
 // static str
-// borrowed str 이 예문은 borrowed str예문이다.!!!!
+// borrowed str
 // 'static
-struct Book<'a> {
+struct Book<'a, 'b> {
     // Generics T,U
     name: &'a str,
-    second_name: &'a str,
+    second_name: &'b str,
 }
 
 fn main() {
     let my_book_title = "my_book_title".to_string();
-    let second_name1 = "young".to_string();
 
-    let my_book = Book {
-        name: &my_book_title,
-        second_name: &second_name1,
-    };
+    //    let my_book = Book {
+    //        name: &my_book_title,
+    //    };
 }
