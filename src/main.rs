15ff12469d5d146c0caf409277f53171e73694aa
YoungHa Kim <ytok1108@aol.com>
2022-02-17 21:50:24 +0900

    use of undeclared lifetime name  'a _____-'

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ struct Adventurer<'a> {
 // implicit == not said
 // elided == not shown
 // 어떤 lifetime인지 미리 말을 해줘야 하는데 말을 안해 줘서 에러남.
-impl Adventurer {
+impl Adventurer<'a> {
     fn take_damage(&mut self) {
         self.hit_points -= 20;
         println!("{} has{} hit hit points left!", self.name, self.hit_points);
