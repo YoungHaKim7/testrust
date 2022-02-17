a267b7856601536cc8f9c37ad7154b278254f424
YoungHa Kim <ytok1108@aol.com>
2022-02-17 21:34:46 +0900

    lifetimes__02

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,15 @@
 struct Book<'a> {
     // Generics T,U
     name: &'a str,
+    second_name: &'a str,
 }
 
 fn main() {
     let my_book_title = "my_book_title".to_string();
+    let second_name1 = "young".to_string();
 
     let my_book = Book {
         name: &my_book_title,
+        second_name: &second_name1,
     };
 }
