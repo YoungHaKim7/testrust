0a1771654a2eb0dcbc6afd6634496278995ec3ab
YoungHa Kim <ytok1108@aol.com>
2022-02-19 16:58:53 +0900

    Kor093 Interior mutability and Cell

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,23 @@
-// interior mutability
-// chaging on the inside
-
-// & immutable reference -> 정확한 표현은shared reference 여러가지를 쓸 수 있음
-// &mut mutable reference -> 정확한 표현은 unique reference 딱 한개만 쓸 수 있다.
-
-// 이거외에 다른게 쓸려고 만들게 cell 이걸 쓰면 mut를 안 써도 바꿀 수 있다.
+use std::cell::Cell;
+// Cell<T>
 // Cell - small room
 
-// cell 외에
-// RefCell
-// Mutex
-// RwLock
+struct PhoneModel {
+    company_name: String,
+    model_name: String,
+    screen_size: f32,
+    memory: usize,
+    date_issued: u32,
+    on_sale: Cell<bool>,
+}
 
-// not thread safe
-// let mut x = 9; // 11
-// thread 1 {
-//     x +=1;
-// }
-// thread 2 {
-//     x +=1;
-// }
-
-fn main() {}
+fn main() {
+    let super_phone_3000 = PhoneModel {
+        company_name: "YY Electronics".to_string(),
+        model_name: "Super Phone 3000".to_string(),
+        screen_size: 7.5,
+        memory: 4_000_000,
+        date_issued: 2020,
+        on_sale: Cell::new(true),
+    };
+}
