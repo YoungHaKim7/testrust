2b5f53773242ade08f7e866f2346d9551802a517
YoungHa Kim <ytok1108@aol.com>
2022-02-21 19:17:26 +0900

    KOr095_RefCell again Result: RefCell { value: "I am a String" } RefCell { value: "I am not a String" }

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,8 @@
-// panic의 정의 : 프로그램을 그만 하는거 , 어떤 방에 들어갈을 때 이 방은 위험한것 같아 하면서 그만두는거임.
-// 위의 정의를 unwind the stack, OS에게 프로그램을 돌려주는거임.!!
-// RefCell 정의 : runtime checked borrwing rules
 use std::cell::RefCell;
 
-// unwind the stack
-#[derive(Debug)]
-struct User {
-    id: u32,
-    year_registered: u32,
-    username: String,
-    active: RefCell<bool>,
-    // Many other fields
-}
-
-fn main() {
-    let user_1 = User {
-        id: 1,
-        year_registered: 2020,
-        username: "User 1".to_string(),
-        active: RefCell::new(true),
-    };
-
-    println!("{user_1:?}");
-    let mut first_reference = user_1.active.borrow_mut();
-
-    println!("{user_1:?}");
-    *first_reference = false;
-    println!("{user_1:?}");
-    drop(first_reference);
-
-    println!("{user_1:?}");
+fn main () {
+    let my_cell = RefCell::new(String::from("I am a String"));
+    println!("{my_cell:?}");
+    *my_cell.borrow_mut() = String::from("I am not a String");
+    println!("{my_cell:?}");
 }
