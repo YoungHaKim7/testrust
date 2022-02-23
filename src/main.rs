5fefc46cee7f5fa8460f3e09e53fa937c7b3b5c6
YoungHa Kim <ytok1108@aol.com>
2022-02-23 20:48:04 +0900

    Kor097_Rc_reference counting_

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 // Rc
+// code 가 막혔을때 clone을 쓰지 말고 차라리 Rc를 써라
+// clone은 anti-type이라고 해서 예상치 못한 오류가 생길 수 있다.
 use std::rc::Rc;
 
-fn takes_a_string(input: String) {
-
+fn takes_a_string(input: Rc<String>) {
+    println!("{input}");
 }
 
-fn also_takes_a_string(input: String) {
-
+fn also_takes_a_string(input: Rc<String>) {
+    println!("{input}");
 }
 
 fn main() {
-    let my_string = "Hello there".to_string();
-    takes_a_string(my_string);
-    also_takes_a_string(my_string);
+    let my_string = Rc::new("Hello there".to_string());
+    takes_a_string(Rc::clone(&my_string));
+    also_takes_a_string(Rc::clone(&my_string));
+    
 }
