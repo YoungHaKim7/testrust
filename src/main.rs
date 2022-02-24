554029739c3601a6fb96d9154f25dacb257c216b
YoungHa Kim <ytok1108@aol.com>
2022-02-24 21:34:48 +0900

    Kor098_Rc example__result: Calgary's history is : Calgary was founded in blah blah blah

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,28 @@
+use std::rc::Rc;
+
 #[derive(Debug)]
 struct City {
     name: String,
     population: u32, 
-    history: String
+    history: Rc<String>
 }
 
 #[derive(Debug)]
 struct CityData {
     names: Vec<String>,
-    histories: Vec<String>
+    histories: Vec<Rc<String>>
 }
 
 fn main() {
     let calgary = City {
         name: "Calgary".to_string(),
         population: 1_336_000,
-        history: "Calgary was founded in blah blah blah".to_string()
+        history: Rc::new("Calgary was founded in blah blah blah".to_string())
     };
 
     let canada_cities = CityData {
         names: vec![calgary.name],
-        histories:vec![calgary.history]
+        histories:vec![Rc::clone(&calgary.history)]
     };
     println!("Calgary's history is : {}", calgary.history);
 }
