1c596e2f24a3f60c30c70d0b54e22d8dccb117d4
YoungHa Kim <ytok1108@aol.com>
2022-02-25 20:15:54 +0900

    Rc and RefCell example002

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
+// bring into scope
+use std::cell::RefCell;
+use std::rc::Rc;
+
 #[derive(Debug)]
-struct DataContainer<'a> {
-    data: &'a mut String
+struct DataContainer {
+    data: Rc<RefCell<String>>
 }
 
 fn main() {
-    let mut important_data = "Super duper important data".to_string();
+    let important_data = Rc::new(RefCell::new("Super duper important data".to_string()));
 
-    let mut container_1 = DataContainer {
-        data: &mut important_data
+    let container_1 = DataContainer {
+        data: Rc::clone(&important_data)
     };
 
-    let mut container_2 = DataContainer {
-        data: &mut important_data
+    let  container_2 = DataContainer {
+        data: Rc::clone(&important_data)
     };
 
     for _ in 0..10 {
-        *container_1.data = String::from("Hi");
-        *container_2.data = String::from("Hi there!");
+        *container_1.data.borrow_mut() = String::from("Hi");
+        *container_2.data.borrow_mut() = String::from("Hi there!");
     }
+
 }
