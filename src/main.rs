9f29078331609ae5e694de2f6036e45f86a43965
YoungHa Kim <ytok1108@aol.com>
2022-02-25 19:41:29 +0900

    Kor099_Rc and RefCell example

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,12 @@
-use std::rc::Rc;
-
 #[derive(Debug)]
-struct City {
-    name: String,
-    population: u32, 
-    history: Rc<String>
-}
-
-#[derive(Debug)]
-struct CityData {
-    names: Vec<String>,
-    histories: Vec<Rc<String>>
+struct DataContainer {
+    data: String
 }
 
 fn main() {
-    let calgary = City {
-        name: "Calgary".to_string(),
-        population: 1_336_000,
-        history: Rc::new("Calgary was founded in blah blah blah".to_string())
-    };
+    let important_data = "Super duper important data".to_string();
 
-    let canada_cities = CityData {
-        names: vec![calgary.name],
-        histories:vec![Rc::clone(&calgary.history)]
+    let container_1 = DataContainer {
+        data: important_data
     };
-    println!("Calgary's history is : {}", calgary.history);
-    
-    println!("Data has {} owners", Rc::strong_count(&calgary.history));
 }
-
