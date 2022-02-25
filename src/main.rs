b3e73dcb8ee7716aa1b78030617fd13d61f28690
YoungHa Kim <ytok1108@aol.com>
2022-02-25 19:46:56 +0900

    Kor099_mut lifetime

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 #[derive(Debug)]
-struct DataContainer {
-    data: String
+struct DataContainer<'a> {
+    data: &'a mut String
 }
 
 fn main() {
-    let important_data = "Super duper important data".to_string();
+    let mut important_data = "Super duper important data".to_string();
 
-    let container_1 = DataContainer {
-        data: important_data
+    let mut container_1 = DataContainer {
+        data: &mut important_data
+    };
+
+    let mut container_2 = DataContainer {
+        data: &mut important_data
     };
 }
