942a5ad09ad7581f2cef659b2805e09cfa6b12c1
YoungHa Kim <ytok1108@aol.com>
2022-02-27 18:11:01 +0900

    type alias and impl __ ex)

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,17 @@
 // type aliases
 // alias = different name
 
+use std::marker::StructuralEq;
+
 type SkipAndTake = std::iter::Take<std::iter::Skip<std::vec::IntoIter<char>>>;
+//newtype
+type MyString = String;
+type MyOtherString(String);
+
+impl SomeTrait for MyOtherString {
+
+}
+
 
 fn skip_five_take_five(input: Vec<char>) -> SkipAndTake {
     input.into_iter().skip(5).take(5)
