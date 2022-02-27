5b0f67fbc2e403faf60c3f8143c165bcb05919c1
YoungHa Kim <ytok1108@aol.com>
2022-02-27 17:51:55 +0900

    type alias

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 // type aliases
 // alias = different name
 
-type SkipAndTake = std::iter::Take<std::iter::Skip<std::vec::IntoIter<char>>> 
+type SkipAndTake = std::iter::Take<std::iter::Skip<std::vec::IntoIter<char>>>;
 
 fn skip_five_take_five(input: Vec<char>) -> SkipAndTake {
     input.into_iter().skip(5).take(5)
