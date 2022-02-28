09e745702340dafa368fac6b6b4f2dcb3eb53771
YoungHa Kim <ytok1108@aol.com>
2022-02-28 21:11:51 +0900

    Kor102_Cow Result: The cow is borrowed Remainder is 1 The cow is owned Remainder is 2 The cow is borrowed Remainder is 0 The cow is borrowed Remainder is 1 The cow is owned Remainder is 2 The cow is borrowed Remainder is 0

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
-// todo!() - I'll do it later
+use std::borrow::Cow;
 
-struct SomeType {
-    name: String,
-    number: u8
+fn module_3(input: u8) -> Cow<'static, str> {
+    match input % 3 {
+        0 => "Remainder is 0".into(),
+        1 => "Remainder is 1".into(),
+        remainder => format!("Remainder is {remainder}").into(),
+    }
 }
 
-fn some_function() -> Vec<SomeType> {
-    todo!();
-    // 예전에는 unimplemented!(); 이거였는데 많이 써서 todo!가 만들어짐 ㅋ:w
-}
+fn main() {
+    for number in 1..=6 {
+        match module_3(number) {
+            Cow::Borrowed(message) => println!("The cow is borrowed {message}"),
+            Cow::Owned(message) => println!("The cow is owned {message}"),
 
-
-fn main () {
-    some_function();
+        }
+    }
 }
