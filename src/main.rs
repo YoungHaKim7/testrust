0fa2a171a15fa8c45d2a4493071362d16a01482d
YoungHa Kim <ytok1108@aol.com>
2022-03-01 16:58:34 +0900

    thread__code 2 ex___

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::thread;
 
 fn main() {
+    // std::thread(|| {
+    //     println!("One second has passed");
+    // });
     let mut join_vec = vec![];
 
     for _ in 0..10 {
@@ -8,7 +11,10 @@ fn main() {
         join_vec.push(handle);
     }
 
-    join_vec.into_iter().for_each(|handle| handle.join().unwrap());
+    // 기다려야한다고 생각되면 밑에 코드를 추가
+    join_vec
+        .into_iter()
+        .for_each(|handle| handle.join().expect("Join Handle"));
 }
 
 
