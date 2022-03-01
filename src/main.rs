be4d21bb499e4c7c543025fd29930c31f05f262f
YoungHa Kim <ytok1108@aol.com>
2022-03-01 16:53:48 +0900

    Kor103_multiple threads__handle

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use std::thread;
 
 fn main() {
-    let join_handle = thread::spawn(|| {
-        println!("I am printing something");
-    });
+    let mut join_vec = vec![];
 
-    join_handle.join(); // wait
+    for _ in 0..10 {
+        let handle = thread::spawn(|| println!("I am printing something"));
+        join_vec.push(handle);
+    }
+
+    join_vec.into_iter().for_each(|handle| handle.join().unwrap());
 }
 
 
