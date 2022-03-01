61e2d0d1992bbd02f461232eabdf9a3019c390ea
YoungHa Kim <ytok1108@aol.com>
2022-03-01 16:50:11 +0900

    Kor103_Multiple threads__003

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
-// multiple threadscfg!(
-// !green threads --Go 가 쓰는 거 가짜 thread
-// rust는 진짜 thread를 쓴다.
 use std::thread;
 
 fn main() {
-    thread::spawn(|| {
+    let join_handle = thread::spawn(|| {
         println!("I am printing something");
-    }); // spawn 파생하다라는 뜻.
-    for _ in 0..10000 {
-        let x = 8;
-    }
+    });
+
+    join_handle.join(); // wait
 }
 
 
