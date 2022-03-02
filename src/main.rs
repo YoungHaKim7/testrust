59c2ac879eb6e87642ffef7a6fe7e1ba6b8ef166
YoungHa Kim <ytok1108@aol.com>
2022-03-02 21:10:57 +0900

    closure may outlive the current function, but it borrows __our_struct__, which is owned by the current function

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,32 @@
-use std::cell::RefCell;
+use std::sync::Mutex;
 use std::thread;
+
+// Mutex
+// Mutual exclusion
 
 trait CoolTrait {
     fn cool_function(&self);
 }
 
 struct OurStruct {
-    data: RefCell<u8>,
+    data: Mutex<u8>,
 }
 
 impl CoolTrait for OurStruct {
     fn cool_function(&self) {
-        *self.data.borrow_mut() += 1;
+        *self.data.lock().unwrap() += 1;
     }
 }
 
 fn main() {
     let our_struct = OurStruct {
-        data: RefCell::new(0)
+        data: Mutex::new(0),
     };
 
     let mut join_vec = vec![];
     for _ in 0..10 {
         let join_handle = thread::spawn(|| {
-            *our_struct.data.borrow_mut() += 1;
+            *our_struct.data.lock().unwrap() += 1;
         });
         join_vec.push(join_handle);
     }
