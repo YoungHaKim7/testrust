bac7c6713a56b116b47fe9f5c67f55adc2b63872
YoungHa Kim <ytok1108@aol.com>
2022-03-03 20:17:03 +0900

    Kor105_Arc with Mutex002__good arc__

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,36 @@
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
 use std::thread;
 
 // Arc<Mutex>
-// Mutex
-// Mutual exclusion
+// atomic reference counter
+// operating system primitives
 
 trait CoolTrait {
     fn cool_function(&self);
 }
 
 struct OurStruct {
-    data: Mutex<u8>,
+    data: Arc<Mutex<u8>>,
 }
 
 impl CoolTrait for OurStruct {
     fn cool_function(&self) {
-        let lock = self.data.lock().unwrap();
-        drop(lock);
         *self.data.lock().unwrap() += 1;
     }
 }
 
 fn main() {
     let our_struct = OurStruct {
-        data: Mutex::new(0),
+        data: Arc::new(Mutex::new(0)),
     };
 
     let mut join_vec = vec![];
+
     for _ in 0..10 {
+        let clone = Arc::clone(&our_struct.data); // Arc<Mutex<u8>>
         let join_handle = thread::spawn(move || {
             // move = take by value
-            *our_struct.data.lock().unwrap() += 1;
+            *clone.lock().unwrap() += 1;
         });
         join_vec.push(join_handle);
     }
