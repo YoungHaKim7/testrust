9802146b85b0a3efb5e482b86ba68772047f0c4c
YoungHa Kim <ytok1108@aol.com>
2022-03-04 20:25:36 +0900

    RwLock__try_read()

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use std::sync::{Mutex, RwLock}; // Read Write (Rw)
 fn main () {
     let my_rwlock = RwLock::new(5);
 
-    let read1 = my_rwlock.read().unwrap(); // lock 
-    let read2 = my_rwlock.read().unwrap();
+    let read1 = my_rwlock.write().unwrap(); // lock 
+    let read2 = my_rwlock.try_read().unwrap();
 
     println!("{read1:?},{read2:?}");
 }
