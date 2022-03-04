57bfcd33e1448d3003bfec16b066cf597fac2996
YoungHa Kim <ytok1108@aol.com>
2022-03-04 20:14:29 +0900

    Kor106__Mutex and RwLock002__val.try_lock(); result : Mutex { data: <locked>, poisoned: false, .. }

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,3 @@
-//
-// Cell 젤 간단하고 젤 빠름 multiful thread 가 필요 없을 경우 사용하면 된다.
-//
-// RefCell -> Thread safe (x)ㄱㅏ 아님 (더 빠름)
-// Mutex -> Thread safe (o)
-//
-// Rc - Thread safe (x) Rc 가 더 빠름
-// Arc - > Thread safe (o)
-// Arc has more overhead  뭔가 더 복잡하다는 뜻
-//
 // Mutex and RwLock
 use std::sync::{Mutex, RwLock}; // Read Write (Rw)
 
@@ -15,7 +5,6 @@ fn main () {
     let my_mutex = Mutex::new(5);
 
     let mut mutex_changer = my_mutex.lock().unwrap();
-    *mutex_changer = 6;
-    drop(mutex_changer);
+    let mut other_mutex_changer = my_mutex.try_lock();
     println!("{my_mutex:?}");
 }
