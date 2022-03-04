986633a2642e9ade45c8c99b3aaecfe1a7bbc7e8
YoungHa Kim <ytok1108@aol.com>
2022-03-04 20:20:14 +0900

    Kor106__Mutex and RwLock002__ if let Ok(valu)____result: Didn't get a lock Mutex { data: <locked>, poisoned: false, .. }

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,12 @@ fn main () {
 
     let mut mutex_changer = my_mutex.lock().unwrap();
     let mut other_mutex_changer = my_mutex.try_lock();
+    if let Ok(value) = other_mutex_changer {
+        println!("The mutex_changer has: {value}");
+    }
+        else {
+        println!("Didn't get a lock");
+        }
+
     println!("{my_mutex:?}");
 }
