e8bbf99dd303eb26b1f3c9d56be3f7e287b19e48
YoungHa Kim <ytok1108@aol.com>
2022-03-05 19:50:12 +0900

    Kor107_Clippy 002  before__bad code__Before---

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,27 @@ fn print_vec_ref(input: &Vec<i32>) {
     }
 }
 
+// cargo clippy
 fn main() {
     let my_vec = vec![8, 9, 10];
     print_vec_ref(&my_vec);
+
+    let mut done = false;
+    let mut counter = 0;
+
+    while done == false {
+        counter += 1;
+
+        if counter > 10 {
+            done = true;
+        }
+    }
+
+    let some_variable = Some(9);
+
+    match some_variable {
+        Some(number) => println!("We got a {number}"),
+        _ => {}
+    }
 }
 
