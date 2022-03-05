b0da2db7608d947f2b0033239097d4594513e149
YoungHa Kim <ytok1108@aol.com>
2022-03-05 20:07:22 +0900

    Clippy___bad code__mismatched types_

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,14 @@ fn print_vec_ref(input: &Vec<i32>) {
         println!("Vec is empty");
     } else {
         input.iter().for_each(|num| println!("{num}"));
-        }
-    
+    }
 }
 
 fn main() {
+    let my_vec = vec![8, 9, 10];
+    let my_array = [8, 9, 10];
+
+    print_vec_ref(&my_vec);
+    print_vec_ref(&my_array);
 }
 
