523aa5bc7e010115c91b9d83a250a1d741ae16c3
YoungHa Kim <ytok1108@aol.com>
2022-03-05 20:04:52 +0900

    Clippy___bad code___

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,14 @@
 // clippy = linter
 //
 fn print_vec_ref(input: &Vec<i32>) {
-    if input.is_empty() {
+    if input.len() == 0 {
         println!("Vec is empty");
     } else {
-        for num in input {
-            println!("{num}");
+        input.iter().for_each(|num| println!("{num}"));
         }
-    }
+    
 }
 
-// cargo clippy
 fn main() {
-    let my_vec = vec![8, 9, 10];
-    print_vec_ref(&my_vec);
-
-    let mut done = false;
-    let mut counter = 0;
-
-    // while done == false 는 while true라는 뜻인데 //while done == false는 안 좋은 code
-    while !done {
-        counter += 1;
-
-        if counter > 10 {
-            done = true;
-        }
-    }
-
-    let some_variable = Some(9);
-
-    if let Some(number) = some_variable {
-        println!("We got {number}");
-    }
 }
 
