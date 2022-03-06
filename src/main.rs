68f5fb2203c4d9cf7838315ef58c19705f80f715
YoungHa Kim <ytok1108@aol.com>
2022-03-06 16:59:43 +0900

    Regardless of the capacity, it has to be 40 bytes.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 struct SomeStruct {
     name: String,
     number: u8,
-    data: Box<[u8; 1000]>,
+    data: Box<[u8; 10000]>,
 }
 
 fn take_thing<T>(thing: T) {}
@@ -12,7 +12,7 @@ fn main() {
     let my_struct = SomeStruct {
         name: "Hi there".to_string(),
         number: 0,
-        data: Box::new([9; 1000])
+        data: Box::new([9; 10000]),
     };
 
     println!("The struct is {} bytes", std::mem::size_of_val(&my_struct));
