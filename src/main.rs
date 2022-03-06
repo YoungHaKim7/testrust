b22eb8bfbb8ae5e12f2e06b8f3b0bb743fe563f7
YoungHa Kim <ytok1108@aol.com>
2022-03-06 17:01:54 +0900

    Box is Smart Point

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 // Box - owned data on the heap
-//
+// Box = Smart Point
+
 struct SomeStruct {
     name: String,
     number: u8,
@@ -15,6 +16,10 @@ fn main() {
         data: Box::new([9; 10000]),
     };
 
-    println!("The struct is {} bytes", std::mem::size_of_val(&my_struct));
+    let my_box = Box::new(9);
+
+    println!("{my_box:?}");
+    println!("{}", *my_box);
+
 }
 
