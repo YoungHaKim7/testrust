30b7575f19c249f931249dc5898e0336f4a2a8e1
YoungHa Kim <ytok1108@aol.com>
2022-03-07 21:07:27 +0900

    Kor109 Trait objects and Box003

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
 trait Booky {}
 
-struct Book;
+struct Book {
+    name: String,
+}
 struct BigBook;
 
+struct City {
+    year_founded: i32
+}
 impl Booky for Book {}
 impl Booky for BigBook {}
+impl Booky for City {}
 
 // dyn = dynamically decided = runtime에서 결정함.
-fn main () {
-    let vec_of_booky_things: Vec<Box<dyn Booky>> = vec![Box::new(Book), Box::new(BigBook)];
-    }
+fn main() {
+    let my_city = City {
+        year_founded: 1989
+    };
+
+    let vec_of_booky_things: Vec<Box<dyn Booky>> = vec![
+        Box::new(Book {
+            name: "my_book".to_string(),
+        }),
+        Box::new(BigBook),
+        Box::new(my_city)
+    ];
+}
