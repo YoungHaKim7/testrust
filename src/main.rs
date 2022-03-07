001d6a785297f34a989f14c086d09cb99a7196dd
YoungHa Kim <ytok1108@aol.com>
2022-03-07 21:01:28 +0900

    Kor109 Trait objects and Box002

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ struct BigBook;
 impl Booky for Book {}
 impl Booky for BigBook {}
 
+// dyn = dynamically decided = runtime에서 결정함.
 fn main () {
-    let vec_of_booky_things: Vec<Booky> = vec![Book, BigBook];
+    let vec_of_booky_things: Vec<Box<dyn Booky>> = vec![Box::new(Book), Box::new(BigBook)];
     }
