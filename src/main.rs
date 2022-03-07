c8d0d90f855854ff46a6c7a5b24528febe9a3d8f
YoungHa Kim <ytok1108@aol.com>
2022-03-07 20:48:29 +0900

    recursive type __List__ has infinite size

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,9 @@
-// Box - owned data on the heap
-// Box = Smart Point
-
-struct SomeStruct {
-    name: String,
-    number: u8,
-    data: Box<[u8; 10000]>,
+struct List {
+    content: List
 }
+// recursive types
+//
 
-fn take_thing<T>(thing: T) {}
+fn main () {
 
-fn main() {
-    let my_struct = SomeStruct {
-        name: "Hi there".to_string(),
-        number: 0,
-        data: Box::new([9; 10000]),
-    };
-
-    let my_box = Box::new(9);
-
-    println!("{my_box}");
-    println!("{}", *my_box);
 }
-
