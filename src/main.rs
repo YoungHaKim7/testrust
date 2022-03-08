6417032d4589b691bc7097ef755f193813eb35a0
YoungHa Kim <ytok1108@aol.com>
2022-03-08 21:42:23 +0900

    Three types of generics003 result: Hi, I'm a 8 Hi, I'm a I am String Hi, I'm a 9 Hi, I'm 5 Hi, I'm I am String_Box_dyn

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,16 @@ use std::fmt::Display;
 fn print<T: Display>(input: T) {
     println!("Hi, I'm a {input}");
 }
+
+// fn print_i32<input: i32>(input: T) {
+//     println!("Hi, I'm a {input}");
+// }
+//
+// fn print_string<input: String>(input: T) {
+//     println!("Hi, I'm a {input}");
+// }
+//
+
 //impl trait
 // concrete (Compiler)
 fn print_2(input: impl Display) {
@@ -15,10 +25,14 @@ fn print_2(input: impl Display) {
 fn print_3(input: Box<dyn Display>) {
      println!("Hi, I'm {input}");
  }
+//vtable
 
 fn main () {
+
     print(8);
+    print(String::from("I am String"));
     print_2(9);
     print_3(Box::new(5));
+    print_3(Box::new("I am String_Box_dyn"));
 
 }
