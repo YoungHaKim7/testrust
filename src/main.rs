9ff9e76817b0e4d995c76310cb6285802f850519
YoungHa Kim <ytok1108@aol.com>
2022-03-08 21:32:56 +0900

    Three types of generics002

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
 // 3 Generics
-//
 use std::fmt::Display;
 
-
+// concrete (Compiler)
 fn print<T: Display>(input: T) {
     println!("Hi, I'm a {input}");
 }
+//impl trait
+// concrete (Compiler)
+fn print_2(input: impl Display) {
+    println!("Hi, I'm a {input}");
+}
+
+// dynamic (Runtime)
+fn print_3(input: Box<dyn Display>) {
+     println!("Hi, I'm {input}");
+ }
 
 fn main () {
     print(8);
+    print_2(9);
+    print_3(Box::new(5));
 
 }
