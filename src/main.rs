8d6b5836ceb0c5d49ac843fa9d661193c9b78368
YoungHa Kim <ytok1108@aol.com>
2022-03-09 15:32:10 +0900

    Kor111_part2___closure__3 kind of

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,13 @@ fn gives_five() -> u8 {
 
 
 
-fn gives_six() -> u8 {
+fn gives_six() -> impl FnMut() {
     6
 }
+// closure 가 쓰는 3가지 trait
+// Fn() -> u8 // reference (&self)
+// FnMut() ->u8 // can mutate (&mut self)
+// FnOnce() ->u8 // can be used once (self)1회용으로 끝남
 
 fn add_to_funtion_output(my_function: fn() -> u8, some_number: u8) {
     let my_number = my_function();
