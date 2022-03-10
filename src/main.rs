62fcd91bd1b65d8900504eb0621a51f05f907cdc
YoungHa Kim <ytok1108@aol.com>
2022-03-10 20:15:09 +0900

    Kor112_closure__basic result: Hello there

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,14 @@
-fn gives_five() -> u8 {
-    5
-}
-
-
-
-fn gives_six() -> impl FnMut() {
-    6
-}
-// closure 가 쓰는 3가지 trait
-// Fn() -> u8 // reference (&self)
-// FnMut() ->u8 // can mutate (&mut self)
-// FnOnce() ->u8 // can be used once (self)1회용으로 끝남
-
-fn add_to_funtion_output(my_function: fn() -> u8, some_number: u8) {
-    let my_number = my_function();
-    let next_number = my_number + some_number;
-    println!("We got {next_number}");
-}
+// Fn (&Self)
+// FnMut (&mut self)
+// FnOnce
+//
 
 fn main () {
-    add_to_funtion_output(gives_five,8);
-    add_to_funtion_output(gives_six,8);
+    let my_string = String::from("Hello there");
+
+    let print_it = || {
+        println!("{my_string}");
+    };
+
+    print_it();
 }
