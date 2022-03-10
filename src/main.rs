deccf444044be8735ea468790140aac335aaed4b
YoungHa Kim <ytok1108@aol.com>
2022-03-10 20:26:03 +0900

    Kor112_Fn_FnMut_FnOnce_004__error code cannot borrow _f_ as mutable, as it is not declared as mutable

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
-fn fn_closure<F>(f: F) {
+fn fn_closure<F>(f: F)
+where
+    F: Fn(),
+{
     f();
 }
 
-fn fn_mut_closure<F>(f: F) {
+fn fn_mut_closure<F>(f: F)
+where
+    F: FnMut(),
+{
     f();
 }
 
-fn fn_once_closure<F>(f: F) {
+fn fn_once_closure<F>(f: F)
+where
+    F: FnOnce(),
+{
     f();
 }
 
-fn main () {
+fn main() {
     let my_string = String::from("Hello there");
 }
