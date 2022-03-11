99cc9302950e8498a794eb28b2e556ad3e183a69
YoungHa Kim <ytok1108@aol.com>
2022-03-11 19:54:57 +0900

    Kor113_About_impl_Trait001__after_sameCode

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 // impl trait
+
 // 
-fn returns_a_closure() -> Box<dyn Fn(i32)> {
-    Box::new(|x| println!("{x}"))
+// fn returns_a_closure() -> Box<dyn Fn(i32)> {
+//     Box::new(|x| println!("{x}"))
+// }
+// same code (About impl Trait)
+fn returns_a_closure() -> impl Fn(i32) {
+    |x| println!("{x}")
 }
+
 
 fn main () {}
