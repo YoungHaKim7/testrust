c39d1a9a1b600512fccd517165c7714b09cc1b88
YoungHa Kim <ytok1108@aol.com>
2022-03-11 20:12:14 +0900

    Generic Function VS. impl_function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 // impl trait
 use std::fmt::Display;
 
+// caller 가 정함.펑션을 만든사람이 정함
 fn generic_function<T: Display>(input: T) {
     println!("{input}");
 }
 
+// 컴퓨터가 알아서 정함 ㅎㅎ
 fn impl_function(input: impl Display) {
     println!("{input}")
 }
