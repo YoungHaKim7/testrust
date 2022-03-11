4d4f62ba63accb310bddf7e9530dab60d75c710c
YoungHa Kim <ytok1108@aol.com>
2022-03-11 20:21:57 +0900

    impl Trait____The code that I use the most in work.

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@
 use std::fmt::Display;
 
 // caller 가 정함.펑션을 만든사람이 정함
+// 이론적으로는 무조건 잘 알아야할 Generic!!!!
 fn generic_function<T: Display>(input: T) {
     println!("{input}");
 }
-type MyString = impl Display;
 
 // 컴퓨터가 알아서 정함 ㅎㅎ
+// 실전에서 젤 많이 쓰는 코드
 fn impl_function(input: impl Display) {
     println!("{input}")
 }
