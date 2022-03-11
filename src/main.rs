71d6a0331f51febc2e6d189aca72da59db297514
YoungHa Kim <ytok1108@aol.com>
2022-03-11 19:59:25 +0900

    Kor113_About_impl_Trait003

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,12 @@
 // impl trait
+use std::fmt::Display;
 
-// 
-// fn returns_a_closure() -> Box<dyn Fn(i32)> {
-//     Box::new(|x| println!("{x}"))
-// }
-// same code (About impl Trait)
-fn returns_a_closure() -> impl Fn(i32) {
-    |x| println!("{x}")
+fn generic_function<T: Display>(input: T) {
+    println!("{input}");
 }
 
-
+fn impl_function(input: impl Display) {
+    println!("{input}")
+}
 fn main () {
-    let my_number = 9;
-    let my_closure = returns_a_closure();
-    my_closure(my_number);
 }
