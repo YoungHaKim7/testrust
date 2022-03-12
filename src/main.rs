07c5840b80848495a364de8c923bf17b6808bc82
YoungHa Kim <ytok1108@aol.com>
2022-03-12 21:25:11 +0900

    _some_char

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 // hint to the compiler
-#[allow(dead_code)]
-#[allow(unused_variables)]
+// #[allow(dead_code)]
+// #[allow(unused_variables)]
 // #[warn(dead_code)]
 struct JustAStruct {}
 
 fn main() {
-    let some_char = 'a';
+    let _some_char = 'a';
 }
