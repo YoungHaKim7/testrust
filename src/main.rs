0b7e624b48f24a4307f22a6379e07f82fab66ef3
YoungHa Kim <ytok1108@aol.com>
2022-03-14 19:53:12 +0900

    Kor116___Cow Part2___Owned Type

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 // Cow 공식 문서 볼때 보라색 표시는 trait
 
 //
-fn main () {
-
+fn main() {
     let string_1 = String::from("Hello there"); // From Trait
     let string_2 = "Hello there".to_string(); // Display Trait
     let string_3: String = "Hello there".into(); // From
-    let string_4 = "Hello there".to_owned(); // ToOwned Trait
+    let string_4 = "Hello there".to_owned(); // ToOwned Trait , str -> String이 아닌 다른 타입으로
+    // 바꾸고 싶을때 씀 Owned Type? Clone할 때 씀
     // str -> String
-
 }
