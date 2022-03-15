686c10481b78f9af66c9a861dbfe553ad6ad7b22
YoungHa Kim <ytok1108@aol.com>
2022-03-15 20:58:38 +0900

    Kor117_Deref trait__ Sample Ex)__

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,29 @@
-// Deref trait
-struct HoldsANumber(u8);
+use std::ops::Deref;
 
+struct DerefExample<T> {
+    value: T
+}
+
+impl<T> Deref for DerefExample<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.value
+    }
+}
 
 fn main () {
-    let my_number = HoldsANumber(20);
-    println!("{}", *my_number + 20);
+let x = DerefExample { value: 'a' };
+assert_eq!('a', *x);
+
 }
+
+// Deref trait
+// struct HoldsANumber(u8);
+//
+
+// 
+// fn main () {
+//     let my_number = HoldsANumber(20);
+//     println!("{}", *my_number + 20);
+// }
