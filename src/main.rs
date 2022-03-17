d1e43ef4944aa21582d02a97076ef47bec372395
YoungHa Kim <ytok1108@aol.com>
2022-03-17 22:17:12 +0900

    Kor119_Cow in practice002__result: User 1 is User { name: ___User 1___ } and User 2 is User { name: __User 2__ }

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
-struct User {
-    name: String
+use std::borrow::Cow;
+
+#[derive(Debug)]
+struct User<'a> {
+    name: Cow<'a, str>,
 }
 
-impl User {
-    fn new(input: &str) -> Self {
-        Self {
-            name: input.to_string()
-        }
-    }
-}
-
-fn main () {
+fn main() {
     let name_1 = "User 1";
     let name_2 = "User 2".to_string();
 
-    let my_user = User::new(name_1);
-    let my_user2 = User::new(&name_2);
+    let user_1 = User {
+        name: name_1.into(),
+    };
+
+    let user_2 = User {
+        name: name_2.into(),
+    };
+
+    println!("User 1 is {user_1:?} and User 2 is {user_2:?}");
 }
