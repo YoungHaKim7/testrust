4f4f713e18ad59480ef1f6843fc47367f50eccf7
YoungHa Kim <ytok1108@aol.com>
2022-03-20 00:28:06 +0900

    Kor121_Add trait002 result: Nauru + Vanuatu = Country { name: __Nauru and Vanuatu__, population: 318485, gdp: 980000000 }

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,38 @@
-struct Book; 
+use std::ops::Add;
 
+#[derive(Debug)]
+struct Country {
+    name: String,
+    population: u32,
+    gdp: u32,
+}
 
+impl Country {
+    fn new(name: &str, population: u32, gdp: u32) -> Self {
+        Self {
+            name: name.to_string(),
+            population,
+            gdp,
+        }
+    }
+}
 
-// + Add
-// - Sub
-// += AddAssign
+impl Add for Country {
+    type Output = Self;
 
-fn main () {
-    let my_book = Book;
-    let second_book = Book;
-    let third_item = my_book + second_book;
+    fn add(self, other: Self) -> Self {
+        Self {
+            name: format!("{} and {}", self.name, other.name),
+            population: self.population + other.population,
+            gdp: self.gdp + other.gdp
+        }
+    }
+}
+fn main() {
+    let nauru = Country::new("Nauru", 10_670, 160_000_000);
+    let vanuatu = Country::new("Vanuatu", 307_815, 820_000_000);
+    let micronesia = Country::new("Micronesia", 104_468, 367_000_000);
+
+    println!("Nauru + Vanuatu = {:?}", nauru + vanuatu);
 
 }
