c934f122811ba03d1a5b2b486f7aa7e5900077b3
YoungHa Kim <ytok1108@aol.com>
2022-03-20 17:29:01 +0900

    Kor122_Default_trait_004

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,35 @@
-// Dafault
+struct Character {
+    name: String,
+    age: u8,
+    height: u32,
+    weight: u32,
+    lifestate: Lifestate,
+}
+
+enum Lifestate {
+    Alive,
+    Dead,
+    NeverAlice,
+    Uncertain,
+}
+
+impl Character {
+    fn new(name: String, age: u8, height: u32, weight: u32, alive: bool) -> Self {
+        Self {
+            name,
+            age,
+            height,
+            weight,
+            lifestate: if alive {
+                Lifestate::Alive
+            } else {
+                Lifestate::Dead
+            },
+        }
+    }
+}
 
 fn main() {
-    println!(
-        "{}, {}, {}, {}",
-        i32::default(),
-        String::default(),
-        f32::default(),
-        char::default()
-    );
+    let npc_1 = Character::new("Billy".to_string(), 15, 170, 70, true);
 }
 
