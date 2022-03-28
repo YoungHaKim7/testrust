cb0c10f2a1af75b29dd7a0738ad8bf7583eda2f4
YoungHa Kim <ytok1108@aol.com>
2022-03-28 19:51:28 +0900

    Kor130_Refactoring 001

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
-#![allow(dead_code)]
-// clippy::cargo 설명
-// crate1 -> crate2 0.1
-// crate3 -> crate2 0.2
-// #![warn(
-//     clippy::pedantic,
-//     clippy::nursery,
-//     clippy::cargo,
-// )]
+#[derive(Clone)]
+struct Calculator {
+    results: Vec<String>,
+    current_input: String,
+    total: i32,
+    adds: bool,
+}
+
+impl Calculator {
+    fn new() -> Self {
+        Self {
+            results: vec![],
+            current_input: String::new(),
+            total: 0,
+            adds: true,
+        }
+    }
+}
+
+// Let's see what breaks
+
+fn main() {
+    let mut calculator = Calculator::new();
+    math("1 + 1");
+}
 
 const OKAY_CHARACTERS: &str = "1234567890+- ";
 
@@ -23,43 +39,42 @@ fn math(input: &str) -> i32 {
         .chars()
         .filter(|character| *character != ' ')
         .collect::<String>();
-    println!("{input}");
-    let mut result_vec = vec![];
-    let mut push_string = String::new();
+    let mut calculator = Calculator::new();
+
     for character in input.chars() {
         match character {
             '+' => {
-                if !push_string.is_empty() {
+                if !calculator.current_input.is_empty() {
                     // ""
-                    result_vec.push(push_string.clone());
-                    push_string.clear();
+                    calculator.results.push(calculator.current_input.clone());
+                    calculator.current_input.clear();
                 }
             }
             '-' => {
-                if push_string.contains('-') || push_string.is_empty() {
-                    push_string.push(character);
+                if calculator.current_input.contains('-') || calculator.current_input.is_empty() {
+                    calculator.current_input.push(character);
                 } else {
-                    result_vec.push(push_string.clone());
-                    push_string.clear();
-                    push_string.push(character);
+                    calculator.results.push(calculator.current_input.clone());
+                    calculator.current_input.clear();
+                    calculator.current_input.push(character);
                 }
             }
             number => {
-                if push_string.contains('-') {
-                    result_vec.push(push_string.clone());
-                    push_string.clear();
-                    push_string.push(number);
+                if calculator.current_input.contains('-') {
+                    calculator.results.push(calculator.current_input.clone());
+                    calculator.current_input.clear();
+                    calculator.current_input.push(number);
                 } else {
-                    push_string.push(number);
+                    calculator.current_input.push(number);
                 }
             }
         }
     }
-    result_vec.push(push_string);
+    calculator.results.push(calculator.current_input);
     // vec!["1".to_string(), "-", "20"];
     let mut total = 0; // Now it's time to do math. Start with a total
     let mut adds = true; // true = add, false = subtract
-    let math_iter = result_vec.into_iter();
+    let math_iter = calculator.results.into_iter();
     for entry in math_iter {
         // Iter through the item
         if entry.contains('-') {
@@ -108,8 +123,4 @@ mod tests {
     fn panics_when_characters_not_right() {
         math("7 + please add seven");
     }
-}
-
-fn main() {
-    let my_number = math("7- + 9 -+ 10     +++----++++");
 }
