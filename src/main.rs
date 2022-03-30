a9cab9ced966dd1477a7c33ea715595dd68faf21
YoungHa Kim <ytok1108@aol.com>
2022-03-30 19:39:49 +0900

    Kor132_The Any Trait003_result: First type: i32 second type: bool First type: alloc::vec::Vec<char> second type: testrust::MyType

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,16 @@ use std::any::{Any, type_name};
 
 struct MyType;
 
-fn get_type_name<T: Any>(input: T) {
-    let my_type = type_name::<T>();
-    println!("{my_type}");
+fn get_type_names<T: Any, U: Any>(_: T, _: U) {
+    let type_of_t = type_name::<T>();
+    let type_of_u = type_name::<U>();
+    println!("First type: {type_of_t}");
+    println!("second type: {type_of_u}");
+
 
 }
 
 fn main() {
-    get_type_name(8);
-    get_type_name(vec![8]);
-    get_type_name(MyType);
+    get_type_names(8, true);
+    get_type_names(vec!['a'], MyType);
 }
