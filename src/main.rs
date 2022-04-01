9b2d2fabe5ccafe20e9dace06a772583de55f934
YoungHa Kim <ytok1108@aol.com>
2022-04-01 21:42:44 +0900

    Kor134__nightly 1.61.0_Rust tutorial__

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+// nightly 1.61.0 rust사용하는 방법 unstable을 미리 써 볼 수 있다.
+#![feature(panic_internals)]
 use std::panic::{set_hook, take_hook};
 
 fn main() {
     let mut impotant_code = 400;
 
     set_hook(Box::new(|panic_info| {
+        panic_info.set_payload(9);
         println!("Didn't get a 200 code yet");
         println!(
             "Panic info: {:?}",
