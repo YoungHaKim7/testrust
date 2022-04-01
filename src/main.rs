da2f5008b563d794f4aef119c1a55bd3af07f0b2
YoungHa Kim <ytok1108@aol.com>
2022-04-01 21:01:52 +0900

    Kor134_Panic hooks 001

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,5 @@
-use std::any::{type_name, Any};
-
-struct MyType;
-
-fn try_do_get_string(input: &dyn Any) {
-    // trait object
-    if let Some(a_string) = input.downcast_ref::<String>() {
-        println!("We got a String! {a_string}");
-    } else {
-        println!("We got something else");
-    }
-}
-
 fn main() {
-    try_do_get_string(&9);
-
-    try_do_get_string(&String::from("Hello there"));
+    let important_number = 9;
+    panic!("Oh the humanity!");
 }
+
