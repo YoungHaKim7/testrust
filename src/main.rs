13926dab1b766cd258db4168a8420dfcd4937201
YoungHa Kim <ytok1108@aol.com>
2022-04-01 21:23:16 +0900

    Kor134_Panic hooks 004 result: Didn't get a 200 code yet Panic info: Some(__Oh the humanity)

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,11 @@ fn main() {
     let mut impotant_code = 400;
     set_hook(Box::new(|panic_info| {
         println!("Didn't get a 200 code yet");
+        println!(
+            "Panic info: {:?}",
+            panic_info.payload().downcast_ref::<&str>()
+        );
     }));
+
     panic!("Oh the humanity!");
 }
