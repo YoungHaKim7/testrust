3214a2abd81e6c407bceaa826062e217465da9f2
YoungHa Kim <ytok1108@aol.com>
2022-04-02 22:29:51 +0900

    Kor135_Time__002 result: 597.25µs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use std::time::Instant;
 
 fn main() {
-    let now = Instant::now();
-    println!("{now:?}");
+    let time_1 = Instant::now();
+    for _ in 0..1000 {
+        let _ = String::from("I am a String to keep you busy");
+    }
+    let time_2 = Instant::now();
+    println!("{:?}", time_2 - time_1);
 }
-
