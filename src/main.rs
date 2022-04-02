e1bf06e9f34630242f53aa1298378bda480ea286
YoungHa Kim <ytok1108@aol.com>
2022-04-02 22:37:45 +0900

    Time__sleep___result: 5.000479791s 5.0007535s 5.000755791s

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use std::time::Instant;
+use std::thread::sleep;
+use std::time::{Duration, Instant};
 
 fn main() {
     let time_1 = Instant::now();
-
+    sleep(Duration::from_secs(5));
     println!("{:?}", time_1.elapsed()); // elapsed = time that passed
     println!("{:?}", time_1.elapsed()); // elapsed = time that passed
     println!("{:?}", time_1.elapsed()); // elapsed = time that passed
