ba7c0e63c0999b220f0eef33dfbe1f8d77e9eaa7
YoungHa Kim <ytok1108@aol.com>
2022-04-02 22:32:17 +0900

    Kor135_Time004_elapsed__time that passed__result: 41ns 110.375µs 113.083µs

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,6 @@ fn main() {
     let time_1 = Instant::now();
 
     println!("{:?}", time_1.elapsed()); // elapsed = time that passed
+    println!("{:?}", time_1.elapsed()); // elapsed = time that passed
+    println!("{:?}", time_1.elapsed()); // elapsed = time that passed
 }
