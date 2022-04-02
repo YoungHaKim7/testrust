e5f3c625df4a09d5618a2bffd1eb196abdada4b1
YoungHa Kim <ytok1108@aol.com>
2022-04-02 22:31:30 +0900

    Kor135_Time003_elapsed__time that passed__result: 41ns

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,6 @@ use std::time::Instant;
 
 fn main() {
     let time_1 = Instant::now();
-    for _ in 0..1000 {
-        let _ = String::from("I am a String to keep you busy");
-    }
-    let time_2 = Instant::now();
-    println!("{:?}", time_2 - time_1);
+
+    println!("{:?}", time_1.elapsed()); // elapsed = time that passed
 }
