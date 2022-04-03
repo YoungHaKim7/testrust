72e0042d0d7236371e4a3bb0a3621fac779e814d
YoungHa Kim <ytok1108@aol.com>
2022-04-03 22:00:56 +0900

    Kor136_Channels003__error code __Sender<i32>__ cannot be shared between threads safely

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,23 @@
 // mpsc
 // Multiple Producer Single Consumer
 use std::sync::mpsc::channel;
+use std::thread;
 
 fn main() {
     let (sender, receiver) = channel();
     sender.send(9).unwrap();
     let received = receiver.recv().unwrap();
     println!("{received}");
+
+    thread::spawn(|| {
+        sender.send(9).unwrap();
+    });
+
+    thread::spawn(|| {
+        sender.send(9).unwrap();
+    });
+
+        println!("{}", receiver.recv().unwrap();
+        println!("{}", receiver.recv().unwrap();
+
 }
