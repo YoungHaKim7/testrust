1128e5c1b2447f0f2e5895e562e7b88d8f08e1dd
YoungHa Kim <ytok1108@aol.com>
2022-04-04 20:49:48 +0900

    sleepy function__waiting 1sec..

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-// Channels
-// mpsc
-// Multiple Producer Single Consumer
 use std::sync::mpsc::channel;
 use std::thread;
+use std::thread::sleep;
+use std::time::Duration;
+
+fn sleepy(time: u64) {
+    sleep(Duration::from_millis(time));
+}
 
 fn main() {
     let (sender, receiver) = channel();
@@ -12,13 +15,15 @@ fn main() {
 
     thread::spawn(move || {
         // take by value
+        sleepy(1000); // 1 sec.
         s1.send(9).unwrap();
     });
 
     thread::spawn(move || {
+        sleepy(1000); // 1 sec.
         s2.send(9).unwrap();
     });
 
-    println!("{:?}", receiver.recv());
+    println!("{:?}", receiver.recv()); // blocking
     println!("{:?}", receiver.recv());
 }
