0dcbe6046b5d7c12f3e262ec8becb89013d2e77e
YoungHa Kim <ytok1108@aol.com>
2022-04-06 23:15:07 +0900

    Kor139_Errors__002__error code expected enum __Result<i32, ParseIntError>__           found unit type __()__

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::num::ParseIntError;
 
 fn try_to_make_number(input: &str) -> Result<i32, ParseIntError> {
-    input.parse::<i32>()
+    let my_number = input.parse::<i32>()?; // Ok(8) else return Err
+    let other_number = input.parse::<f32>()?;
 }
 
 fn main() {}
