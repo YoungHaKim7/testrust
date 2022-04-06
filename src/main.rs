1d396901b85ce45d81b94f99c8010838d43b3ca8
YoungHa Kim <ytok1108@aol.com>
2022-04-06 23:36:42 +0900

    Kor139_Errors__004__

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,37 @@
-use std::num::ParseIntError;
+use std::error::Error;
+use std::fmt::{Display, Formatter};
 
-fn try_to_make_number(int_input: &str, float_input: &str) -> Result<(), ParseIntError> {
-    let my_number = int_input.parse::<i32>()?; // Ok(8) else return Err
-    let other_number = float_input.parse::<f32>()?;
+// anyhow 이 복잡한걸 편하게 만드는 extern-crate
+#[derive(Debug)]
+enum CompanyError {
+    CouldntConnect,
+    NotEnoughData,
+    UserTimeOut,
 }
+
+impl Display for CompanyError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
+        write!(f, "Got a CompanyError")
+    }
+}
+
+#[derive(Debug)]
+struct BaseError;
+
+impl Display for BaseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), std::fmt::Error> {
+        write!(f, "Got a BaseError")
+    }
+}
+
+impl Error for CompanyError {}
+
+impl Error for BaseError {}
+
+// fn try_to_make_number(int_input: &str, float_input: &str) -> Result<(), ParseIntError> {
+//     let my_number = int_input.parse::<i32>()?; // Ok(8) else return Err
+//     let other_number = float_input.parse::<f32>()?;
+//     Ok()
+// }
 
 fn main() {}
