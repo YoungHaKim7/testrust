380c0d084d9493e060ff33662079e29646f4441b
YoungHa Kim <ytok1108@aol.com>
2022-04-06 23:18:22 +0900

    Kor139_Errors__003__error code
    the trait __From<ParseFloatError>__ is not implemented for __ParseIntError__

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::num::ParseIntError;
 
-fn try_to_make_number(input: &str) -> Result<i32, ParseIntError> {
-    let my_number = input.parse::<i32>()?; // Ok(8) else return Err
-    let other_number = input.parse::<f32>()?;
+fn try_to_make_number(int_input: &str, float_input: &str) -> Result<(), ParseIntError> {
+    let my_number = int_input.parse::<i32>()?; // Ok(8) else return Err
+    let other_number = float_input.parse::<f32>()?;
 }
 
 fn main() {}
