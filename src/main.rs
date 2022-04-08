3e01821c84f6b1aaa6e0d7e7e880d335d57bfa3d
YoungHa Kim <ytok1108@aol.com>
2022-04-08 19:53:27 +0900

    Kor141 anyhow__004 __result: Err(invalid float literal) Err(invalid digit found in string)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use anyhow::{anyhow, Error};
 
 fn try_to_make_numbers(int: &str, float: &str) -> Result<(), Error> {
+    let my_integer = int.parse::<i32>()?;
+    let my_float = float.parse::<f64>()?;
+
     let x = 9;
     if x == 9 {
-        return Err(anyhow!("Uh oh, x should't be 9"));
+        return Err(anyhow!("Uh oh, x shouldn't be 9"));
     }
     Ok(())
 }
