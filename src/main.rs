8cc3ae0df844756c2706e4aa36e2f0efedb058ed
YoungHa Kim <ytok1108@aol.com>
2022-04-08 20:00:46 +0900

    Kor141 anyhow vs thiserror

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
+// thiserror 더 정확한 에러를 만들고 싶을때
+// anyhow 그냥 간단한 에러를 만들때 쓰면 된다.(아주 간단한 에러 자세히 X)
 use anyhow::{anyhow, Context, Error};
 
 fn try_to_make_numbers(int: &str, float: &str) -> Result<(), Error> {
     let my_integer = int.parse::<i32>().with_context(|| "Extra info is here")?;
     let my_float = float
         .parse::<f64>()
-        .with_context(|| "Extra flota info is here")?;
+        .with_context(|| "Extra float info is here")?;
     Ok(())
 }
-
-
 
 fn main() {
     let first_try = try_to_make_numbers("8", "tnohenthojek");
