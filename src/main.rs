3d547c66e0db758057dd1a580dfb7a01445a8fdb
YoungHa Kim <ytok1108@aol.com>
2022-04-13 20:26:31 +0900

    Random___thread_rng__Rng__result: 5 3 7 6 1

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use rand;
+use rand::{thread_rng, Rng};
 
 fn main() {
+    let mut rng = thread_rng();
     for _ in 0..5 {
-        let random_u16 = rand::random::<u16>();
-        let random_char = rand::random::<char>();
-        println!("{random_u16}, {random_char}");
+        println!("{}", rng.gen_range(1..10)); // gen_range(1,10);
     }
 }
