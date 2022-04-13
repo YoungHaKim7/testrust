6db789950e6dda9bd7a5419de6d4e832bd4db261
YoungHa Kim <ytok1108@aol.com>
2022-04-13 20:29:42 +0900

    Random___thread_rng__Rng_002 char 'a'__'result: ᵫ 쉌 롒 Բ

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ use rand::{thread_rng, Rng};
 fn main() {
     let mut rng = thread_rng();
     for _ in 0..5 {
-        println!("{}", rng.gen_range(1..=11)); // gen_range(1,10);
+        println!("{}", rng.gen_range('a'..'행')); // gen_range(1,10);
     }
 }
