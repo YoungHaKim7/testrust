d756f814cc1390e6d0aa2eaba1a9712fcd06466b
YoungHa Kim <ytok1108@aol.com>
2022-04-14 23:08:44 +0900

    Kor char print '-' print 1000 ea

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
+use rand::prelude::*;
+use std::convert::TryFrom; // Note: talk about prelude
+
+// prelude 만드는 방법
+// mod prelude {
+//     pub use TypeName;
+// }
+
 fn main() {
-    //    u32 -> char는 4bite
-    let some_char = char::from(99);
-    println!("{some_char}");
+    let mut random_generator = rand::thread_rng();
+    for _ in 0..1000 {
+        let bigger_character =
+            char::try_from(random_generator.gen_range(u32::MIN..u32::MAX)).unwrap_or('-');
+        print!("{bigger_character}");
+    }
 }
 
