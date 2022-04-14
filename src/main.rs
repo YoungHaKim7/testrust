55a2bb41541c4fc947e30300a70ee3dc04fc2e70
YoungHa Kim <ytok1108@aol.com>
2022-04-14 23:18:07 +0900

    Kor147 chr___print

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use rand::prelude::*;
-use std::convert::TryFrom; // Note: talk about prelude
+// Note: talk about prelude
 
 // prelude 만드는 방법
 // mod prelude {
@@ -8,7 +8,7 @@ use std::convert::TryFrom; // Note: talk about prelude
 
 fn main() {
     let mut random_generator = rand::thread_rng();
-    for _ in 0..1000 {
+    for _ in 0..40000 {
         let bigger_character =
             char::try_from(random_generator.gen_range(u32::MIN..u32::MAX)).unwrap_or('-');
         print!("{bigger_character}");
