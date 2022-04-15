f27ca19fdc59a66ae3e9e32dbc6dabd558ff485a
YoungHa Kim <ytok1108@aol.com>
2022-04-15 22:08:22 +0900

    Kor148_Std library tour 2

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,9 @@
-use rand::prelude::*;
-// Note: talk about prelude
-
-// prelude 만드는 방법
-// mod prelude {
-//     pub use TypeName;
-// }
+// integers
+// .checked_add()
+// .saturating_add()
+// checked_mul()
 
 fn main() {
-    let mut random_generator = rand::thread_rng();
-    for _ in 0..40000 {
-        let bigger_character =
-            char::try_from(random_generator.gen_range(u32::MIN..u32::MAX)).unwrap_or('-');
-        print!("{bigger_character}");
-    }
+    let first_number = 100u8;
+    println!("{}", first_number.saturating_add(200));
 }
-
