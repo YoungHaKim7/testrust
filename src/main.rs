df59dc8faa6aa112c49ad8f3118173620671dee4
YoungHa Kim <ytok1108@aol.com>
2022-04-16 20:52:45 +0900

    bool -> Option<T>___Option<&str> result: Got a true Got a true [Some(_It's true, you know_), None, Some(__It's true, you know_), None, None]

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 // .then
 
 fn main() { // bool -> Option<T>
-println!("{:?}", false.then(|| {
-    9
-}));
+    let bool_vec = vec![true, false,true, false, false];
+
+    let option_vec = bool_vec
+        .iter()
+        .map(|item| {
+            item.then(|| {
+                println!("Got a {item}!");
+                "It's true, you know" // Option<&str>
+            })
+        })
+        .collect::<Vec<_>>();
+    println!("{option_vec:?}");
 }
