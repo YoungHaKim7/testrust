652632ac0409b4623d5eaa32e0329d7d21618c84
YoungHa Kim <ytok1108@aol.com>
2022-04-18 19:59:08 +0900

    Kor151___insert vec ___fast__ Inserts an element at position index within the vector, shifting all elements after it to the right.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 // dedup - deduplicate
 fn main() {
-    let mut vec = vec!["foo", "bar", "Bar", "baz", "bar"];
-    vec.dedup_by(|a, b| a.eq_ignore_ascii_case(b));
-    println!("{:?}", vec);
+    let mut vec = vec![1, 2, 3];
+    vec.insert(1, 4);
+    assert_eq!(vec, [1, 4, 2, 3]);
+    vec.insert(4, 5);
+    assert_eq!(vec, [1, 4, 2, 3, 5]);
 }
