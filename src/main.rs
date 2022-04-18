a370d74a43dd94ee183659b694e67702e5f31ed4
YoungHa Kim <ytok1108@aol.com>
2022-04-18 19:42:41 +0900

    shrink_to_fit().vec __result: 10000 20000 10001

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 // dedup - deduplicate
 fn main() {
     let mut my_vec = vec![10; 10000];
-    println!("{}", my_vec.capacity());
+    println!("{}", my_vec.capacity()); // Vec::with_capacity(10001);
     my_vec.push(9);
 
+    println!("{}", my_vec.capacity());
+    my_vec.shrink_to_fit();
     println!("{}", my_vec.capacity());
 }
