3f9149b52c312abc0bb4b662ab17b79bc5a5c92b
YoungHa Kim <ytok1108@aol.com>
2022-04-18 19:39:20 +0900

    .capacity()

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 // dedup - deduplicate
 fn main() {
-    let mut my_vec = vec!["sun", "moon", "sun", "moon", "sun", "moon", "moon"];
-    my_vec.sort_unstable();
-    my_vec.dedup();
-    println!("{my_vec:?}");
+    let my_vec = vec![10; 10000];
+    println!("{}", my_vec.capacity());
 }
