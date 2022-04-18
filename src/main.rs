b2a78cdbf4a3bdfceb950e100188fe9ce6683d7d
YoungHa Kim <ytok1108@aol.com>
2022-04-18 19:18:11 +0900

    Kor151_Std library tour 4__sort__result: [0, 0, 0, 80, 90, 100]

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,5 @@
-// String 은 u8타입으로 UTF-8 로 변환?
-// A UFT-8-encoded, growable string.
-// The String type is the most common string type that has ownership over the contents of the string. It has a close relationship with its borrowed counterpart, the primitive str.
-// pub struct Sting {
-//     vec: Vec<u8>,
-// }
-
-#[derive(Debug)]
-struct NeedsAStatic {
-    name: &'static str,
-}
-
-fn get_our_data() -> String {
-    "Data".to_string()
-}
-
 fn main() {
-    // Vec<T> Box<T> // owned data
-    let our_data = get_our_data(); // String
-    let boxed_data = Box::new(our_data); // Box<String>
-    let leaked_data = Box::leak(boxed_data); // &'static str
-
-    let our_stuct = NeedsAStatic { name: leaked_data };
-
-    println!("{our_stuct:?}");
+    let mut my_vec = vec![100, 90, 80, 0, 0, 0];
+    my_vec.sort();
+    println!("{my_vec:?}");
 }
