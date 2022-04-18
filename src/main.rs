a3f67dd7f894cbf9e14893c353c2dfff16c5a506
YoungHa Kim <ytok1108@aol.com>
2022-04-18 19:56:40 +0900

    dedup_by__ Removes all but the first of consecutive elements in the vector satisfying a given equality relation. result: [_foo_, _bar_, _baz_, _bar_]

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 // dedup - deduplicate
-#![feature(slice_partition_dedup)]
 fn main() {
-    let mut slice = ["foo", "Foo", "BAZ", "Bar", "bar", "baz", "BAZ"];
-
-    let (dedup, duplicates) = slice.partition_dedup_by(|a, b| a.eq_ignore_ascii_case(b));
-
-    println!("{:?}", dedup);
-    println!("{:?}", duplicates);
+    let mut vec = vec!["foo", "bar", "Bar", "baz", "bar"];
+    vec.dedup_by(|a, b| a.eq_ignore_ascii_case(b));
+    println!("{:?}", vec);
 }
