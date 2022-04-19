5492452f9750ec421321b6762ba0d0aeef5c0628
YoungHa Kim <ytok1108@aol.com>
2022-04-19 20:35:12 +0900

    Kor152_Std library tour 5__retain__result: [10, 11]

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-// dedup - deduplicate
+// retain
+// String pop
+// from_utf8
+// from_utf8_lossy
+// split_off
+
 fn main() {
-    let mut vec = vec![1, 2, 3];
-    vec.insert(1, 4);
-    assert_eq!(vec, [1, 4, 2, 3]);
-    vec.insert(4, 5);
-    assert_eq!(vec, [1, 4, 2, 3, 5]);
+    let mut my_vec = vec![8, 9, 10, 11]; // .iter().filter()
+    my_vec.retain(|number| number > &9);
+    println!("{my_vec:?}");
 }
