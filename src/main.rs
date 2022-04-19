a68110c6c610d7e5ca7274afbc837287c8aea83f
YoungHa Kim <ytok1108@aol.com>
2022-04-19 21:06:59 +0900

    check No.___chars__count

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@
 
 fn main() {
     let mut my_string = String::from("Hello there");
+    my_string.chars().count(); // split_off 쓸 길이를 미리 알아보는 코드
     let second_string = my_string.split_off(5);
     println!("{my_string}, {second_string}");
 }
