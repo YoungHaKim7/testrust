8b75289c70087b265c3c845435777a9fd8e5c8ff
YoungHa Kim <ytok1108@aol.com>
2022-04-21 00:31:28 +0900

    :( i love std crates

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,19 @@
 // OsString, OsStr
 // FFI, Foreign function interface
 
+// rand
+// cargo.toml ->
+// [dependencies]
+// rand = "0.8.1"
+
 #![no_implicit_prelude]
+
+extern crate std;
+use std::convert::From;
+use std::{println, string::String, vec};
 fn main() {
     let my_vec = vec![8, 9, 10];
     let my_string = String::from("This won't work");
-    println!("{my_vec}, {my_string}");
+    println!("{my_vec:?}, {my_string:?}");
 }
 
