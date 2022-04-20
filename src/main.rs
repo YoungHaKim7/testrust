f28d50a92b8a408b158f9499722f5a3abfef8e35
YoungHa Kim <ytok1108@aol.com>
2022-04-21 00:26:54 +0900

    Kor153_Std library tour6___no_implicit_prelude  prelude thank you

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-// retain
-// String pop
-// from_utf8
-// from_utf8_lossy
-// split_off
+// String, &str
+// CString, CStr
+// OsString, OsStr
+// FFI, Foreign function interface
 
+#![no_implicit_prelude]
 fn main() {
-    let mut my_string = String::from("Hello there");
-    my_string.chars().count(); // split_off 쓸 길이를 미리 알아보는 코드
-    let second_string = my_string.split_off(5);
-    println!("{my_string}, {second_string}");
+    let my_vec = vec![8, 9, 10];
+    let my_string = String::from("This won't work");
+    println!("{my_vec}, {my_string}");
 }
+
