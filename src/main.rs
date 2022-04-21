3f5ac5bbc05668fc1f9a4e936d5d6379c5a8fb17
YoungHa Kim <ytok1108@aol.com>
2022-04-22 07:49:14 +0900

    Box<T> result: 8 11

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 use std::mem::{size_of, size_of_val};
 
 struct MyStruct {
-    bunch_of_stuff: [u32; 1000],
+    bunch_of_stuff: Box<[u32; 1000]>,
 }
-
-
 
 fn main() {
     println!("{} {}", size_of::<MyStruct>(), size_of_val("I am a &str"));
