71c53df29f078140fb8d4c5d1d9d871fa9162c3e
YoungHa Kim <ytok1108@aol.com>
2022-04-22 08:00:48 +0900

    not 9byte___16byte___ look at ******

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::mem::align_of;
 
 struct MyStruct {
-    bunch_of_stuff: u8,
+    bunch_of_stuff: u8, // 1 byte
     more_stuff: u64,
-}
+} // /9
+
+// ********
+// *///////
 
 fn main() {
     println!("{}", align_of::<MyStruct>());
+    println!("{}", std::mem::size_of::<MyStruct>());
 }
