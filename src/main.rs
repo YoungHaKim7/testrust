67f8df02a107f14cae36bf6d43727f3732610c30
YoungHa Kim <ytok1108@aol.com>
2022-04-22 20:42:02 +0900

    transmute__DON'T USE THIS __ struct 를 믿게 하기 ㅎㅎu8을 i32로 믿어 ㅋㅋㅋ

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,14 @@ use std::mem::{replace, swap, take, transmute};
 // where
 //     T: Default,
 //     // transmute DON'T USE THIS ㅋㅋㅋㅋ
+#[derive(Debug)]
+struct MyStruct {
+    number: i32,
+}
 
 fn main() {
     let my_numbers = [8u8, 9, 10, 11]; // [u8; 4] 4bytes
-    let new_number = unsafe { transmute::<[u8; 4], i32>(my_numbers) };
+    let new_number = unsafe { transmute::<[u8; 4], MyStruct>(my_numbers) };
 
-    println!("{new_number}");
+    println!("{new_number:?}");
 }
