00b0575ff8718f5e5358debbaf21ed6d8041ba97
YoungHa Kim <ytok1108@aol.com>
2022-04-22 20:14:02 +0900

    Kor155: Std library tour8__swap__result: string_1: I am String 2, string_2: I am String 1

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
-use std::mem::align_of;
+use std::mem::{replace, swap, take, transmute};
 
-struct MyStruct {
-    bunch_of_stuff: u16, // 4 byte
-    more_stuff: u32,
-} // /8
+// //swap
+// pub fn swap<T>(x: &mut T, y: &mut T)
 
-// ****
-// *///
+// // replace
+// pub fn replace<T>(dest: &mut T, src: T) -> T
+
+// // take
+// pub fn take<T>(dest: &mut T) -> T
+// where
+//     T: Default,
+//     // transmute DON'T USE THIS ㅋㅋㅋㅋ
 
 fn main() {
-    println!("{}", align_of::<MyStruct>());
-    println!("{}", std::mem::size_of::<MyStruct>());
+    let mut string_1 = String::from("I am String 1");
+    let mut string_2 = String::from("I am String 2");
+
+    swap(&mut string_1, &mut string_2);
+
+    println!("string_1: {string_1}, string_2: {string_2}");
 }
