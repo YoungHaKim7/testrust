6e2a5f7ca8e4f78faa9bdb85eec51397f4e0f370
YoungHa Kim <ytok1108@aol.com>
2022-04-23 23:11:29 +0900

    Kor156_Std library tour9__dbg__column_line_file_module_path__macro

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,9 @@
-use std::mem::{replace, swap, take, transmute};
-
-// //swap
-// pub fn swap<T>(x: &mut T, y: &mut T)
-
-// // replace
-// pub fn replace<T>(dest: &mut T, src: T) -> T
-
-// // take
-// pub fn take<T>(dest: &mut T) -> T
-// where
-//     T: Default,
-//     // transmute DON'T USE THIS ㅋㅋㅋㅋ
-#[derive(Debug)]
-struct MyStruct {
-    number: i32,
-}
+// column!()
+// line!()
+// line!()
+// module_path!()
 
 fn main() {
-    let my_numbers = [8u8, 9, 10, 11]; // [u8; 4] 4bytes
-    let new_number = unsafe { transmute::<[u8; 4], MyStruct>(my_numbers) };
-
-    println!("{new_number:?}");
+    dbg!()
 }
+
