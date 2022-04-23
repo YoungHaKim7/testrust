1944b1111923d555d5c1332b125297b64a0bba4d
YoungHa Kim <ytok1108@aol.com>
2022-04-23 23:41:07 +0900

    concat__result: atrue7.7Hello

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,5 @@
-// column!()
-// line!()
-// line!()
-// module_path!()
-
 fn main() {
-    println!("{} {} {} {}", column!(), line!(), file!(), module_path!());
-
-    let my_file = include_str!(
-        "/Users/globalyoung/Documents/Project/Github/rust_project/testrust/txt_sort/rust_sort.txt"
-    );
-
-    println!("{:?}", include_bytes!("/Users/globalyoung/Documents/Project/Github/rust_project/testrust/txt_sort/rust_sort.txt"
-    ));
+    let my_string_slice = concat!('a', true, 7.7, "Hello");
+    println!("{my_string_slice}");
 }
 
