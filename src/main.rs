ef2696c324d17c20fc3eec1621a0961316b63a7c
YoungHa Kim <ytok1108@aol.com>
2022-04-26 01:01:40 +0900

    debug___macro___result: 9 _Hi there_ [8, 9, 10]

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 macro_rules! might_print {
     ($input:expr) => {
-        println!("{}", $input);
+        println!("{:?}", $input);
     };
 }
 
 fn main() {
     might_print!(9);
     might_print!("Hi there");
+    let my_vec = vec![8, 9, 10];
+    might_print!(my_vec);
 }
