a47d6adde3bea2fd52aa65df4b9a6d74c1025192
YoungHa Kim <ytok1108@aol.com>
2022-04-26 00:47:57 +0900

    Kor158__Macros 2__result: error code no rules expected the token __9_

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 macro_rules! might_print {
-    (Hey there 하하하 will this still work? ) => {
-        println!("You guessed the secret message");
-    };
-    () => {
-        println!("You didn't guess it")
+    (input) => {
+        println!("", input);
     };
 }
 
 fn main() {
-    might_print!(Hey there 하하하 will this still work? );
+    might_print!(9);
+    might_print!("Hi there");
 }
