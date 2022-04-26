1b7bf84c6daee0433d2018de09b1e692943ed157
YoungHa Kim <ytok1108@aol.com>
2022-04-27 01:24:43 +0900

    Macro 3 good

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 // Programming Rust
 // Perhaps after reading this chapter you've decided that you hate macros.
 
-macro_rules! print_anything {
-    ($($input:tt)<<+) => {
-        let output = stringify!($($input),+);
-        println!("{output}");
+macro_rules! make_a_function {
+    ($name:ident, $($input:tt),*) => {
+        fn $name() {
+            let output = stringify!($($input),*);
+            println!("{output}");
+        }
     };
 }
 
 fn main() {
-    print_anything!(thothe << 9);
+    make_a_function!(print_it, 6, 7, 8, "I");
+    make_a_function!(say_its_nice, this, is, really, nice);
+    print_it();
+    say_its_nice();
 }
