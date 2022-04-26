5e56610eba6aa0a5981a580429b7e4836cf96f9f
YoungHa Kim <ytok1108@aol.com>
2022-04-27 01:05:12 +0900

    Macro__seperator___result: thothe, 9

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@
 // Perhaps after reading this chapter you've decided that you hate macros.
 
 macro_rules! print_anything {
-    ($($input:tt),+) => {
+    ($($input:tt)<<+) => {
         let output = stringify!($($input),+);
         println!("{output}");
     };
 }
 
 fn main() {
-    print_anything!(thothe, 9, thdoefgoe, 765);
+    print_anything!(thothe << 9);
 }
