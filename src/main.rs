2d9403bc7ab3e2efa783ae6dc16257a1fcf4f6f2
YoungHa Kim <ytok1108@aol.com>
2022-04-28 01:06:19 +0900

    Kor160_Macros4___result: Let's print this;

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,15 @@
-// Programming Rust
-// Perhaps after reading this chapter you've decided that you hate macros.
-
-macro_rules! make_a_function {
-    ($name:ident, $($input:tt),*) => {
-        fn $name() {
-            let output = stringify!($($input),*);
-            println!("{output}");
-        }
+macro_rules! my_macro {
+    () => {
+        println!("Let's print this;")
+    };
+    ($input:expr) => {
+        my_macro!()
+    };
+    ($($input:expr),*) => {
+        my_macro!();
     };
 }
 
 fn main() {
-    make_a_function!(print_it, 6, 7, 8, "I");
-    make_a_function!(say_its_nice, this, is, really, nice);
-    print_it();
-    say_its_nice();
+    my_macro!(vec![8, 9, 10]);
 }
