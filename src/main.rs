dd42adbefa2f24563bc528cf2f207821f87a6b73
YoungHa Kim <ytok1108@aol.com>
2022-04-28 20:35:52 +0900

    array__use

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,13 @@
 // type generics - generics over a type
 // const generics - generics over a const
-// const generics
-// C++의 __ constexpr 와 비슷함
-
-struct SomeVecs<T> {
-    vec_1: Vec<T>,
-    vec_2: Vec<T>,
+struct SomeArrays<T> {
+    array_1: [T; 3],
+    array_2: [T; 4],
 }
 
 fn main() {
-    let my_vecs = SomeVecs {
-        vec_1: vec![8, 9, 10],   // [i32; 3]
-        vec_2: vec![89, 98, 98], // [i32; 4]
+    let my_arrays = SomeArrays {
+        array_1: [8, 9, 10],
+        array_2: [89, 98, 98, 99],
     };
 }
