f8ebcd06518372455c3e00254b17237deef96dfb
YoungHa Kim <ytok1108@aol.com>
2022-04-28 20:42:02 +0900

    Const ___const  :) result: same

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 // type generics - generics over a type
 
 // const generics - generics over a const
+
+const BIG: usize = 100;
+
 #[derive(Debug)]
 struct SomeArrays<T, const N: usize> {
     array_1: [T; N],
@@ -9,8 +12,8 @@ struct SomeArrays<T, const N: usize> {
 
 fn main() {
     let my_arrays = SomeArrays {
-        array_1: [0; 100],
-        array_2: [0; 100],
+        array_1: [0; BIG],
+        array_2: [0; BIG],
     };
     println!("{my_arrays:?}")
 }
