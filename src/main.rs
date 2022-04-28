64ff63acbbfe6c46d7e1d58e92fd689a70be527f
YoungHa Kim <ytok1108@aol.com>
2022-04-28 20:37:42 +0900

    const generics - generics over a const___good__

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 // type generics - generics over a type
+
 // const generics - generics over a const
-struct SomeArrays<T> {
-    array_1: [T; 3],
-    array_2: [T; 4],
+struct SomeArrays<T, const N: usize> {
+    array_1: [T; N],
+    array_2: [T; N],
 }
 
 fn main() {
     let my_arrays = SomeArrays {
-        array_1: [8, 9, 10],
-        array_2: [89, 98, 98, 99],
+        array_1: [0; 100],
+        array_2: [0; 100],
     };
 }
