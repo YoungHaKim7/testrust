f644555fa6253f3a46bf4d8c9a6cc4cb3511070b
YoungHa Kim <ytok1108@aol.com>
2022-04-29 20:29:31 +0900

    allocations are not allowed in constant functions__error code__

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
-// const fn
+use std::time::Duration;
 
-const fn get_number(input: i32) -> i32 {
-    if input > 10 {
-        9
-    } else {
-        10
-    }
+const fn get_my_duration(seconds: u64, nanos: u32) -> Duration {
+    Duration::new(seconds, nanos)
+}
+const fn check() {
+    let my_vec = vec![8, 9, 10];
 }
 
-
-
-const MY_NUMBER: i32 = get_number(10);
+const MY_DURATION: Duration = get_my_duration(100, 10);
 
 fn main() {
+    println!("{MY_DURATION:?}");
+    let other_duration = get_my_duration(50, 100);
 }
