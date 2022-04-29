96ba5d7a47527fda7161800a3d0d51225d8f4997
YoungHa Kim <ytok1108@aol.com>
2022-04-29 20:22:33 +0900

    Kor162__Const functions___

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,16 @@
-// type generics - generics over a type
-
-// const generics - generics over a const
-
-// const context
-
 // const fn
 
-// resolves to a const value.
-
-const BIG: usize = 100;
-
-#[derive(Debug)]
-struct SomeArrays<T, const N: usize> {
-    array_1: [T; N],
-    array_2: [T; N],
+const fn get_number(input: i32) -> i32 {
+    if input > 10 {
+        9
+    } else {
+        10
+    }
 }
 
+
+
+const MY_NUMBER: i32 = get_number(10);
+
 fn main() {
-    let my_thing = 1; // const context
-    let my_arrays = SomeArrays {
-        array_1: [0; BIG],
-        array_2: [0; 99],
-    };
-    println!("{my_arrays:?}")
 }
