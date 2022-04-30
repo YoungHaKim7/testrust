8c1bae17dee9b84f392d9a9857b52ef629a3b0e1
YoungHa Kim <ytok1108@aol.com>
2022-04-30 19:15:00 +0900

    Error Code ___const___calls in constants are limited to constant functions, tuple structs and tuple variants

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
-const MY_VEC: Vec<i32> = vec![8, 9, 10];
+const ERROR_LISTENER: ErrorListener = ErrorListener {
+    url: "www.nthdidjf.com".to_string(),
+};
+
+#[derive(Debug)]
+struct ErrorListener {
+    url: String,
+}
+
+impl ErrorListener {
+    fn check_for_error(&self) -> Result<(), ()> {
+        Ok(())
+    }
+}
+
+fn do_stuff() {
+    ERROR_LISTENER.check_for_error();
+}
 
 fn main() {}
 
