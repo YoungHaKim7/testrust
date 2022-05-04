e87bf2d07fd72ec84049d63c462e2ecf0cdec06b
YoungHa Kim <ytok1108@aol.com>
2022-05-04 19:54:37 +0900

    Kor167_Reqwest_1__result: title___:___San Francisco__

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,8 @@
-use lazy_static::lazy_static;
-use serde::{Deserialize, Serialize};
-
-// feature
-#[derive(Serialize, Deserialize)]
-struct ErrorListener {
-    url: String,
-}
-
-lazy_static! {
-    static ref ERROR_LISTENER: ErrorListener = ErrorListener {
-        url: "toheo".to_string()
-    };
-}
-
 fn main() {
-    let rng = rand::thread_rng();
+    let body = reqwest::blocking::get("https://www.metaweather.com/api/location/search/?query=san")
+        .unwrap()
+        .text()
+        .unwrap();
 
-    println!("Hello, world");
-
-    let my_string = String::from("I am String");
-
-    let my_vec = vec![8, 9, 10];
-
-    let my_new_vec = my_vec
-        .iter() // iterators are lazy
-        .map(|number| number + 1)
-        .collect::<Vec<i32>>();
+    println!("{body:?}");
 }
