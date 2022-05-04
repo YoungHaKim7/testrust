e2cc8c0a213b5dcbd02fbf422516bee6179e9120
YoungHa Kim <ytok1108@aol.com>
2022-05-04 20:07:59 +0900

    Kor167_Reqwest_1_______

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+#[derive(Serialize, Deserialize)]
+struct City {
+    title: String,
+    location_type: String,
+    woeid: u32,
+    latt_long: String,
+}
 fn main() {
     let body = reqwest::blocking::get("https://www.metaweather.com/api/location/search/?query=san")
         .unwrap()
