2c5140d9bcf167f1f440f1c344c4b766c669d054
YoungHa Kim <ytok1108@aol.com>
2022-05-22 18:29:25 +0900

    Kor170_fn and async fn___time__Instance

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
+// non-blocking
+// blocking thread blocking
+use std::time;
 use tokio;
 
-fn give_8() -> u8 {
-    // blocking function
-    8
+async fn give_data() -> u8 {
+    // impl Future<Output = u8>
+    7
 }
 
-async fn async_give_8() -> u8 {
-    // impl Future<Output = u8>
-    8
+async fn give_data_again() -> u8 {
+    7
 }
 
 #[tokio::main]
 async fn main() {
-    let my_number = give_8();
-    let my_async_number = async_give_8().await;
-    println!("{my_async_number}");
+    let now = time::Instant::now();
+
+    println!("{:?}", now.elapsed());
 }
