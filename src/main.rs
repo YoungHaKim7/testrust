d8a8857bc53cf95a3d2b58de080d55b328cbeb72
YoungHa Kim <ytok1108@aol.com>
2022-05-22 18:36:38 +0900

    fn and async fn __sleep__result: 7 1.005260208s

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,19 @@
 use std::time;
 use tokio;
 
+fn sleep(duration: u64) {
+    // fn - blocking
+    std::thread::sleep(time::Duration::from_millis(duration));
+}
+
 async fn give_data() -> u8 {
     // impl Future<Output = u8>
+    sleep(1000); // 1sec
     7
 }
 
 async fn give_data_again() -> u8 {
+    sleep(1000);
     7
 }
 
@@ -17,6 +24,7 @@ async fn main() {
     let now = time::Instant::now();
 
     let number_one = give_data().await; // Did not poll yet let number_two = give_data_again();
+    let number_two = give_data_again();
 
     println!("{:?}", number_one);
 
