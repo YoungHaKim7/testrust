0c130c2140a08cde10ae13302e6bacb07bad8fe4
YoungHa Kim <ytok1108@aol.com>
2022-06-19 22:57:29 +0900

    trim() 최고  result: Please type something, or x to escape 35 You wrote: 35n g You wrote: gn s You wrote: sn x You wrote: xn

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ fn main() {
     println!("Please type something, or x to escape");
     let mut input_string = String::new(); // PartialEq<&str>
 
-    while input_string != "x" {
+    while input_string.trim() != "x" {
         input_string.clear();
         io::stdin().read_line(&mut input_string).unwrap();
-        println!("You wrote: {input_string}");
+        println!("You wrote: {input_string:?}");
     }
     println!("See you later!!!");
 }
