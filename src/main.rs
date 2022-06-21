3556691cb36e596a1d653424be37ac3b0533e4bf
YoungHa Kim <ytok1108@aol.com>
2022-06-21 22:14:00 +0900

    Kor174__Program arguments__result: Args { inner: [target/debug/testrust, hello, there, I, am, giving, you, some, args] }

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
-use std::io;
+use std::env;
 
 fn main() {
-    println!("Please type something, or x to escape");
-    let mut input_string = String::new(); // PartialEq<&str>
-
-    while input_string.trim() != "x" {
-        input_string.clear();
-        io::stdin().read_line(&mut input_string).unwrap();
-        println!("You wrote: {input_string:?}");
-    }
-    println!("See you later!!!");
+    let args = env::args();
+    println!("{args:?}");
 }
