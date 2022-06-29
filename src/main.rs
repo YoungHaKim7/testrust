b11b01a1fc66e1df19d09d554b94aa304d7e2763
YoungHa Kim <ytok1108@aol.com>
2022-06-29 22:09:46 +0900

    내가 원하는 env가 없으면 패닉이 나게 해야한다.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
-    std::env::set_var("RUST_LOG", "DEBUG");
     for v in std::env::vars() {
         println!("{v:?}")
     }
+
+    println!("{}", std::env::var("LANGUAGE").unwrap());
 }
 
