6d0531755764e655f20727819ed8b6eb02e677d8
YoungHa Kim <ytok1108@aol.com>
2022-06-29 21:38:56 +0900

    Kor Environment variables__result: (CARGO, /Users/globalyoung/.rustup/toolchains/stable-aarch64-apple-darwin/bin/cargo) (CARGO_HOME, /Users/globalyoung/.cargo) (CAR

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,6 @@
-use lazy_static::lazy_static;
-use once_cell::sync::OnceCell;
-use std::sync::Mutex;
-
-lazy_static! {
-    static ref LAZY_STATIC_LOG_INFO: Mutex<String> = Mutex::new(String::new());
-}
-
-static ONCECELL_LOG_INFO: OnceCell<Mutex<String>> = OnceCell::new();
-
-// Rust 1.63 Good! awesome
-static COOLER_LOG_INFO: Mutex<String> = Mutex::new(String::new());
-
-// Rust 1.63 nightly code
-
 fn main() {
-    *LAZY_STATIC_LOG_INFO.lock().unwrap() = "Important imformation".to_string();
-    ONCECELL_LOG_INFO.set(Mutex::new(String::new())).unwrap();
-    *ONCECELL_LOG_INFO.get().unwrap().lock().unwrap() = "Important information".to_string();
-
-    println!(
-        "One: {:?} \n Two: {:?}",
-        LAZY_STATIC_LOG_INFO.lock().expect("Could not lock mutes"),
-        ONCECELL_LOG_INFO
-    );
-
-    // Rust 1.63 nightly code
-    *COOLER_LOG_INFO.lock().unwrap() = "Important information".to_string();
-    println!("{COOLER_LOG_INFO:?}");
-
-    // Rust 1.63 nightly code
+    for v in std::env::vars() {
+        println!("{v:?}")
+    }
 }
+
