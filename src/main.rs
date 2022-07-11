61461360b1cca31bcd342e392434ca7a1927939f
YoungHa Kim <ytok1108@aol.com>
2022-07-11 21:06:49 +0900

    Rust 1.63 beta code __result: num_3 is : 0 num_3 is : 0 Mutex { data: 2, poisoned: false, .. } 10 0

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,26 @@
-use std::sync::{Arc, Mutex};
+// rust 1.63 beta code ___
+use std::sync::Mutex;
 use std::thread;
 
 fn main() {
-    let num_1 = Arc::new(Mutex::new(0));
-    let num_2 = 0;
+    let num_1 = Mutex::new(0);
+    let mut num_2 = 0;
     let num_3 = 0;
 
-    let clone_1 = Arc::clone(&num_1);
-    let clone_2 = Arc::clone(&num_1);
+    thread::scope(|s| {
+        s.spawn(|| {
+            *num_1.lock().unwrap() += 1;
+            num_2 += 10;
+            println!("num_3 is : {num_3}");
+        });
 
-    // Rust's closures 는 기본적으로 레퍼런스로 쓰려고 하기 때문에
-    // move 로 해 줘야 한다.
-    let mut join_vec = vec![];
+        s.spawn(|| {
+            *num_1.lock().unwrap() += 1;
+            // num_2 += 10;
+            println!("num_3 is : {num_3}");
+        });
+    });
 
-    join_vec.push(thread::spawn(move || {
-        println!("I am in thread 1");
-        *clone_1.lock().unwrap() += 1;
-    }));
-
-    join_vec.push(thread::spawn(move || {
-        println!("I am in thread 2");
-        *clone_2.lock().unwrap() += 1;
-    }));
-
-    for handle in join_vec {
-        handle.join().unwrap();
-    }
-
-    println!("{num_1:?}");
+    println!("{num_1:?} {num_2} {num_3}");
 }
 
