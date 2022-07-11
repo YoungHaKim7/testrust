e6414e4e1d94f071abfc48e180fb6969a53f8223
YoungHa Kim <ytok1108@aol.com>
2022-07-11 20:53:43 +0900

    Scoped threads __ arc 기본 사용법

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,32 @@
+use std::sync::{Arc, Mutex};
 use std::thread;
 
 fn main() {
-    let num_1 = 0; // 'static만 가능하다 지금  rust1.62 version 에서는
+    let num_1 = Arc::new(Mutex::new(0));
     let num_2 = 0;
     let num_3 = 0;
 
-    thread::spawn(|| {
+    let clone_1 = Arc::clone(&num_1);
+    let clone_2 = Arc::clone(&num_1);
+
+    // Rust's closures 는 기본적으로 레퍼런스로 쓰려고 하기 때문에
+    // move 로 해 줘야 한다.
+    let mut join_vec = vec![];
+
+    join_vec.push(thread::spawn(move || {
         println!("I am in thread 1");
-    });
+        *clone_1.lock().unwrap() += 1;
+    }));
 
-    thread::spawn(|| {
+    join_vec.push(thread::spawn(move || {
         println!("I am in thread 2");
-    });
+        *clone_2.lock().unwrap() += 1;
+    }));
 
-    for _ in 0..110000 {
-        let _x = 1;
+    for handle in join_vec {
+        handle.join().unwrap();
     }
+
+    println!("{num_1:?}");
 }
 
