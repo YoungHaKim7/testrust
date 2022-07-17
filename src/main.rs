1effb8e16fc59eff11e7c5c7d1d2c1f8f0ffad66
YoungHa Kim <ytok1108@aol.com>
2022-07-17 23:48:29 +0900

    Kor179__Working with files004__truncate_append result: Hello there Hello there

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 use std::{fs::File, io::Write};
 
 fn main() {
-    let mut file = File::create("file.txt").unwrap();
-    file.write_all("Hello there\n".as_bytes()).unwrap();
+    // let mut file = File::create("file.txt").unwrap();
+    let mut file = File::options()
+        .read(true)
+        .write(true)
+        .append(true)
+        .truncate(false)
+        .open("file.txt")
+        .unwrap();
+    file.write_all(b"Hello there\n").unwrap();
 }
