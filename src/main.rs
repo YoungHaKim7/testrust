a71920f7eadf484de94a05a78b96160fde622705
YoungHa Kim <ytok1108@aol.com>
2022-07-17 23:44:47 +0900

    Kor179__Working with files003___as_bytes()

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ use std::{fs::File, io::Write};
 
 fn main() {
     let mut file = File::create("file.txt").unwrap();
-    file.write_all(b"Hello there\n").unwrap();
+    file.write_all("Hello there\n".as_bytes()).unwrap();
 }
