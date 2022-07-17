97cb4a1cfebcab72839af2ef30791194f4eb8198
YoungHa Kim <ytok1108@aol.com>
2022-07-18 00:34:18 +0900

    Kor179___Reading files result: Hello there Hello there Hello there Hello there Hello there

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{fs::File, io::Write};
+use std::{
+    fs::File,
+    io::{Read, Write},
+};
 
 fn main() {
     // let mut file = File::create("file.txt").unwrap();
@@ -9,5 +12,9 @@ fn main() {
         .truncate(false)
         .open("file.txt")
         .unwrap();
-    file.write_all(b"Hello there\n").unwrap();
+    // file.write_all(b"Hello there\n").unwrap();
+
+    let mut file_string = String::new();
+    file.read_to_string(&mut file_string).unwrap();
+    println!("{file_string}");
 }
