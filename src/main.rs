be03057724bd1a2a674370cdfeaa465a1c7cbe7f
YoungHa Kim <ytok1108@aol.com>
2022-07-18 00:36:58 +0900

    Kor179___Reading files result2: Hello there Hello there Hello there Hello there Hello there

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fs::read_to_string;
 use std::{
     fs::File,
     io::{Read, Write},
@@ -5,16 +6,18 @@ use std::{
 
 fn main() {
     // let mut file = File::create("file.txt").unwrap();
-    let mut file = File::options()
-        .read(true)
-        .write(true)
-        .append(true)
-        .truncate(false)
-        .open("file.txt")
-        .unwrap();
+    // let mut file = File::options()
+    //     .read(true)
+    //     .write(true)
+    //     .append(true)
+    //     .truncate(false)
+    //     .open("file.txt")
+    //     .unwrap();
     // file.write_all(b"Hello there\n").unwrap();
 
-    let mut file_string = String::new();
-    file.read_to_string(&mut file_string).unwrap();
-    println!("{file_string}");
+    // let mut file_string = String::new();
+    // file.read_to_string(&mut file_string).unwrap();
+    // println!("{file_string}");
+    println!("{}", read_to_string("file.txt").unwrap());
+    // let my_str = include_str!("file.txt");
 }
