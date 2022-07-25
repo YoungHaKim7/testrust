9bb9ac2ef3af804fab3b41b805dcefda4999a80d
YoungHa Kim <ytok1108@aol.com>
2022-07-25 20:37:53 +0900

    Eng_easy rust 187_Using serde

---
+++ easyRustProject/187UsingSerde/serde/src/main.rs
@@ -0,0 +1,14 @@
+// serde
+// Serialization - JSON, YAML
+// Deserialization - into a Rust type
+
+r#"
+{
+    "id": "8787", 
+    "name": "Mr UserSon"
+}
+"#;
+
+fn main() {
+    println!("Hello, world!");
+}
