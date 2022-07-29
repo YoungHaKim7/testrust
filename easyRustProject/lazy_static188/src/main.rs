888e97a7c76f1bfc6f128769af92f3086c4db35e
YoungHa Kim <ytok1108@aol.com>
2022-07-29 22:25:53 +0900

    The lazy_static crate __result: Doing stuff. Is there an error? Checking stuff. Any errors?

--- easyRustProject/lazy_static188/src/main.rs
+++ easyRustProject/lazy_static188/src/main.rs
@@ -6,11 +6,30 @@ struct ErrorListener {
     client: Client,
 }
 
+impl ErrorListener {
+    fn check_for_error(&self) -> Result<(), ()> {
+        Ok(())
+    }
+}
+
+fn do_stuff() {
+    println!("Doing stuff. Is there an error?");
+    ERROR_LISTENER.check_for_error().unwrap();
+}
+
+fn check_something_else() {
+    println!("Checking stuff. Any errors?");
+    ERROR_LISTENER.check_for_error().unwrap();
+}
+
 lazy_static! {
-    static ref ERORR_LISTENER: ErrorListener = ErrorListener {
+    static ref ERROR_LISTENER: ErrorListener = ErrorListener {
         url: "lksdjflksjdflksdjf".to_string(),
         client: Client::default(),
     };
 }
 
-fn main() {}
+fn main() {
+    do_stuff();
+    check_something_else();
+}
